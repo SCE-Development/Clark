874c654f92daa761be6dec3fff1750fd3c51fa11
Rolando Javier <rjavier441@gmail.com>
2018-01-24 14:47:00 -0800

    This commit adds a help prompt to the SCE Database Setup Script

--- mdbi/tools/sce_db_setup_v0.js
+++ mdbi/tools/sce_db_setup_v0.js
@@ -23,6 +23,8 @@ var arg = process.argv[2];
 var mongo_settings = require("../mongo_settings");
 var mongo = require("mongodb").MongoClient;
 var mdb = require("../mongoWrapper");	// acquire MongoDB API Wrappers
+
+// Globals
 var mongoOptions = {
 	"appname": "SCE DB Setup v0"
 };
@@ -78,123 +80,127 @@ var placeholders = {
 		"imgPath": "placeholder"
 	}
 };
+var url = `mongodb://${encodeURIComponent(credentials.user)}:${encodeURIComponent(credentials.pwd)}@${mongo_settings.hostname}:${mongo_settings.port}/${mongo_settings.database}`;
 
 // BEGIN Database Client
-var url = `mongodb://${encodeURIComponent(credentials.user)}:${encodeURIComponent(credentials.pwd)}@${mongo_settings.hostname}:${mongo_settings.port}/${mongo_settings.database}`;
-mongo.connect(url, function (err, db) {
-	console.log("Server connection established. Authenticating...");
+if (arg === "--help") {
+	help();
+} else {
+	mongo.connect(url, function (err, db) {
+		console.log("Server connection established. Authenticating...");
 
-	// Authenticate
-	if (!err) {
-		console.log("Auth Successful...");
+		// Authenticate
+		if (!err) {
+			console.log("Auth Successful...");
 
-		// Handle arguments
-		switch (arg) {
-			// Get database stats
-			case "--stats": {
-				console.log("Getting db statistics...");
-				db.command({"dbStats": 1}, function (err, results) {
-					if (err) {
-						console.log((typeof err === "object") ? JSON.stringify(err) : err);
-					} else {
-						console.log(results);
-					}
+			// Handle arguments
+			switch (arg) {
+				// Get database stats
+				case "--stats": {
+					console.log("Getting db statistics...");
+					db.command({"dbStats": 1}, function (err, results) {
+						if (err) {
+							console.log((typeof err === "object") ? JSON.stringify(err) : err);
+						} else {
+							console.log(results);
+						}
 
-					// End database connection
-					endSession(db);
-				});
-				break;
-			}
+						// End database connection
+						endSession(db);
+					});
+					break;
+				}
 
-			// Initialize database
-			default: {
-				console.log("Initializing db to SCE specifications...");
+				// Initialize database
+				default: {
+					console.log("Initializing db to SCE specifications...");
 
-				// Manually create the required collections using MongoDB functions (do not use Mongo Wrappers, since they have a security feature that blocks the creation of new collections that do not exist)...
-				mdb.database = db;
-				var promiseServerActivations = new Promise (function (resolve, reject) {
-					db.collection("serverActivations").insertOne(placeholders.serverActivations, null, function (error, result) {
-						if (error) {
-							console.log(`Error creating collection serverActivations: ${error}`);
-						} else {
-							resolve();
-						}
+					// Manually create the required collections using MongoDB functions (do not use Mongo Wrappers, since they have a security feature that blocks the creation of new collections that do not exist)...
+					mdb.database = db;
+					var promiseServerActivations = new Promise (function (resolve, reject) {
+						db.collection("serverActivations").insertOne(placeholders.serverActivations, null, function (error, result) {
+							if (error) {
+								console.log(`Error creating collection serverActivations: ${error}`);
+							} else {
+								resolve();
+							}
+						});
 					});
-				});
-				var promiseMember = new Promise (function (resolve, reject) {
-					db.collection("Member").insertOne(placeholders.Member, null, function (error, result) {
-						if (error) {
-							console.log(`Error creating collection Member: ${error}`);
-						} else {
-							resolve();
-						}
+					var promiseMember = new Promise (function (resolve, reject) {
+						db.collection("Member").insertOne(placeholders.Member, null, function (error, result) {
+							if (error) {
+								console.log(`Error creating collection Member: ${error}`);
+							} else {
+								resolve();
+							}
+						});
 					});
-				});
-				var promiseMembershipData = new Promise (function (resolve, reject) {
-					db.collection("MembershipData").insertOne(placeholders.MembershipData, null, function (error, result) {
-						if (error) {
-							console.log(`Error creating collection MembershipData: ${error}`);
-						} else {
-							resolve();
-						}
+					var promiseMembershipData = new Promise (function (resolve, reject) {
+						db.collection("MembershipData").insertOne(placeholders.MembershipData, null, function (error, result) {
+							if (error) {
+								console.log(`Error creating collection MembershipData: ${error}`);
+							} else {
+								resolve();
+							}
+						});
 					});
-				});
-				var promiseDoorCode = new Promise (function (resolve, reject) {
-					db.collection("DoorCode").insertOne(placeholders.DoorCode, null, function (error, result) {
-						if (error) {
-							console.log(`Error creating collection DoorCode: ${error}`);
-						} else {
-							resolve();
-						}
+					var promiseDoorCode = new Promise (function (resolve, reject) {
+						db.collection("DoorCode").insertOne(placeholders.DoorCode, null, function (error, result) {
+							if (error) {
+								console.log(`Error creating collection DoorCode: ${error}`);
+							} else {
+								resolve();
+							}
+						});
 					});
-				});
-				var promiseClearanceLevel = new Promise (function (resolve, reject) {
-					db.collection("ClearanceLevel").insertOne(placeholders.ClearanceLevel, null, function (error, result) {
-						if (error) {
-							console.log(`Error creating collection ClearanceLevel: ${error}`);
-						} else {
-							resolve();
-						}
+					var promiseClearanceLevel = new Promise (function (resolve, reject) {
+						db.collection("ClearanceLevel").insertOne(placeholders.ClearanceLevel, null, function (error, result) {
+							if (error) {
+								console.log(`Error creating collection ClearanceLevel: ${error}`);
+							} else {
+								resolve();
+							}
+						});
 					});
-				});
-				var promiseAbility = new Promise (function (resolve, reject) {
-					db.collection("Ability").insertOne(placeholders.Ability, null, function (error, result) {
-						if (error) {
-							console.log(`Error creating collection Ability: ${error}`);
-						} else {
-							resolve();
-						}
+					var promiseAbility = new Promise (function (resolve, reject) {
+						db.collection("Ability").insertOne(placeholders.Ability, null, function (error, result) {
+							if (error) {
+								console.log(`Error creating collection Ability: ${error}`);
+							} else {
+								resolve();
+							}
+						});
 					});
-				});
-				var promiseSessionData = new Promise (function (resolve, reject) {
-					db.collection("SessionData").insertOne(placeholders.SessionData, null, function (error, result) {
-						if (error) {
-							console.log(`Error creating collection SessionData: ${error}`);
-						} else {
-							resolve();
-						}
+					var promiseSessionData = new Promise (function (resolve, reject) {
+						db.collection("SessionData").insertOne(placeholders.SessionData, null, function (error, result) {
+							if (error) {
+								console.log(`Error creating collection SessionData: ${error}`);
+							} else {
+								resolve();
+							}
+						});
 					});
-				});
-				var promiseAnnouncement = new Promise (function (resolve, reject) {
-					db.collection("Announcement").insertOne(placeholders.Announcement, null, function (error, result) {
-						if (error) {
-							console.log(`Error creating collection Announcement: ${error}`);
-						} else {
-							resolve();
-						}
+					var promiseAnnouncement = new Promise (function (resolve, reject) {
+						db.collection("Announcement").insertOne(placeholders.Announcement, null, function (error, result) {
+							if (error) {
+								console.log(`Error creating collection Announcement: ${error}`);
+							} else {
+								resolve();
+							}
+						});
 					});
-				});
 
-				promiseServerActivations.then((msg) => {
-					promiseMember.then((msg) => {
-						promiseMembershipData.then((msg) => {
-							promiseDoorCode.then((msg) => {
-								promiseClearanceLevel.then((msg) => {
-									promiseAbility.then((msg) => {
-										promiseSessionData.then((msg) => {
-											promiseAnnouncement.then((msg) => {
-												console.log("Setup Complete...");
-												endSession(db);
+					promiseServerActivations.then((msg) => {
+						promiseMember.then((msg) => {
+							promiseMembershipData.then((msg) => {
+								promiseDoorCode.then((msg) => {
+									promiseClearanceLevel.then((msg) => {
+										promiseAbility.then((msg) => {
+											promiseSessionData.then((msg) => {
+												promiseAnnouncement.then((msg) => {
+													console.log("Setup Complete...");
+													endSession(db);
+												});
 											});
 										});
 									});
@@ -202,18 +208,18 @@ mongo.connect(url, function (err, db) {
 							});
 						});
 					});
-				});
-				break;
+					break;
+				}
+			}
+		} else {
+			// Report error and end database connection
+			console.log(`Auth Failed: ${err}`);
+			if (db) {
+				endSession(db);
 			}
 		}
-	} else {
-		// Report error and end database connection
-		console.log(`Auth Failed: ${err}`);
-		if (db) {
-			endSession(db);
-		}
-	}
-});
+	});
+}
 // END Database Client
 
 
@@ -226,7 +232,7 @@ mongo.connect(url, function (err, db) {
 	@returns 	n/a
 	@details 	This function ends the MongoDB session by first logging out the authenticated user and closing the connection directly.
 */
-function endSession(mongoDatabase, callback) {
+function endSession (mongoDatabase, callback) {
 	console.log("Ending session...");
 
 	mongoDatabase.logout();
@@ -235,6 +241,22 @@ function endSession(mongoDatabase, callback) {
 	if (typeof callback === "function") {
 		callback();
 	}
+}
+
+/*
+	@function 	help
+	@parameter 	n/a
+	@returns 	n/a
+	@details 	This function prints a help prompt to console.
+*/
+function help () {
+	console.log("\nsce_db_setup_v0.js - The SCE Core-v4 MongoDB Schema Setup Script");
+	console.log("\nCommand Synopsis:");
+	console.log("\t\"node sce_db_setup_v0.js [option]\"");
+	console.log("\nOptions:");
+	console.log("\t--stat\n\t\tAcquires current MongoDB database statistics for the SCE database");
+	console.log("\t--init\n\t\tThe default behavior; initializes the database to the structure described by schema_v0.js");
+	console.log("\t--help\n\t\tRuns this help prompt");
 }
 // END Utility Functions
 
