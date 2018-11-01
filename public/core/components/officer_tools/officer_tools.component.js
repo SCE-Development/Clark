df80f0db199e2280df4614ac0f96f05e28b67c5a
Mian Hashim Shah <mian_hashim_shah@hotmail.com>
2018-11-01 15:49:44 -0700

    Integrating eventManager component (#42)
    Next steps and feedback from the Event Planning/PR team:
    Izzy:
    1) Run an initial email release to test out the sending capability.
    2) Integration of Google Calendar.
    2a) Set the time and date for Google Calendar. 
    2b) Set a reminder functionality and see if it reminds with the 'body' of the post.
    
    Hashim:
    1) UI change.
    1a) Type of event, margins, rename the post delete button to 'clear' button, change 'Publish' to 'Published', and add SCE logo.
    1b) The events tab on the home page.

--- public/core/components/officer_tools/officer_tools.component.js
+++ public/core/components/officer_tools/officer_tools.component.js
@@ -27,7 +27,8 @@ angular.module("officertools").component("officertools", {
 			"eodProfitForm": `https://goo.gl/vcBWma`,
 			"cashCountProcedures": `https://www.goo.gl/kA8yTE`,
 			"storeSlideshow": `https://docs.google.com/presentation/d/1iRopCuN8qT0e_easSX0t6StIwvqul-OgHZpkkA28g5o/edit#slide=id.p`,
-			"announcement": `https://sce.engr.sjsu.edu/wp-admin/edit.php`
+			"announcement": `localhost:3000/eventManager`
+			//"announcement": `https://sce.engr.sjsu.edu/wp-admin/edit.php`
 		};
 
 
