a3c1af61844d11c0bb4adbca945a63c7f7a78d84
Rolando Javier <rjavier441@gmail.com>
2018-11-16 18:29:46 -0800

    Updating local repo with latest commits

---
+++ public/core/js/dashboard_legacy.js
@@ -0,0 +1,34 @@
+//	PROJECT: 		Core-v4
+// 	Name: 			Rolando Javier
+// 	File: 			dashboard.js
+// 	Date Created: 	January 28, 2018
+// 	Last Modified: 	January 28, 2018
+// 	Details:
+// 					This file contains all front-end javascript for the dashboard.html admin dashboard.
+// 	Dependencies:
+// 					AngularJS 1.6.7
+"use strict"
+
+$(document).ready(init());
+
+// BEGIN init
+function init () {
+	console.log("You have entered the SCE Core v4 Admin Dashboard");
+	setDebug(true);	// have post() logging
+}
+// END init
+
+
+
+// BEGIN Angular
+// angular.module("adminDashboard",[]);
+// END Angular
+
+
+
+// BEGIN Utility Functions
+// END Utility Functions
+
+
+
+// END dashboard.js
