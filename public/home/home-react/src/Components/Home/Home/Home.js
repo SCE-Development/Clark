b36a96e9143ff8028dc5dd72dee471cba1e14bab
Bren Santos <santos.brenj@gmail.com>
2019-03-05 12:23:03 -0800

    Migrated benefits page into the homepage

--- public/home/home-react/src/Components/Home/Home/Home.js
+++ public/home/home-react/src/Components/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './Home.css';
 import Slideshow from '../Slideshow/Slideshow.js';
 import Footer from '../Footer/Footer.js';
+import Benefits from '../../Benefits/benefitsCode/App';
 
 class Home extends Component {
  render() {
    return (
      <div className="home">
-       <Slideshow/>
+       <Slideshow className="slideshow"/>
+       <Benefits className="benefits"/>
        <Footer/>
      </div>
    );
