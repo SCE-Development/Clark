d0c20426fff47b0d30b9732f9e378913f3d51d87
Rolando Javier <rjavier441@gmail.com>
2019-03-10 18:15:08 -0700

    Added preliminary fields and field capturing logic
    for React.

--- public/home/home-react/src/Components/MembershipApplication/membershipApplication.jsx
+++ public/home/home-react/src/Components/MembershipApplication/membershipApplication.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './membershipApplication.css';
 import { Col, Button, Form, FormGroup, Label, Input, TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, CardText, Row, Modal,ModalHeader , ModalFooter, ModalBody} from 'reactstrap';
 import classnames from 'classnames';
 
@@ -11,7 +12,30 @@ export default class MembershipApplication extends React.Component {
         super( props );
 
         // Set initial state
-        this.state = {};
+        this.state = {
+            firstName: "",
+            middleInitial: "",
+            lastName: "",
+            email: ""
+        };
+    }
+
+    // @function        mutateFirstName()
+    // @description     This function handles input field changes for
+    //                  the first name field
+    // @parameters      (event) e       the javascript event passed
+    //                                  to this handle when the input
+    //                                  changes.
+    // @returns         n/a
+    mutateFirstName( e ) {
+
+        // Create a copy of the current state
+        var tempState = Object.assign( this.state );
+
+        // Set the new state
+        tempState.firstName = e.target.value;
+        this.setState(tempState);
+        // console.log("Firstname:", tempState.firstName);
     }
 
     // @function        render()
@@ -21,8 +45,40 @@ export default class MembershipApplication extends React.Component {
     // @returns         (jsx) html      The generated html content 
     render() {
         return (
-            <div>
-                <h1>Testing</h1>
+            <div class="membership-application">
+                <h1 class="page-title">Member Registration</h1>
+                <div class="notice">
+                    <span class="critical">*</span><span class="important"> = This is a required field</span>
+                </div>
+                <Form class="page-form">
+                    <h3>General Information</h3>
+                    <FormGroup>
+                        <Label for="firstName">First Name*</Label>
+                        <Input type="text" onChange={this.mutateFirstName.bind(this)} value={this.state.firstName} name="firstName" id="input_firstName" placeholder="(e.g. John)"></Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="middleInitial">Middle Initial*</Label>
+                        <Input type="text" value={this.state.middleInitial} name="middleInitial" id="input_middleInitial" placeholder="(e.g. J)"></Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lastName">Last Name*</Label>
+                        <Input type="text" value={this.state.lastName} name="lastName" id="input_lastName" placeholder="(e.g. Doe)"></Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email*</Label>
+                        <Input type="email" value={this.state.email} name="email" id="input_email" placeholder="example@email.com"></Input>
+                    </FormGroup>
+
+                    <h3>Account Configuration</h3>
+                    <FormGroup>
+                        <Label for="username">Username*</Label>
+                        <Input type="text" name="username" id="input_username" placeholder="(e.g. sce_user)"></Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">Password*</Label>
+                        <Input type="text" name="password" id="input_password" placeholder="(e.g. sce_password)"></Input>
+                    </FormGroup>
+                </Form>
             </div>
         );
     }
