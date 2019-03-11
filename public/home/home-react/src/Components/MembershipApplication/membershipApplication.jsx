5313f74e10c63caab10d42de270aee562db2f2a8
Rolando Javier <rjavier441@gmail.com>
2019-03-10 18:46:20 -0700

    Adds request logic to pull the availability of a specific username.
    However, this requires supplementary APIs to promote secure search
    of usernames and info

--- public/home/home-react/src/Components/MembershipApplication/membershipApplication.jsx
+++ public/home/home-react/src/Components/MembershipApplication/membershipApplication.jsx
@@ -16,13 +16,15 @@ export default class MembershipApplication extends React.Component {
             firstName: "",
             middleInitial: "",
             lastName: "",
-            email: ""
+            email: "",
+            username: "",
+            password: ""
         };
     }
 
     // @function        mutateFirstName()
     // @description     This function handles input field changes for
-    //                  the first name field
+    //                  the aforementioned field
     // @parameters      (event) e       the javascript event passed
     //                                  to this handle when the input
     //                                  changes.
@@ -36,6 +38,111 @@ export default class MembershipApplication extends React.Component {
         tempState.firstName = e.target.value;
         this.setState(tempState);
         // console.log("Firstname:", tempState.firstName);
+    }
+
+    // @function        mutateMiddleInitial()
+    // @description     This function handles input field changes for
+    //                  the aforementioned field
+    // @parameters      (event) e       the javascript event passed
+    //                                  to this handle when the input
+    //                                  changes.
+    // @returns         n/a
+    mutateMiddleInitial( e ) {
+
+        // Create a copy of the current state
+        var tempState = Object.assign( this.state );
+
+        // Set the new state
+        tempState.middleInitial = e.target.value;
+        this.setState(tempState);
+    }
+    
+    // @function        mutateLastName()
+    // @description     This function handles input field changes for
+    //                  the aforementioned field
+    // @parameters      (event) e       the javascript event passed
+    //                                  to this handle when the input
+    //                                  changes.
+    // @returns         n/a
+    mutateLastName( e ) {
+
+        // Create a copy of the current state
+        var tempState = Object.assign( this.state );
+
+        // Set the new state
+        tempState.lastName = e.target.value;
+        this.setState(tempState);
+    }
+    
+    // @function        mutateEmail()
+    // @description     This function handles input field changes for
+    //                  the aforementioned field
+    // @parameters      (event) e       the javascript event passed
+    //                                  to this handle when the input
+    //                                  changes.
+    // @returns         n/a
+    mutateEmail( e ) {
+
+        // Create a copy of the current state
+        var tempState = Object.assign( this.state );
+
+        // Set the new state
+        tempState.email = e.target.value;
+        this.setState(tempState);
+    }
+    
+    // @function        mutateUsername()
+    // @description     This function handles input field changes for
+    //                  the aforementioned field
+    // @parameters      (event) e       the javascript event passed
+    //                                  to this handle when the input
+    //                                  changes.
+    // @returns         n/a
+    mutateUsername( e ) {
+
+        // Create a copy of the current state
+        var tempState = Object.assign( this.state );
+
+        // Set the new state
+        tempState.username = e.target.value;
+        this.setState(tempState);
+
+        // Check if the username is available by querying the server
+        var request = require( 'superagent' );
+        request.post('http://localhost:3000/api/user/search')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .send( {
+            'type': ''  // TODO: Create an API meant solely for checking if a username is available (making it safe for public exposure)
+        } )
+        .end( function( err, response ){
+            
+            if( response && response.status < 300 ){
+
+                // Success
+                // TODO: Check if a username was found
+            } else {
+                
+                // Failure
+                // TODO: Respond with error
+            }
+        } );
+    }
+    
+    // @function        mutatePassword()
+    // @description     This function handles input field changes for
+    //                  the aforementioned field
+    // @parameters      (event) e       the javascript event passed
+    //                                  to this handle when the input
+    //                                  changes.
+    // @returns         n/a
+    mutatePassword( e ) {
+
+        // Create a copy of the current state
+        var tempState = Object.assign( this.state );
+
+        // Set the new state
+        tempState.password = e.target.value;
+        this.setState(tempState);
     }
 
     // @function        render()
@@ -58,25 +165,25 @@ export default class MembershipApplication extends React.Component {
                     </FormGroup>
                     <FormGroup>
                         <Label for="middleInitial">Middle Initial*</Label>
-                        <Input type="text" value={this.state.middleInitial} name="middleInitial" id="input_middleInitial" placeholder="(e.g. J)"></Input>
+                        <Input type="text" onChange={this.mutateMiddleInitial.bind(this)} value={this.state.middleInitial} name="middleInitial" id="input_middleInitial" placeholder="(e.g. J)"></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label for="lastName">Last Name*</Label>
-                        <Input type="text" value={this.state.lastName} name="lastName" id="input_lastName" placeholder="(e.g. Doe)"></Input>
+                        <Input type="text" onChange={this.mutateLastName.bind(this)} value={this.state.lastName} name="lastName" id="input_lastName" placeholder="(e.g. Doe)"></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label for="email">Email*</Label>
-                        <Input type="email" value={this.state.email} name="email" id="input_email" placeholder="example@email.com"></Input>
+                        <Input type="email" onChange={this.mutateEmail.bind(this)} value={this.state.email} name="email" id="input_email" placeholder="example@email.com"></Input>
                     </FormGroup>
 
                     <h3>Account Configuration</h3>
                     <FormGroup>
                         <Label for="username">Username*</Label>
-                        <Input type="text" name="username" id="input_username" placeholder="(e.g. sce_user)"></Input>
+                        <Input type="text" onChange={this.mutateUsername.bind(this)} value={this.state.username} name="username" id="input_username" placeholder="(e.g. sce_user)"></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label for="password">Password*</Label>
-                        <Input type="text" name="password" id="input_password" placeholder="(e.g. sce_password)"></Input>
+                        <Input type="text" onChange={this.mutatePassword.bind(this)} value={this.state.password} name="password" id="input_password" placeholder="(e.g. sce_password)"></Input>
                     </FormGroup>
                 </Form>
             </div>
