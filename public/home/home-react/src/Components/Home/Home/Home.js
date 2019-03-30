b0ae29343a207af4e3664c545823b1afcc67bf15
pranavpatilsce <ompranav97@gmail.com>
2019-03-29 23:19:30 -0700

    Done with calendar

--- public/home/home-react/src/Components/Home/Home/Home.js
+++ public/home/home-react/src/Components/Home/Home/Home.js
@@ -24,25 +24,28 @@ import Iframe from 'react-iframe';
 class Home extends Component {
  render() {
    return (
-     <div className="home">
+      <div className="home">
        <Slideshow className="slideshow"/>
        <Jumbotron/>
 
-		<h1 className="text-center"><h1 className="display-4">SCE Events Calendar</h1> </h1>	
-		<p className="lead" className="text-center"> Add SCE Calendar to your own by clicking the icon on the bottom right side!</p>
+      <h1 className="text-center"><h1 className="display-4">SCE Events Calendar</h1> </h1>	
+      <p className="lead" className="text-center"> Add SCE Calendar to your own by clicking the Google Calendar icon on the bottom right side!</p>
 
-		<Iframe url="https://calendar.google.com/calendar/b/4/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=llv828585faitko1m2nh39s3js%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FLos_Angeles"
-		width="1000px"
-		height="800px"
-		id="myId"
-		className="myClassname"
-		display="initial"
-		position="relative"
-		allowFullScreen/> 
-
+      <div class="outer-div"> 
+          <div class="inner-div">
+              <Iframe url="https://calendar.google.com/calendar/b/4/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=llv828585faitko1m2nh39s3js%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FLos_Angeles"
+              width="1000px"
+              height="800px"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"
+              allowFullScreen/> 
+          </div>
+      </div>
        <Benefits className="benefits"/>
        <Footer/>
-     </div>
+      </div>
    );
  }
 }
