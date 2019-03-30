2b962a946ce82ca8b5463efd741a3a47e8b7db2e
pranavpatilsce <ompranav97@gmail.com>
2019-03-29 19:34:59 -0700

    added the team pics section and a sample card.

--- public/home/home-react/src/Components/TheTeam/App.js
+++ public/home/home-react/src/Components/TheTeam/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col, Media, Card, CardBody, Button, CardTitle, CardText, CardImg } from 'reactstrap';
 import classnames from 'classnames';
+import Ari from './teamPics/2018_2019/arisKoumis.png';
+import Evan from './teamPics/2018_2019/evanUgarte.png';
+import Pranav from './teamPics/2018_2019/pranavPatil.png';
+import Keven from './teamPics/2018_2019/kevenGallegos.png';
+import Alisha from './teamPics/2018_2019/alishaMehndiratta.png';
+import Thenu from './teamPics/2018_2019/thenuSenthil.png';
+import DRP from './teamPics/2018_2019/DRP.png';
+import Lorena from './teamPics/2018_2019/lorenaSilva.png';
+import Nick from './teamPics/2018_2019/nickDerry.png';
 
 export default class Example extends React.Component {
   constructor(props) {
@@ -19,6 +28,7 @@ export default class Example extends React.Component {
       });
     }
   }
+  //                
   render() {
     return (
       <div>
@@ -44,14 +54,25 @@ export default class Example extends React.Component {
         <TabContent activeTab={this.state.activeTab}>
           <TabPane tabId="1">
             <Row>
-              <Col sm="6">
+              <Col sm="4">
                 <Card body>
+                  <Media>
+                    <Media left href="#ArisKoumis">
+                      <CardImg height="250" width="250" src={Ari}/>
+                    </Media>
+                    <Media body>
+                      <Media heading>
+                        Aris Koumis
+                      </Media>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </Media>
+                  </Media>
                   <CardTitle>Special Title Treatment</CardTitle>
                   <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
                   <Button>Go somewhere</Button>
                 </Card>
               </Col>
-              <Col sm="6">
+              <Col sm="4">
                 <Card body>
                   <CardTitle>Special Title Treatment</CardTitle>
                   <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
