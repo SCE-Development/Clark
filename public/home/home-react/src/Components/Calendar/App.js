9bf7b49022ef78938e2819c183018b4b26adb9f8
Pranav Patil <ompranav97@gmail.com>
2019-03-30 14:01:03 -0700

    Pranav/dev latest (#59)
    * adding microsoft imagine
    
    * Minor UI fix
    
    * adding the lab kits page
    
    * the 124 LabKits is done
    
    * Took out trash
    
    * Added Khalil's SJTwo link. CMPE 124 kits code is there (if needed in future) but commented out.
    
    * Added the about section
    
    * Adding the team profiles.
    
    * added the team pics section and a sample card.
    
    * Adding the Calendar
    
    * Done with calendar
    
    * added the in progress on json for 3Dprinting

---
+++ public/home/home-react/src/Components/Calendar/App.js
@@ -0,0 +1,11 @@
+import React, { Component, Button } from 'react';
+import Iframe from 'react-iframe'
+
+const MyCalendar = props => (
+	
+  <div>
+    <iframe src="https://calendar.google.com/calendar/embed?src=llv828585faitko1m2nh39s3js%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="2400" height="600" frameborder="0" scrolling="no"></iframe>
+  </div>
+)
+
+export default MyCalendar;
