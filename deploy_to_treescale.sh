0bb57ac0f02e0f3603f40589147205fa4e9ce870
Hong Joon Chew <hchew@proofpoint.com>
2019-04-23 15:20:06 -0700

    Typo in .travis.yml, added comments on deploy_to_treescale and .travis.yml

--- deploy_to_treescale.sh
+++ deploy_to_treescale.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Push to TreeScale, env vars are found in travis-ci.com as secret env vars.
 echo "Logging in to TreeScale" | docker login repo.treescale.com -u "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 docker push repo.treescale.com/devscesjsu/sce-core-backend-dev:staging
 docker push repo.treescale.com/devscesjsu/sce-core-frontend-dev:staging
