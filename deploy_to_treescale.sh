08641f7bfa50d03e97e16d6b7308ca6d09a92553
Hong Joon Chew <hchew@proofpoint.com>
2019-04-23 16:29:44 -0700

    Moved docker login to .travis.yml

--- deploy_to_treescale.sh
+++ deploy_to_treescale.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # Push to TreeScale, env vars are found in travis-ci.com as secret env vars.
-echo "$DOCKER_PASSWORD" | docker login repo.treescale.com -u "$DOCKER_USERNAME" --password-stdin
 docker push repo.treescale.com/devscesjsu/sce-core-backend-dev:staging
 docker push repo.treescale.com/devscesjsu/sce-core-frontend-dev:staging
