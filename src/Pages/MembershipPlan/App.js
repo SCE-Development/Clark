036040f9648d587ff12b30da99e65d1aaf8a0d5c
aashrav <aashrav.shetty01@gmail.com>
2019-10-23 09:45:12 -0700

    changed css again

--- src/Pages/MembershipPlan/App.js
+++ src/Pages/MembershipPlan/App.js
@@ -47,9 +47,9 @@ componentDidMount(){
 }
  makeCard(title, img){
 
-  return <div className = "card2" id = {title} onClick ={this.cardSelected.bind(this,title)} >
+  return <div className = "card" id = {title} onClick ={this.cardSelected.bind(this,title)} >
       <img className = "img" src = {img} ></img>
-      <div className ='card2-body'>
+      <div className ='card-body'>
         <h4 style = {{alignSelf:'left'}}>{title}</h4>
         {this.getBody(title)}  
       </div>
