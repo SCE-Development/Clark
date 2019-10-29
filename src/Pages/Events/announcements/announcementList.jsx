610438dd18fc722c549b47cebc71b36df1ee7fca
Habib <has.sorathia@gmail.com>
2019-10-29 13:46:48 -0700

    added modal functionality

--- src/Pages/Events/announcements/announcementList.jsx
+++ src/Pages/Events/announcements/announcementList.jsx
@@ -1,79 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Container,
     Row,
     Col,
-    Card
+    Card,
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
 } from "reactstrap";
-// import EventModal from "./eventModal"
+
 import "./announcementPage.css"
 
-class AnnouncementList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            events: [
-                {
-                    title: "Git Tutorial",
-                    location: "ENG 292",
-                    description: "boom",
-                    date: "09/03/1999",
-                    time: "4:30 PM"
 
-                },
-                {
-                    title: "ROS MASTERS UNITED",
-                    location: "ENG 294",
-                    description: "boom",
-                    date: "09/03/1999",
-                    time: "4:30 PM"
 
-                },
-                {
-                    title: "Big brain time",
-                    location: "ENG 292",
-                    description: "boom",
-                    date: "09/03/1999",
-                    time: "4:30 PM"
+const AnnouncementList = () => {
 
-                },
-                {
-                    title: "Burn down the entire school",
-                    location: "hahaha",
-                    description: "boom asdjfhakjsdfhlkasdhfkajhsdlfkjhaskldjfh" 
+    let state = {
+        // currentEvent: null,
+        events: [
+            {
+                title: "Git Tutorial",
+                location: "ENG 292",
+                description: "boom",
+                date: "09/03/1999",
+                time: "4:30 PM"
+
+            },
+            {
+                title: "ROS MASTERS UNITED",
+                location: "ENG 294",
+                description: "boom",
+                date: "09/03/1999",
+                time: "4:30 PM"
+
+            },
+            {
+                title: "Big brain time",
+                location: "ENG 292",
+                description: "boom",
+                date: "09/03/1999",
+                time: "4:30 PM"
+
+            },
+            {
+                title: "Burn down the entire school",
+                location: "hahaha",
+                description: "boom asdjfhakjsdfhlkasdhfkajhsdlfkjhaskldjfh"
                     + "jshdflkjahslkdfjhalk",
-                    date: "09/03/1999",
-                    time: "4:30 PM"
+                date: "09/03/1999",
+                time: "4:30 PM"
 
-                },
-                {
-                    title: "china tour && die forever",
-                    location: "Dubai",
-                    description: "boom",
-                    date: "09/03/1999",
-                    time: "4:30 PM"
+            },
+            {
+                title: "china tour && die forever",
+                location: "Dubai",
+                description: "boom",
+                date: "09/03/1999",
+                time: "4:30 PM"
 
 
-                },
+            },
 
-            ]
-        }
+        ]
+
+
     }
-    getModal = () => {
-        return this.getModal;
+
+    const [modal, setModal] = useState(false);
+    // getModal = () => {
+    //     console.log("we in this");
+    //     return <Modal name="habib" open={true} />;
+    // }
+    const [currentEvent, setEvent] = useState();
+    const toggle = () => {
+        setModal(!modal)
     }
-    render() {
-        // console.log(event.description.substring(0,19))
-        return (
-            <Container>
-                {this.state.events.map((event) => {
+
+    // console.log(event.description.substring(0,19))
+    return (
+        <Container>
+            {currentEvent == null ? <p /> :
+                <Modal isOpen={modal} toggle={toggle}>
+                    <ModalHeader toggle={toggle}>{currentEvent.title}</ModalHeader>
+                    <ModalBody>
+                        {currentEvent.description}
+                    </ModalBody>
+                    <ModalFooter>
+                        Date: {currentEvent.date} {" "}
+                        Location: {currentEvent.location} {" "}
+                        Time: {currentEvent.time}{" "}
+
+                        <Button color="primary" onClick={toggle}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+            }
+            {
+                state.events.map((event, index) => {
                     return (
-                        
-                            
-                            <Card className="lordie"> 
-                            {/* onClick={() => {
-                                console.log("go to hell")
-                            }} */}
+
+                        <div key={index} onClick={() => {
+                            console.log(event.title)
+                            setEvent(event)
+                            toggle()
+                        }} >
+
+                            <Card className="cards">
+
                                 <Row>
                                     <Col>
                                         <h1>{event.title}</h1>
@@ -81,20 +115,20 @@ class AnnouncementList extends React.Component {
                                 </Row>
                                 <Row>
                                     <Col>
-                                
-                                        <b>Description:</b> 
-                                        {event.description.length > 60 ? 
-                                        event.description.substring(0, 59) + 
-                                        "..." : event.description }
+
+                                        <b>Description:</b>
+                                        {event.description.length > 60 ?
+                                            event.description.substring(0, 59) +
+                                            "..." : event.description}
                                     </Col>
-                                    
+
                                 </Row>
                                 <Row>
                                     <Col>
                                         <b>Date:</b> {event.date}
-    
+
                                     </Col>
-                                    <Col xd = "1">
+                                    <Col xd="1">
                                         <b>Location:</b> {event.location}
                                     </Col>
                                     <Col>
@@ -102,13 +136,13 @@ class AnnouncementList extends React.Component {
                                     </Col>
                                 </Row>
                             </Card>
-                        
+                        </div>
                     );
                 })
-                }
-            </Container>
-        );
-    }
+            }
+        </Container>
+    );
 }
+
 
 export default AnnouncementList;
--- src/Pages/Events/announcements/announcementPage.jsx
+++ src/Pages/Events/announcements/announcementPage.jsx
@@ -1,26 +1,6 @@
 import React from "react";
-import { Card, CardTitle, Col, CardText } from "reactstrap";
 import "./announcementPage.css";
 import AnnouncementList from "./announcementList";
-import Layout from "../../../Components/Layout/Layout";
-import {
-  // NavbarBrand,
-  // ListGroup,
-  // ListGroupItem,
-  // ListGroupItemHeading,
-  // ListGroupItemText
-  Jumbotron,
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  FormGroup,
-  Label,
-  Input
-} from "reactstrap";
-// import axios from 'axios';
-// import NavBarTop from "./navBarTop.jsx";
 
 export default class AnnouncementsPage extends React.Component {
   constructor(props) {
@@ -30,28 +10,6 @@ export default class AnnouncementsPage extends React.Component {
     console.log("Local storage:");
     console.dir(window.localStorage);
 
-    this.state = {
-      publishedEvents: [
-        {
-          eventName: "SJSU Eng conference1",
-          eventDescription: "blah",
-          eventDate: "10/10/10",
-          eventTime: "10pm",
-          eventLocation: "SJSU",
-          eventHost: "Hashim",
-          eventContact: "you"
-        },
-        {
-          eventName: "SJSU Eng conference2",
-          eventDescription: "blah",
-          eventDate: "10/10/10",
-          eventTime: "10pm",
-          eventLocation: "SJSU",
-          eventHost: "Hashim",
-          eventContact: "you"
-        }
-      ]
-    };
   }
 
   //first load when page start
