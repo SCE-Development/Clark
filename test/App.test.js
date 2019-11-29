1e2534ccc087c52fa5febf49600c18e2156eeab9
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2019-11-28 20:41:40 -0800

    Added Enzyme for React Component Testing (#112)
    * added testing dependencies
    
    * Moved contents of index.js to App.js
    
    * Added new index.js file to render contents of App
    
    * Added babelrc file and first enzyme test
    
    * Changed npm scripts for api and frontend testing

---
+++ test/App.test.js
@@ -0,0 +1,19 @@
+/* global describe it */
+import 'jsdom-global/register'
+import React from 'react'
+import Enzyme, { mount } from 'enzyme'
+import { expect } from 'chai'
+
+import App from '../src/App'
+import Adapter from 'enzyme-adapter-react-16'
+import { Router } from 'react-router-dom'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<App />', () => {
+  it('Should render a <Router /> component with one child', () => {
+    const wrapper = mount(<App />)
+    expect(wrapper.find(Router)).to.have.lengthOf(1)
+    expect(wrapper.find(Router).children()).to.have.lengthOf(1)
+  })
+})
