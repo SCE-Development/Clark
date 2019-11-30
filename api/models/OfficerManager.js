26b1f44e252375692b7b6378a9f1b6f6dd154843
ThaiQ <quachhongthai200@gmail.com>
2019-11-29 20:39:15 -0800

    added api model and routing for the Officers Manager tool

---
+++ api/models/OfficerManager.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const OfficersSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    facebook: {
+      type: String,
+      required: false
+    },
+    github: {
+      type: String,
+      required: false
+    },
+    instagram: {
+      type: String,
+      required: false
+    },
+    // The access level is defined as follows:
+    // -2: Ban
+    // -1: Pending
+    // 0: Member
+    // 1: Officer
+    // 2: Admin
+    level: {
+      type: Number,
+      require: true
+    },
+    team: {
+      type: String,
+      require: true
+    },
+    quote: {
+      type: String,
+      require: false
+    },
+    major: {
+      type: String,
+      require: true
+    }
+  },
+  { collection: 'Officers' }
+)
+
+module.exports = mongoose.model('Officers', OfficersSchema)
