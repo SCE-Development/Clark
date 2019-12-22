6bd0b0323c50c92a40c3a60d52cf5bbc34943297
ThaiQ <quachhongthai200@gmail.com>
2019-12-21 20:32:48 -0800

    fixing commenting conventions for frontend test cases

--- test/frontend/OfficerDB.test.js
+++ test/frontend/OfficerDB.test.js
@@ -10,13 +10,13 @@ import { Table } from 'reactstrap'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe('Officer Manager: <Table />', () => {
+describe('<Table />', () => {
   it('Should render a <Table /> component with one child', () => {
     const wrapper = mount(<OfficerManager />)
     expect(wrapper.find(Table)).to.have.lengthOf(1)
   })
 
-  it('Should render a <tbody /> component with 2 child', () => {
+  it('Should render a <tbody /> component with 2 children', () => {
     const wrapper = mount(<OfficerManager />)
     expect(wrapper.find('tbody').children()).to.have.lengthOf(2)
   })
