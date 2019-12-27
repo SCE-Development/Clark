2b52aaf00b85a5b88f0d7587793e4305bb9c0b83
ThaiQ <quachhongthai200@gmail.com>
2019-12-26 16:02:02 -0800

    New modules

--- src/Enums.js
+++ src/Enums.js
@@ -6,10 +6,26 @@ export const membershipStatus = [
   { key: 'Admin', value: 2 }
 ]
 
-export function getKey (enums, value) {
-  return enums.filter(e => e.value === value)[0].key
+export function getKey (enumArray, value) {
+  return enumArray.filter(e => e.value === value)[0].key
 }
 
-export function getValue (enums, key) {
-  return enums.filter(e => e.key === key)[0].value
+export function getValue (enumArray, key) {
+  return enumArray.filter(e => e.key === key)[0].value
+}
+
+export function getAllKeys (enumArray) {
+  const array = []
+  enumArray.forEach(e => {
+    array.push(e.key)
+  })
+  return array
+}
+
+export function getAllValues (enumArray) {
+  const array = []
+  enumArray.forEach(e => {
+    array.push(e.value)
+  })
+  return array
 }
