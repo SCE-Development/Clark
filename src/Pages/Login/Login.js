2ac7e764af8ba321f81f6cd4208c0c281ecfec85
Evan Ugarte <evanuxd@gmail.com>
2019-12-27 14:17:26 -0800

    Made Login Component Functional
    Now use hooks to simplify the code and reduce lines. I also took the Inputs present on the page and
    moved them to it's own components.

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,25 +1,16 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Layout from '../../Components/Layout/Layout'
+import LoginInput from './LoginInput'
 import './Login.css'
 
-export default class extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      email: '',
-      password: '',
-      message: '',
-      AnimationCSS1: {},
-      AnimationCSS2: {}
-    }
+export default function Login (props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  componentDidMount () {
+  function checkToken () {
     const token = window.localStorage
       ? window.localStorage.getItem('jwtToken')
       : ''
@@ -31,32 +22,25 @@ export default class extends Component {
     }
   }
 
-  handleChange (e) {
-    const state = { ...this.state }
-    state[e.target.name] = e.target.value
-    this.setState(state)
-  }
+  useEffect(() => {
+    checkToken()
+    // eslint-disable-next-line
+  }, [])
 
-  handleSubmit (e) {
+  function handleSubmit (e) {
     e.preventDefault()
-
-    const { email, password } = this.state
-
     axios
       .post('/api/user/login', { email, password })
       .then(result => {
         window.localStorage.setItem('jwtToken', result.data.token)
-        this.updateLastLoginDate(email, result.data.token)
-        this.setState({ message: '' })
+        updateLastLoginDate(result.data.token)
       })
       .catch(error => {
-        this.setState({
-          message: error.response.data.message
-        })
+        setErrorMsg(error.response.data.message)
       })
   }
 
-  updateLastLoginDate (email, token) {
+  function updateLastLoginDate (token) {
     axios
       .post('/api/user/edit', {
         queryEmail: email,
@@ -65,105 +49,43 @@ export default class extends Component {
         token: token
       })
       .then(() => {
-        this.props.history.push('/dashboard')
+        props.history.push('/dashboard')
       })
       .catch(err => {
         console.log(err)
       })
   }
 
-  updateCSS (opt) {
-    opt === 1
-      ? this.setState({
-        AnimationCSS1: {
-          top: '-5px',
-          borderBottom: '2px solid GREEN'
-        }
-      })
-      : this.setState({ AnimationCSS1: {} })
+  const fields = [
+    {
+      type: 'email',
+      placeholder: 'Email',
+      handleChange: e => setEmail(e.target.value)
+    },
+    {
+      type: 'password',
+      placeholder: 'Password',
+      handleChange: e => setPassword(e.target.value)
+    }
+  ]
 
-    opt === 2
-      ? this.setState({
-        AnimationCSS2: {
-          top: '-5px',
-          borderBottom: '2px solid GREEN'
-        }
-      })
-      : this.setState({ AnimationCSS2: {} })
-  }
-
-  inputBox (field) {
-    return (
-      <div
-        style={field.style}
-        onFocus={() => {
-          this.updateCSS(field.css)
-        }}
-        onBlur={() => {
-          this.updateCSS()
-        }}
-        className='txtb'
-      >
-        <span />
-        <input
-          type={field.type}
-          name={field.type}
-          placeholder={field.placeholder}
-          value={field.value}
-          onChange={this.handleChange}
-          required
-        />
-      </div>
-    )
-  }
-
-  render () {
-    const {
-      message,
-      email,
-      password,
-      AnimationCSS1,
-      AnimationCSS2
-    } = this.state
-    const fields = [
-      {
-        style: AnimationCSS1,
-        css: 1,
-        value: email,
-        type: 'email',
-        placeholder: 'Email'
-      },
-      {
-        style: AnimationCSS2,
-        css: 2,
-        value: password,
-        type: 'password',
-        placeholder: 'Password'
-      }
-    ]
-
-    return (
-      <Layout>
-        <form onSubmit={this.handleSubmit}>
-          <div id='body'>
-            <img id='img' alt='sce logo' src='images/SCE-glow.png' />
-
-            {message !== '' && <span>{message}</span>}
-
-            {fields.map(field => {
-              return this.inputBox(field)
-            })}
-
-            <button type='submit' id='loginBtn'>
-              Login
-            </button>
-
-            <p id='SignUp'>
-              <Link to='/register'>Create an account</Link>
-            </p>
-          </div>
-        </form>
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <form onSubmit={handleSubmit}>
+        <div id='body'>
+          <img id='img' alt='sce logo' src='images/SCE-glow.png' />
+          {errorMsg && <span>{errorMsg}</span>}
+          {fields.map((field, index) => {
+            return <LoginInput key={index} field={field} />
+          })}
+          <button type='submit' id='loginBtn'>
+            Login
+          </button>
+          <p id='SignUp'>
+            <Link to='/register'>Create an account</Link>
+          </p>
+        </div>
+      </form>
+    </Layout>
+  )
 }
