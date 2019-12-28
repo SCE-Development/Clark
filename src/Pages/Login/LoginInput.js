67eaab2bcdb69d1015e2328030cf1246e1f4a780
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2019-12-27 21:59:46 -0800

    Made Login Component Functional (#173)
    Now use hooks to simplify the code and reduce lines. I also took the Inputs present on the page and
    moved them to it's own components.

---
+++ src/Pages/Login/LoginInput.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+export default function InputBox (props) {
+  const [style, setStyle] = useState({})
+  const animatedCSS = {
+    top: '-5px',
+    borderBottom: '2px solid GREEN'
+  }
+
+  return (
+    <div
+      style={style}
+      onFocus={() => {
+        setStyle(animatedCSS)
+      }}
+      onBlur={() => {
+        setStyle({})
+      }}
+      className='txtb'
+    >
+      <span />
+      <input
+        type={props.field.type}
+        name={props.field.type}
+        placeholder={props.field.placeholder}
+        value={props.field.value}
+        onChange={props.field.handleChange}
+        required
+      />
+    </div>
+  )
+}
