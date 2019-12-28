5d7f113903fe5e5a0da06fef63f27aedc1f44b27
ThaiQ <quachhongthai200@gmail.com>
2019-12-27 18:55:11 -0800

    remove test

--- test/frontend/MemberDashboard.test.js
+++ test/frontend/MemberDashboard.test.js
@@ -3,7 +3,6 @@ import 'jsdom-global/register'
 import React from 'react'
 import Enzyme, { mount } from 'enzyme'
 import { expect } from 'chai'
-import { Modal } from 'reactstrap'
 
 import MemberDashboard from '../../src/Pages/Overview/App.js'
 import Adapter from 'enzyme-adapter-react-16'
@@ -26,10 +25,5 @@ describe('<MemberDashboard />', () => {
   it('Should render a <tbody /> component with one child', () => {
     const wrapper = mount(<MemberDashboard />)
     expect(wrapper.find('tbody')).to.have.lengthOf(1)
-  })
-
-  it('Should render a <Modal /> component with 2 children', () => {
-    const wrapper = mount(<MemberDashboard />)
-    expect(wrapper.find(Modal)).to.have.lengthOf(2)
   })
 })
