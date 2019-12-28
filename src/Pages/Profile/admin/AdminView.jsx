d871211866aff7a77722d56714558241fc456474
ThaiQ <quachhongthai200@gmail.com>
2019-12-27 19:20:45 -0800

    refactor AdminView

--- src/Pages/Profile/admin/AdminView.jsx
+++ src/Pages/Profile/admin/AdminView.jsx
@@ -12,12 +12,11 @@ import {
   Label,
   ModalBody,
   ModalFooter,
-  ModalHeader
 } from "reactstrap";
 import axios from 'axios'
+import Display from './Profile.js'
 const bcrypt = require('bcrypt-nodejs')
-const enums = require( '../../../Enums' )
-const display = require ('./profile.js')
+const enums = require('../../../Enums')
 
 export default function Editor(props) {
   // first name, last name, middle initial, email, pass, door code
@@ -27,7 +26,7 @@ export default function Editor(props) {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [doorCode, setDoorCode] = useState("")
-  const [major,setMajor] = useState("")
+  const [major, setMajor] = useState("")
   const [user, setUser] = useState({ ...props.user })
   const [toggle, setToggle] = useState(false)
   const [pagesPrinted, setPagesPrinted] = useState(user.pagesPrinted)
@@ -45,12 +44,12 @@ export default function Editor(props) {
 
     //hash pass
     const salt = bcrypt.genSaltSync(10)
-    const hashed = (password.trim()==='') ?
-    user.password : bcrypt.hashSync(password, salt)
+    const hashed = (password.trim() === '') ?
+      user.password : bcrypt.hashSync(password, salt)
 
     //Change term?
-    numberOfSemestersToSignUpFor===0 &&
-    setNumberOfSemestersToSignUpFor(undefined)
+    numberOfSemestersToSignUpFor === 0 &&
+      setNumberOfSemestersToSignUpFor(undefined)
 
     const editedUser = {
       ...user,
@@ -66,7 +65,7 @@ export default function Editor(props) {
       numberOfSemestersToSignUpFor: numberOfSemestersToSignUpFor
     }
 
-    setUser({...editedUser})
+    setUser({ ...editedUser })
     setToggle(!toggle)
 
     await axios
@@ -111,182 +110,174 @@ export default function Editor(props) {
       })
   }
 
-  function editModalButton() {
-    let formGroups = [
-      {
-        label: 'First Name',
-        placeholder: user.firstName,
-        onChange: (e) => setFirstName(e.target.value)
-      },
-      {
-        label: 'Last Name',
-        placeholder: user.lastName,
-        onChange: (e) => setLastName(e.target.value)
-      },
-      {
-        label: 'Middle Initial',
-        placeholder: user.middleInitial,
-        onChange: (e) => setMiddleInitial(e.target.value)
-      },
-      {
-        label: 'Email',
-        type: 'email',
-        placeholder: user.email,
-        onChange: (e) => setEmail(e.target.value)
-      },
-      {
-        label: 'Password',
-        type: 'password',
-        placeholder: 'make it secure',
-        onChange: (e) => setPassword(e.target.value)
-      },
-      {
-        label: 'Door Code',
-        placeholder: 'make it secure',
-        onChange: (e) => setDoorCode(e.target.value)
-      },
-      {
-        label: 'Major',
-        placeholder: user.major,
-        onChange: (e) => setMajor(e.target.value)
-      }
-    ]
+  const formGroups = [
+    {
+      label: 'First Name',
+      placeholder: user.firstName,
+      onChange: (e) => setFirstName(e.target.value)
+    },
+    {
+      label: 'Last Name',
+      placeholder: user.lastName,
+      onChange: (e) => setLastName(e.target.value)
+    },
+    {
+      label: 'Middle Initial',
+      placeholder: user.middleInitial,
+      onChange: (e) => setMiddleInitial(e.target.value)
+    },
+    {
+      label: 'Email',
+      type: 'email',
+      placeholder: user.email,
+      onChange: (e) => setEmail(e.target.value)
+    },
+    {
+      label: 'Password',
+      type: 'password',
+      placeholder: 'make it secure',
+      onChange: (e) => setPassword(e.target.value)
+    },
+    {
+      label: 'Door Code',
+      placeholder: 'make it secure',
+      onChange: (e) => setDoorCode(e.target.value)
+    },
+    {
+      label: 'Major',
+      placeholder: user.major,
+      onChange: (e) => setMajor(e.target.value)
+    }
+  ]
 
-    return (
-      <div>
-        <div>
-          <Button
-            color="primary"
-            style={{
-              position: 'relative',
-              left: '80%'
-            }}
-            onClick={() => { handleToggle() }}>
-            Edit
-          </Button>
-        </div>
-
-        <Modal isOpen={toggle}>
-          <ModalHeader>Modal title</ModalHeader>
-          <ModalBody>
-            <Form>
-
-              {formGroups.map((group, index) => {
-                return <FormGroup key={index}>
-                  <Label>{group.label}</Label>
-                  <Input
-                    type={group.type || 'email'}
-                    name={group.type}
-                    placeholder={group.placeholder}
-                    onChange={group.onChange}
-                  />
-                </FormGroup>
-              })}
-
-              Change expiration date to
-              <select onChange={(e)=>{setNumberOfSemestersToSignUpFor(e.target.value)}}>
-                {
-                  [{value:0, name:'Keep Same'},
-                  {value:0, name:'Expired Membership'},
-                  {value:1, name:'This semester'},
-                  {value:2, name:'2 semesters'}
-                ].map((ele, ind)=>{
-                return <option key ={ind} value={ele.value}>{ele.name}</option>
-                })}
-              </select>
-
-              <Button
-                type='button'
-                onClick={() => {
-                  setPagesPrinted(0);
-                }}
-                color="info"
-                style={{marginTop:'5px'}}>
-                Reset Pages!
-              </Button>
-
-              <FormGroup tag="fieldset">
-                <legend>Membership Status</legend>
-                {enums.getAllValues(enums.membershipStatus).map((membership, index) => {
-                  return <FormGroup check key={index}>
-                    <Label check>
-                      <Input
-                        type="radio"
-                        name="radio1"
-                        value={membership}
-                        onChange={() => {
-                          setuserMembership(membership)
-                        }}
-                      />
-                      {enums.getKey(enums.membershipStatus,membership)}
-                    </Label>
-                  </FormGroup>
-                })}
-              </FormGroup>
-
-            </Form>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button
-              color="primary"
-              onClick={() => { handleSubmissionToggle() }}>
-              Submit
-            </Button>
-            <Button
-              color="secondary"
-              onClick={() => { handleToggle() }}>
-              Cancel
-            </Button>
-          </ModalFooter>
-
-        </Modal>
-      </div>
-    );
-  }
+  const membership = [
+    { value: 0, name: 'Keep Same' },
+    { value: 0, name: 'Expired Membership' },
+    { value: 1, name: 'This semester' },
+    { value: 2, name: '2 semesters' }
+  ]
 
   return (
-    <div>
-      <div>
-        <div className="center">
-          <ul className="profileInfo">
-            <Container>
-              <img
-                alt='profile'
-                style={{ height: '300px' }}
-                src='images/SCE-glow.png' />
-            </Container>
+    <div className="center">
+      <ul className="profileInfo">
+        <Container>
+          <img
+            alt='profile'
+            style={{ height: '300px' }}
+            src='images/SCE-glow.png' />
+        </Container>
 
-            {display.displayProfile(user, membershipValidUntil)}
+        <Display user={user} membershipValidUntil={membershipValidUntil} />
 
-            <Row>
-              <Col>{editModalButton()}</Col>
-            </Row>
-
-            <Modal
-              style={
-                {
-                  marginTop: '320px',
-                }}
-              isOpen={toggleSubmit}>
+        <Row>
+          <Col>
+            <div>
               <Button
-                onClick={async () => { await handleSubmission() }}
-                color="primary">
-                YES!
-                </Button>
-              <Button
+                color="primary"
                 style={{
-                  marginTop: '10px'
+                  position: 'relative',
+                  left: '80%'
                 }}
-                onClick={() => handleSubmissionToggle()}
-                color="danger">
-                Nah! It's a mistake.
+                onClick={() => { handleToggle() }}>
+                Edit
                 </Button>
-            </Modal>
+            </div>
 
-          </ul>
-        </div>
-      </div>
+            <Modal isOpen={toggle}>
+              <ModalBody>
+                <Form>
+
+                  {formGroups.map((group, index) =>
+                    <FormGroup key={index}>
+                      <Label>{group.label}</Label>
+                      <Input
+                        type={group.type || 'email'}
+                        name={group.type}
+                        placeholder={group.placeholder}
+                        onChange={group.onChange}
+                      />
+                    </FormGroup>
+                  )}
+
+                  Change expiration date to
+                  <select onChange={(e) => { setNumberOfSemestersToSignUpFor(e.target.value) }}>
+                    {
+                      membership.map((ele, ind) =>
+                        <option key={ind} value={ele.value}>{ele.name}</option>
+                      )}
+                  </select>
+
+                  <Button
+                    type='button'
+                    onClick={() => {
+                      setPagesPrinted(0);
+                    }}
+                    color="info"
+                    style={{ marginTop: '5px' }}>
+                    Reset Pages!
+                  </Button>
+
+                  <FormGroup tag="fieldset">
+                    <legend>Membership Status</legend>
+                    {enums.getAllValues(enums.membershipStatus).map((membership, index) =>
+                      <FormGroup check key={index}>
+                        <Label check>
+                          <Input
+                            type="radio"
+                            name="radio1"
+                            value={membership}
+                            onChange={() => {
+                              setuserMembership(membership)
+                            }}
+                          />
+                          {enums.getKey(enums.membershipStatus, membership)}
+                        </Label>
+                      </FormGroup>
+                    )}
+                  </FormGroup>
+
+                </Form>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button
+                  color="primary"
+                  onClick={() => { handleSubmissionToggle() }}>
+                  Submit
+            </Button>
+                <Button
+                  color="secondary"
+                  onClick={() => { handleToggle() }}>
+                  Cancel
+            </Button>
+              </ModalFooter>
+
+            </Modal>
+          </Col>
+        </Row>
+
+        <Modal
+          style={
+            {
+              marginTop: '320px',
+            }}
+          isOpen={toggleSubmit}>
+          <Button
+            onClick={async () => { await handleSubmission() }}
+            color="primary">
+            YES!
+                </Button>
+          <Button
+            style={{
+              marginTop: '10px'
+            }}
+            onClick={() => handleSubmissionToggle()}
+            color="danger">
+            Nah! It's a mistake.
+                </Button>
+        </Modal>
+
+      </ul>
     </div>
   )
 }
