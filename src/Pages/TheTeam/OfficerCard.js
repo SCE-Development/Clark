8f8a93b48a06b02e954fb1acb557d7bd6603c469
Seema Vora <seema.s.vora@gmail.com>
2020-01-17 16:06:54 -0800

    Added Test Cases

--- src/Pages/TheTeam/OfficerCard.js
+++ src/Pages/TheTeam/OfficerCard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Ionicon from 'react-ionicons'
-import { Col, Media, Card, CardImg, Row, Container } from 'reactstrap'
-// import Slideshow from '../../Components/Slideshow/Slideshow'
+import { Col, Media, CardImg, Row, Container } from 'reactstrap'
 
 const socialMedia = [
   {
@@ -20,17 +19,6 @@ const socialMedia = [
     color: '#757575'
   }
 ]
-// function Test() {
-//     if (socialMedia.icon == 'logo-facebook') {
-//         <a href={props.target.facebook} />
-//     } else if (socialMedia.icon == 'logo-linkedin') {
-//         <a href={props.target.linkedin} />
-//     } else if (socialMedia.icon == 'logo-github') {
-//         <a href={props.target.github} />
-//     }else{
-//         <a href={' '} />
-//     }
-// }
 
 function OfficerCard (props) {
   var col
@@ -39,113 +27,46 @@ function OfficerCard (props) {
   } else {
     col = 'col2'
   }
-
   return (
     <>
       <Col sm='4' id={col}>
-        <Card body className='wholeCard'>
+        <div className='wholeCard card-body'>
           <Media>
-            <Media href={props.target.tag} className='Pic'>
-              <CardImg height='250' width='250' src={props.target.source} />
+            <Media href={props.tag} className='Pic'>
+              <CardImg
+                className='officer-picture d-none d-xl-block'
+                height='250'
+                width='250'
+                src={props.source}
+              />
             </Media>
             <Media body className='SubTitle'>
               <Container>
-                <Media heading className='NameHead'>
-                  <h2>{props.target.name}</h2>
-                  <h5>{props.target.major}</h5>
-                  <Row className='icon-row'>
-                    {socialMedia.map((item, num) => {
-                      return (
-                        <Media key={num} className='link'>
-                          <Ionicon
-                            icon={item.icon}
-                            fontSize={item.fontSize}
-                            color={item.color}
-                          />
-                        </Media>
-                      )
-                    })}
-                  </Row>
-                </Media>
+                <h2 className='name-title'>{props.name}</h2>
+                <h5 className='major-title'>{props.major}</h5>
+                <Row className='icon-row'>
+                  {socialMedia.map((item, num) => {
+                    return (
+                      <Media key={num} className='link'>
+                        <Ionicon
+                          icon={item.icon}
+                          fontSize={item.fontSize}
+                          color={item.color}
+                        />
+                      </Media>
+                    )
+                  })}
+                </Row>
               </Container>
             </Media>
           </Media>
-          <Media className='description'>
+          <Media className='description d-none d-xl-block'>
             Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
             scelerisque ante
           </Media>
-        </Card>
+        </div>
       </Col>
     </>
   )
 }
 export default OfficerCard
-
-//   export default OfficerCard
-
-//   import React, { useState } from 'react'
-//   import Ionicon from 'react-ionicons'
-//   import {
-//     TabContent,
-//     TabPane,
-//     Row,
-//     Col,
-//     Media,
-//     Card,
-//     CardImg
-//   } from 'reactstrap'
-
-//   function OfficerCard(props) {
-//       var col
-//       if (props.index % 2 === 0) {
-//         col = 'col1'
-//       } else {
-//         col = 'col2'
-//       }
-//       return (
-//         <Col sm='4' id={col}>
-//           <Card body className='wholeCard'>
-//             <Media>
-//               <Media href={props.target.tag} className='Pic'>
-//                 <CardImg height='250' width='250' src={props.target.source} />
-//               </Media>
-//               <Media body className='SubTitle'>
-//                 <Media heading className='NameHead'>
-//                   <h2>{props.target.name}</h2>
-//                   <h5>{props.target.major}</h5>
-//                 </Media>
-//                 <Media className='link'>
-//                   <a href={props.target.facebook}>
-//                     <Ionicon
-//                       icon='logo-facebook'
-//                       fontSize='35px'
-//                       color='#757575'
-//                     />
-//                   </a>
-//                   <a href={props.target.linkedin}>
-//                     <Ionicon
-//                       icon='logo-linkedin'
-//                       fontSize='35px'
-//                       color='#757575'
-//                     />
-//                   </a>
-//                   <a href={props.target.github}>
-//                     <Ionicon
-//                       icon='logo-github'
-//                       fontSize='35px'
-//                       color='#757575'
-//                     />
-//                   </a>
-//                 </Media>
-//               </Media>
-//             </Media>
-//             <Media className='description'>
-//               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-//               scelerisque ante
-//                     </Media>
-//           </Card>
-//         </Col>
-//       )
-//     }
-
-//     export default OfficerCard
---
+++ test/frontend/OfficerCard.test.js
@@ -0,0 +1,37 @@
+/* global describe it */
+import 'jsdom-global/register'
+import React from 'react'
+import Enzyme, { mount } from 'enzyme'
+import { expect } from 'chai'
+
+import OfficerCard from '../../src/Pages/TheTeam/OfficerCard'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<OfficerCard />', () => {
+  const officerInfo = {
+    name: 'Evan ',
+    source: 'link',
+    major: 'Software Engineering'
+  }
+
+  const wrapper = mount(<OfficerCard {...officerInfo} />)
+  it('Should render the pictures of the officers', () => {
+    expect(wrapper.find('.officer-picture').get(0).props.src).to.equal(
+      officerInfo.source
+    )
+  })
+
+  it('Should render the name of the officers', () => {
+    expect(wrapper.find('.name-title').get(0).props.children).to.equal(
+      officerInfo.name
+    )
+  })
+
+  it('Should render the major of the officers', () => {
+    expect(wrapper.find('.major-title').get(0).props.children).to.equal(
+      officerInfo.major
+    )
+  })
+})
