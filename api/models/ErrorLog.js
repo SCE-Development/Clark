fe17d7d9fe73e06d04e7bf4146af8775389bc76d
Seema Vora <Seema.s.vora@gmail.com>
2020-02-15 13:50:21 -0800

    Added a Schema for Errors (#293)
    Resolves #292

---
+++ api/models/ErrorLog.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ErrorLogSchema = new Schema(
+  {
+    userEmail: {
+      type: String
+    },
+    errorTime: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    query: {
+      type: String,
+      required: true
+    },
+    errorDescription: {
+      type: String,
+      required: true
+    }
+  },
+  { collection: 'ErrorLogs' }
+)
+
+module.exports = mongoose.model('ErrorLog', ErrorLogSchema)
