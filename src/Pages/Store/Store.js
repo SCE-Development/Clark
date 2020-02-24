09881c16a104cfc1c4644286b7f668b1aca91273
Chak Sum Chan <jason@Chaks-MacBook-Pro.local>
2020-02-24 14:00:23 -0800

    created store page

---
+++ src/Pages/Store/Store.js
@@ -0,0 +1,15 @@
+import React from 'react'
+export default class Store extends React.Component {
+  constructor (props) {
+    super(props)
+
+    // Variables
+    this.state = {
+      yourVariable: 'Go In HERE!'
+    }
+  }
+
+  render () {
+    return <div>PAGE CONTENT ggggg</div>
+  }
+}
