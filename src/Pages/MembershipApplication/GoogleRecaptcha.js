9232a3b1fad8347874c25cd5a962f8bf76312b3e
admin <admin@admins-MacBook-Pro.local>
2020-03-15 19:55:27 -0700

    captcha

---
+++ src/Pages/MembershipApplication/GoogleRecaptcha.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
+import { sitekey } from '../../config/config'
+
+class GoogleRecaptcha extends Component {
+  constructor (props, ...args) {
+    super(props, ...args)
+    this.state = { isVerified: false }
+    this.recapRef = React.createRef()
+  }
+
+  handleChange = recaptchaToken => {
+    return new Promise((resolve, reject) => {
+      if (recaptchaToken && recaptchaToken.length) this.props.setVerified(true)
+      if (recaptchaToken === null) this.props.setVerified(false)
+      resolve()
+    })
+  }
+
+  render () {
+    return (
+      <ReCAPTCHA
+        size='normal'
+        theme='light'
+        ref={this.recapRef}
+        sitekey={sitekey}
+        onChange={this.handleChange}
+      />
+    )
+  }
+}
+export default GoogleRecaptcha
