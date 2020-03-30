4233d91817cec22b0b36bd507fcd36289411c79f
Seema Vora <seema.s.vora@gmail.com>
2020-03-30 15:25:14 -0700

    Fixes Linting Error

---
+++ api/util/errorLog-function.js
@@ -0,0 +1,27 @@
+const ErrorLog = require('../models/ErrorLog');
+
+/**
+ * Add a new error.
+ * @param {Object} newError - The error that is to be added
+ * @param {(string|undefined)} req.body.userEmail - The email of the user
+ *                                                  who has sent this error
+ * @param {(string|undefined)} req.body.errorTime- The time the error occured
+ * @param {string} req.body.apiEndpoint - The location of the error
+ * @param {string} req.body.errordescription - The description of the error
+ */
+async function addErrorLog(req){
+  let errorSaved = true;
+  const newError = new ErrorLog({
+    userEmail: req.body.userEmail,
+    errorTime: req.body.errorTime,
+    apiEndpoint: req.body.apiEndpoint,
+    errorDescription: req.body.errorDescription
+  });
+  await newError.save()
+    .catch(_ => {
+      errorSaved = false;
+    });
+  return errorSaved;
+}
+
+module.exports = {addErrorLog};
