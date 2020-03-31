5cf5e85ac3cbd4d48f3fe0dbf1b6923c33c8e408
aashrav <aashrav.shetty01@gmail.com>
2020-03-31 11:49:34 -0700

    added InventoryItem api, routing, and testing

---
+++ api/routes/InventoryItem.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const router = express.Router();
+const InventoryItem = require('../models/InventoryItem');
+const settings = require('../util/settings');
+const logger = require(`${settings.util}/logger`);
+const {
+  checkIfTokenSent,
+  checkIfTokenValid
+} = require('../util/token-functions');
+const {
+  OK,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  FORBIDDEN,
+  NOT_FOUND
+} = require('../constants').STATUS_CODES;
+
+router.get('/getItems', (req, res) => {
+  const category = req.body.category ? { category: req.body.category } : {};
+  InventoryItem.find(category).then(items => res.status(OK).send(items));
+});
+
+router.post('/editItem', (req, res) => {
+  if (!checkIfTokenSent(req)) {
+    return res.sendStatus(FORBIDDEN);
+  } else if (!checkIfTokenValid(req)) {
+    return res.sendStatus(UNAUTHORIZED);
+  }
+
+  const { name, price, stock, category, description, picture } = req.body;
+
+  InventoryItem.findOne({ name: req.body.name })
+    .then(item => {
+      item.name = name || item.name;
+      item.price = price || item.price;
+      item.stock = stock || item.stock;
+      item.category = category || item.category;
+      item.description = description || item.description;
+      item.picture = picture || item.picture;
+      item
+        .save()
+        .then(ret => {
+          res
+            .status(OK)
+            .json({ ret, item: 'Inventory item updated successfully' });
+        })
+        .catch(err => {
+          res.status(BAD_REQUEST).send({
+            err,
+            message: 'Inventory item was not updated'
+          });
+        });
+    })
+    .catch(err => {
+      res.status(NOT_FOUND).send({ err, message: 'item not found' });
+    });
+});
+
+router.post('/addItem', (req, res) => {
+  if (!checkIfTokenSent(req)) {
+    return res.sendStatus(FORBIDDEN);
+  } else if (!checkIfTokenValid(req)) {
+    return res.sendStatus(UNAUTHORIZED);
+  }
+  const newItem = new InventoryItem({
+    name: req.body.name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category: req.body.category,
+    description: req.body.description,
+    picture: req.body.picture
+  });
+
+  InventoryItem.create(newItem, (error, post) => {
+    if (error) {
+      logger.log(`InventoryItem /addItem error: ${error}`);
+      return res.sendStatus(BAD_REQUEST);
+    }
+    return res.json(post);
+  });
+});
+
+router.post('/deleteItem', (req, res) => {
+  if (!checkIfTokenSent(req)) {
+    return res.sendStatus(FORBIDDEN);
+  } else if (!checkIfTokenValid(req)) {
+    return res.sendStatus(UNAUTHORIZED);
+  }
+  InventoryItem.deleteOne({ name: req.body.name }, (error, form) => {
+    if (error) {
+      return res.sendStatus(BAD_REQUEST);
+    }
+    if (form.n < 1) {
+      return res.sendStatus(NOT_FOUND);
+    } else {
+      return res.status(OK).send({ message: `${req.body.name} was deleted.` });
+    }
+  });
+});
+
+module.exports = router;
---
+++ test/InventoryItem.js
@@ -0,0 +1,275 @@
+/* global describe it before after */
+// During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+const InventoryItem = require('../api/models/InventoryItem');
+// Require the dev-dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const constants = require('../api/constants');
+const { OK, BAD_REQUEST, UNAUTHORIZED, NOT_FOUND } = constants.STATUS_CODES;
+
+let app = null;
+
+const expect = chai.expect;
+// tools for testing
+const tools = require('../util/testing-utils/tools.js');
+const {
+  setTokenStatus,
+  resetMock,
+  restoreMock
+} = require('./mocks/TokenValidFunctions');
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('InventoryItem', () => {
+  before(done => {
+    app = tools.initializeServer();
+    // Before each test we empty the database
+    tools.emptySchema(InventoryItem);
+    done();
+  });
+
+  after(done => {
+    restoreMock();
+    tools.terminateServer(done);
+  });
+
+  beforeEach(() => {
+    setTokenStatus(false);
+  });
+
+  afterEach(() => {
+    resetMock();
+  });
+
+  const token = '';
+  let itemName = '';
+  const VALID_NEW_ITEM = {
+    name: 'Chips',
+    price: 0.75,
+    stock: 25,
+    category: 'Food',
+    description: 'Food that you can eat',
+    picture: '%&(&%##$^&(&#@'
+  };
+  const ITEM_WITH_INVALID_TOKEN = {
+    token: 'invalid'
+  };
+  const ITEM_WITHOUT_REQUIRED_FIELDS = {
+    name: 'chocolate'
+  };
+  const ITEM_WITH_INVALID_NAME = {
+    name: 'bob'
+  };
+  const UPDATED_ITEM = {
+    name: 'Chips',
+    price: 0.5,
+    stock: 15,
+    category: 'Snack',
+    description: 'A snack that you can eat',
+    picture: ')(^#!@$%*(*'
+  };
+
+  describe('/POST addItem', () => {
+    it('Should return 403 when an invalid token is supplied', done => {
+      chai
+        .request(app)
+        .post('/api/InventoryItem/addItem')
+        .send(ITEM_WITH_INVALID_TOKEN)
+        .then(function(res) {
+          expect(res).to.have.status(UNAUTHORIZED);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('Should return 400 when the required fields aren\'t filled in', done => {
+      setTokenStatus(true);
+      chai
+        .request(app)
+        .post('/api/InventoryItem/addItem')
+        .send({ token, ...ITEM_WITHOUT_REQUIRED_FIELDS })
+        .then(function(res) {
+          expect(res).to.have.status(BAD_REQUEST);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('Should return statusCode 200 when all required fields are filled in',
+      done => {
+        setTokenStatus(true);
+        chai
+          .request(app)
+          .post('/api/InventoryItem/addItem')
+          .send({ token, ...VALID_NEW_ITEM })
+          .then(function(res) {
+            expect(res).to.have.status(OK);
+            done();
+          })
+          .catch(err => {
+            throw err;
+          });
+      });
+  });
+
+  describe('/GET getItems', () => {
+    it('Should return an object of all items', done => {
+      setTokenStatus(true);
+      chai
+        .request(app)
+        .get('/api/InventoryItem/getItems')
+        .then(function(res) {
+          expect(res).to.have.status(OK);
+          const getItemsResponse = res.body;
+          getItemsResponse.should.be.a('array');
+          expect(getItemsResponse).to.have.length(1);
+          expect(getItemsResponse[0].name).to.equal(VALID_NEW_ITEM.name);
+          expect(getItemsResponse[0].price).to.equal(VALID_NEW_ITEM.price);
+          expect(getItemsResponse[0].stock).to.equal(VALID_NEW_ITEM.stock);
+          expect(getItemsResponse[0].category).to
+            .equal(VALID_NEW_ITEM.category);
+          expect(getItemsResponse[0].picture).to.equal(VALID_NEW_ITEM.picture);
+          expect(getItemsResponse[0].description).to.equal(
+            VALID_NEW_ITEM.description
+          );
+          itemName = getItemsResponse[0].name;
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+  });
+
+  describe('/POST editItem', () => {
+    it('Should return 403 when an invalid token is supplied', done => {
+      chai
+        .request(app)
+        .post('/api/InventoryItem/editItem')
+        .send({ itemId: itemName, ...ITEM_WITH_INVALID_TOKEN })
+        .then(function(res) {
+          expect(res).to.have.status(UNAUTHORIZED);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('Should return 404 when an item by an invalid id isn\'t found', done => {
+      setTokenStatus(true);
+      chai
+        .request(app)
+        .post('/api/InventoryItem/editItem')
+        .send({ token, ...ITEM_WITH_INVALID_NAME })
+        .then(function(res) {
+          expect(res).to.have.status(NOT_FOUND);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('Should return 200 when an item is sucessfully updated', done => {
+      setTokenStatus(true);
+      chai
+        .request(app)
+        .post('/api/InventoryItem/editItem')
+        .send({ token, name: itemName, ...UPDATED_ITEM })
+        .then(function(res) {
+          expect(res).to.have.status(OK);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('The update should be reflected in the database', done => {
+      setTokenStatus(true);
+      chai
+        .request(app)
+        .get('/api/InventoryItem/getItems')
+        .then(function(res) {
+          expect(res).to.have.status(OK);
+          const getItemsResponse = res.body;
+          expect(getItemsResponse).to.have.length(1);
+          expect(getItemsResponse[0].name).to.equal(UPDATED_ITEM.name);
+          expect(getItemsResponse[0].price).to.equal(UPDATED_ITEM.price);
+          expect(getItemsResponse[0].stock).to.equal(UPDATED_ITEM.stock);
+          expect(getItemsResponse[0].category).to.equal(UPDATED_ITEM.category);
+          expect(getItemsResponse[0].description).to.equal(
+            UPDATED_ITEM.description
+          );
+          expect(getItemsResponse[0].picture).to.equal(UPDATED_ITEM.picture);
+
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+  });
+
+  describe('/POST deleteItem', () => {
+    it('Should return 403 when an invalid token is supplied', done => {
+      chai
+        .request(app)
+        .post('/api/InventoryItem/deleteItem')
+        .send(ITEM_WITH_INVALID_TOKEN)
+        .then(function(res) {
+          expect(res).to.have.status(UNAUTHORIZED);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('Should return 404 when an item is not found', done => {
+      setTokenStatus(true);
+      chai
+        .request(app)
+        .post('/api/InventoryItem/deleteItem')
+        .send({ token, ...ITEM_WITH_INVALID_NAME })
+        .then(function(res) {
+          expect(res).to.have.status(NOT_FOUND);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('Should return 200 when an item is sucessfully deleted', done => {
+      setTokenStatus(true);
+      chai
+        .request(app)
+        .post('/api/InventoryItem/deleteItem')
+        .send({ token, name: itemName })
+        .then(function(res) {
+          expect(res).to.have.status(OK);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('The deleted item should be reflected in the database', done => {
+      setTokenStatus(true);
+      chai
+        .request(app)
+        .get('/api/InventoryItem/getItems')
+        .then(function(res) {
+          expect(res).to.have.status(OK);
+          const getItemsResponse = res.body;
+          getItemsResponse.should.be.a('array');
+          expect(getItemsResponse).to.have.length(0);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+  });
+});
