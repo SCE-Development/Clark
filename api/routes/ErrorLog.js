278a61ebf6ecb73c108e2891fd39e2eb9e0bec07
Seema Vora <Seema.s.vora@gmail.com>
2020-04-05 19:11:51 -0700

    Change Parameters in ErrorLog Utility Function (#425)

--- api/routes/ErrorLog.js
+++ api/routes/ErrorLog.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const ErrorLog = require('../models/ErrorLog');
 const { OK, BAD_REQUEST } = require('../constants').STATUS_CODES;
-const { addErrorLog } = require('../util/errorLog-function');
+const { addErrorLog } = require('../util/errorLog');
 
 router.post('/addErrorLog', async (req, res) => {
-  if (!await addErrorLog(req)) {
+  if (!await addErrorLog(req.body.userEmail, req.body.errorTime,
+    req.body.apiEndpoint, req.body.errorDescription)) {
     return res.sendStatus(BAD_REQUEST);
   } else {
     res.sendStatus(OK);
