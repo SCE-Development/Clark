42fd0c32a2ae1c765b5d2a867073bc44dc4fcd83
IsitaB <50391828+IsitaB@users.noreply.github.com>
2020-05-23 16:06:58 -0700

    Refactor MembershipForm UI (#367) (#428)
    *Added uniform background to MembershipForm and Login pages
    *Card styling for forms on both pages
    *Changed button styling
    *Edited tests

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Row, Container } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import LoginInput from './LoginInput';
 import { loginUser } from '../../APIFunctions/User';
@@ -8,6 +9,18 @@ export default function Login(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
+  const fields = [
+    {
+      type: 'email',
+      placeholder: 'Email',
+      handleChange: e => setEmail(e.target.value)
+    },
+    {
+      type: 'password',
+      placeholder: 'Password',
+      handleChange: e => setPassword(e.target.value)
+    }
+  ];
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -23,34 +36,23 @@ export default function Login(props) {
     }
   }
 
-  const fields = [
-    {
-      type: 'email',
-      placeholder: 'Email',
-      handleChange: e => setEmail(e.target.value)
-    },
-    {
-      type: 'password',
-      placeholder: 'Password',
-      handleChange: e => setPassword(e.target.value)
-    }
-  ];
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div id='body'>
-        <img id='img' alt='sce logo' src='images/SCE-glow.png' />
-        {errorMsg && <span>{errorMsg}</span>}
-        {fields.map((field, index) => {
-          return <LoginInput key={index} field={field} />;
-        })}
-        <button type='submit' id='loginBtn'>
-          Login
-        </button>
-        <p id='SignUp'>
-          <Link to='/register'>Create an account</Link>
-        </p>
-      </div>
-    </form>
+    <Container fluid className='background'>
+      <Row className='form-card-login'>
+        <form onSubmit={handleSubmit}>
+          <img id='img' alt='sce logo' src='images/SCE-glow.png' />
+          {errorMsg && <span>{errorMsg}</span>}
+          {fields.map((field, index) => {
+            return <LoginInput key={index} field={field} />;
+          })}
+          <button type='submit' id='loginBtn'>
+              Login
+          </button>
+          <p id='SignUp'>
+            <Link to='/register'>Create an account</Link>
+          </p>
+        </form>
+      </Row>
+    </Container>
   );
 }
