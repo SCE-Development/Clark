77769e8e9531c1771907e8ecf7569b7b7829c63f
Evan Ugarte <evanuxd@gmail.com>
2020-05-29 21:53:40 -0700

    Change API Calls to Use RPC Service
    The frontend now calls directly on SCE-RPC Express endpoints.

--- src/APIFunctions/LedSign.js
+++ src/APIFunctions/LedSign.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { ApiResponse } from './ApiResponses';
+import { RPC_API_URL } from '../config/config';
 
 /**
  * Checks to see if the sign is accepting requests. This is done
@@ -10,7 +11,7 @@ import { ApiResponse } from './ApiResponses';
 export async function healthCheck(officerName) {
   let status = new ApiResponse();
   await axios
-    .post('api/LedSign/healthCheck', { officerName })
+    .post(RPC_API_URL + '/SceRpcApi/LedSign/healthCheck', { officerName })
     .then(res => {
       status.responseData = res.data;
     })
@@ -41,27 +42,16 @@ export async function getAllSignLogs() {
 }
 
 /**
- * Add a new sign log.
- * @param {Object} newEvent - The event that is to be added
- * @param {string} newEvent.title - The title of the new event
- * @param {(string|undefined)} newEvent.description - The description of the
- * new event
- * @param {string} newEvent.eventLocation - The location of the new event
- * @param {string} newEvent.eventDate - The late of the new event
- * @param {string} newEvent.startTime - The start time of the new event
- * @param {string} newEvent.endTime - The end time of the new event
- * @param {(string|undefined)} newEvent.eventCategory - The category of the new
- * event
- * @param {(string|undefined)} newEvent.imageURL - A URL of the image of the
- * event
- * @param {string} token - The user's jwt token for authentication
+ * Update the text of the sign.
+ * @param {Object} signData - An object containing all of the sign data (text,
+ * colors, etc.) sent to the RPC client.
  * @returns {ApiResponse} Containing any error information related to the
  * request
  */
 export async function updateSignText(signData) {
   let status = new ApiResponse();
   await axios
-    .post('api/LedSign/updateSignText', { ...signData })
+    .post(RPC_API_URL + '/SceRpcApi/LedSign/updateSignText', { ...signData })
     .then(res => {
       status = res.data;
     })
