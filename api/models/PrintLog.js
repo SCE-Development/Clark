e3f110386c79e86f482a350b4a6cdc8a7c40b05c
1234momo <garychang9@gmail.com>
2020-06-23 22:46:15 -0700

    Add PrintLog API and Schema (#512)
    * added schema for printer
    
    * created printer analytics API
    
    * Created tests for PrintLog API
    
    * fix styling issues
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

---
+++ api/models/PrintLog.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PrintLogSchema = new Schema(
+  {
+    numPages: {
+      type: Number,
+      required: true
+    },
+    chosenPrinter: {
+      type: String,
+      required: true
+    },
+    printedDate: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    memberName: {
+      type: String,
+      required: true
+    }
+  },
+  { collection: 'PrintLogs' }
+);
+
+module.exports = mongoose.model('PrintLog', PrintLogSchema);
