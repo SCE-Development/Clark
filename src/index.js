9fb26baf153b2572f0efa22e22d631803709876d
Farah Masood <farah.masood@sjsu.edu>
2020-07-02 17:45:18 -0700

    Add Dark Theme Mode
    Resolves #530
    Resolves #570

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import Routing from './Routing';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { checkIfUserIsSignedIn } from './APIFunctions/Auth';
+import { checkIfDarkThemeActive } from './APIFunctions/dark-theme';
+
 
 function App(props) {
   const [authenticated, setAuthenticated] = useState(false);
@@ -22,6 +24,7 @@ function App(props) {
 
   useEffect(() => {
     getAuthStatus();
+    checkIfDarkThemeActive();
     // eslint-disable-next-line
   }, [])
 
