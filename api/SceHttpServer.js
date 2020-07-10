41c0ba0893c27313e962ef6332a381c875accb93
sce <sce@sce.engr.sjsu.edu>
2020-07-10 10:26:10 -0700

    generated csr key

--- api/SceHttpServer.js
+++ api/SceHttpServer.js
@@ -120,20 +120,10 @@ if (typeof module !== 'undefined' && !module.parent) {
   const generalApiEndpoints = __dirname + '/routes/';
   const loggingApiEndpoints = __dirname + '/logging/';
   const mailerApiEndpoints = __dirname + '/mailer/mailer.js';
-
-  const generalServer = new SceHttpServer(generalApiEndpoints, 8080);
-  const loggingServer = new SceHttpServer(loggingApiEndpoints, 8081);
-  const mailerServer = new SceHttpServer(mailerApiEndpoints, 8082);
-
-  generalServer.initializeEndpoints().then(() => {
-    generalServer.openConnection();
-  });
-  loggingServer.initializeEndpoints().then(() => {
-    loggingServer.openConnection();
-  });
-  mailerServer.initializeEndpoints().then(() => {
-    mailerServer.openConnection();
-  });
+	
+const g = new SceHttpServer([generalApiEndpoints,loggingApiEndpoints,mailerApiEndpoints],8080);
+	g.initializeEndpoints().then(()=> {
+	g.openConnection();
+});
 }
-
 module.exports = { SceHttpServer };
