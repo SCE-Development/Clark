2ca04882cd6f4204166160c18fe8074d882e0820
thibui0908 <thi.t.bui@sjsu.edu>
2020-07-20 14:22:26 -0700

    added s3 bucket backend

---
+++ api/cloud_api/routes/S3Bucket.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { OK, NOT_FOUND } = require('../../util/constants').STATUS_CODES;
+const s3BucketKeys = require('../../config/config.json').S3Bucket;
+const { S3BucketApiHandler } = require('../util/S3BucketApiHandler');
+
+router.post('/getSignedUrl', (req, res) => {
+  const fileName = req.body.fileName;
+  const fileType = req.body.fileType;
+  const bucket = s3BucketKeys.BUCKET;
+
+  const s3Params = {
+    Bucket: bucket,
+    Key: fileName,
+    Expires: 500,
+    ContentType: fileType,
+    ACL: 'public-read',
+  };
+
+  const s3Handler = new S3BucketApiHandler();
+
+  s3Handler
+    .getSignedUrl(s3Params, bucket)
+    .then((url) => {
+      res.status(OK).send({ url });
+    })
+    .catch((_) => {
+      res.sendStatus(NOT_FOUND);
+    });
+});
+
+module.exports = router;
