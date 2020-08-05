d75ac3060e7da79b521fa813eede681c62a68c70
Silenchezhian <54826539+Silenchezhian@users.noreply.github.com>
2020-08-05 14:31:06 -0700

    added google sheets backend (#549)
    Started working on testing
    
    testing done
    
    fixed errors
    
    fixed a comment
    
    made requested changes

--- api/cloud_api/util/SceGoogleApiHandler.js
+++ api/cloud_api/util/SceGoogleApiHandler.js
@@ -4,6 +4,8 @@ const { google } = require('googleapis');
 const { googleApiKeys } = require('../../config/config.json');
 const nodemailer = require('nodemailer');
 const { consoleColors } = require('../../util/constants');
+const { reject } = require('bluebird');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 const {
   CLIENT_SECRET, CLIENT_ID, REDIRECT_URIS,
@@ -322,6 +324,44 @@ class SceGoogleApiHandler {
       smtpTransport.sendMail(mailTemplate, (error, response) => {
         error ? reject(error) : resolve(response);
         smtpTransport.close();
+      });
+    });
+  }
+  /**
+ * Adds responses from officer app form to a google spreadsheet
+ * @param {String} sheetsId spreadsheet id for the spreadsheet to add to
+ * @param {object} data response data from the officer application form
+ */
+  async writeToForm(sheetsId, data){
+    return new Promise(async (resolve, reject)=>{
+      GoogleSpreadsheet.openById(sheetsId, (error, response) => {
+        if (error){
+          reject(false);
+        }
+      });
+      const doc = new GoogleSpreadsheet(sheetsId);
+      this.checkIfTokenFileExists(this.tokenPath, (error, response)=> {
+        if(error){
+          reject(false);
+        }
+      });
+      await doc.useServiceAccountAuth(require(this.tokenPath));
+      await doc.loadInfo();
+      const sheet = doc.sheetsByIndex[0];
+      const row = {
+        Name: data.name,
+        Email: data.email,
+        'Graduation Month': data.gradMonth,
+        'Graduation Year': data.gradYear,
+        'Work Experience': data.experience,
+        LinkedIn: data.linkedin
+      };
+      sheet.addRow(row, (error, response) => {
+        if(error){
+          reject(false);
+        }else{
+          resolve(response);
+        }
       });
     });
   }
---
+++ src/APIFunctions/GoogleSheets.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { ApiResponse } from './ApiResponses';
+import { MAILER_API_URL } from '../config/config.json';
+
+/**
+ * Invokes the google spreadsheet API to add officer application form data to it
+ * @param {object} row - the data from the officer application form
+ * @param {String} sheetsId - ID of the google sheet getting updated
+ * @returns {ApiResponse} Containing any error information related to the
+ * request
+ */
+export async function addToSpreadsheet(row, sheetsId){
+  let status = new ApiResponse();
+  await axios
+    .post(MAILER_API_URL + '/api/sheets/addToSpreadsheet',
+      {row, sheetsId})
+    .then((res)=>{
+      status.responseData = res;
+      status.error = false;
+    })
+    .catch(err => {
+      status.responseData = err;
+      status.error = true;
+    });
+  return status;
+}
