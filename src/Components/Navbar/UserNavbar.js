f84a78dc24d2748bd5aa3f3cc9750447f14d4825
Farah Masood <farah.masood@sjsu.edu>
2020-09-01 19:46:47 -0700

    Fixed Minimization Error

--- src/Components/Navbar/UserNavbar.js
+++ src/Components/Navbar/UserNavbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'universal-cookie';
+import './navbar.css';
 import {
   ButtonDropdown,
   Collapse,
@@ -150,20 +151,20 @@ export default function UserNavBar(props) {
               </DropdownItem>
               {props.user && props.user.accessLevel >=
                 membershipState.MEMBER && (
-                <DropdownItem tag='h1' className='dropdown-submenu drp-item'>
-                  <DropdownItem className='drp-item' id='btndrp-text'>
+                  <DropdownItem tag='h1' className='dropdown-submenu drp-item'>
+                    <DropdownItem className='drp-item' id='btndrp-text'>
                       Printing
                   </DropdownItem>
-                  <DropdownMenu className='drp-menu'>
-                    <DropdownItem className='drp-item' href='/2DPrinting'>
+                    <DropdownMenu className='drp-menu'>
+                      <DropdownItem className='drp-item' href='/2DPrinting'>
                         2D Printing
                     </DropdownItem>
-                    <DropdownItem className='drp-item' href='/3DPrintingForm'>
+                      <DropdownItem className='drp-item' href='/3DPrintingForm'>
                         3D Printing
                     </DropdownItem>
-                  </DropdownMenu>
-                </DropdownItem>
-              )}
+                    </DropdownMenu>
+                  </DropdownItem>
+                )}
               {unauthedRoutes.map((link, index) => {
                 return (
                   <DropdownItem className='drp-item' key={index}>
@@ -216,20 +217,20 @@ export default function UserNavBar(props) {
                   </DropdownMenu>
                 </DropdownItem>
               ) : (
-                <DropdownItem tag='h1' className='dropdown-submenu drp-item'>
-                  <DropdownItem className='drp-item' id='btndrp-text'>
+                  <DropdownItem tag='h1' className='dropdown-submenu drp-item'>
+                    <DropdownItem className='drp-item' id='btndrp-text'>
                       Join Us!
                   </DropdownItem>
-                  <DropdownMenu right className='drp-menu'>
-                    <DropdownItem className='drp-item' href='/register'>
+                    <DropdownMenu right className='drp-menu'>
+                      <DropdownItem className='drp-item' href='/register'>
                         Membership Application
                     </DropdownItem>
-                    <DropdownItem className='drp-item' href='/login'>
+                      <DropdownItem className='drp-item' href='/login'>
                         Login
                     </DropdownItem>
-                  </DropdownMenu>
-                </DropdownItem>
-              )}
+                    </DropdownMenu>
+                  </DropdownItem>
+                )}
             </DropdownMenu>
           </ButtonDropdown>
         </NavbarToggler>
@@ -309,20 +310,20 @@ export default function UserNavBar(props) {
                 </DropdownMenu>
               </UncontrolledDropdown>
             ) : (
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle id='navlink-text' nav caret>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle id='navlink-text' nav caret>
                     Join Us!
                 </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem className='drp-item' href='/register'>
+                  <DropdownMenu right>
+                    <DropdownItem className='drp-item' href='/register'>
                       Membership Application
                   </DropdownItem>
-                  <DropdownItem className='drp-item' href='/login'>
+                    <DropdownItem className='drp-item' href='/login'>
                       Login
                   </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            )}
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              )}
           </Nav>
         </Collapse>
       </Navbar>
