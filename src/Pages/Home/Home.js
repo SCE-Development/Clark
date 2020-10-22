8d077c065c1b2c3f8c57933e8adec30c136b23f4
surabhi gupta <surabhi2029@gmail.com>
2020-10-22 11:14:21 -0700

    Adds Group Pic and Title

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,10 +6,10 @@ import 'aos/dist/aos.css';
 import 'animate.css/animate.min.css';
 
 import './home.css';
-//import Slideshow from '../../Components/Slideshow/Slideshow.js';
+// import Slideshow from '../../Components/Slideshow/Slideshow.js';
 import Footer from '../../Components/Footer/Footer.js';
-//import Jumbotron from '../../Components/Jumbotron/Jumbotron.js';
-//import Iframe from 'react-iframe';
+// import Jumbotron from '../../Components/Jumbotron/Jumbotron.js';
+// import Iframe from 'react-iframe';
 import { Row, Col, Container } from 'reactstrap';
 // import Calendar from '../../Calendar/App.js'; <Calendar/>
 
