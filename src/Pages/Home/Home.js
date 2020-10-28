d4cf1c427c2057c650c6cfb149da45cab4e947d5
surabhi gupta <surabhi2029@gmail.com>
2020-10-28 11:47:22 -0700

    Creates parallax scrolling effect for homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,10 +5,12 @@ import './home.css';
 import Footer from '../../Components/Footer/Footer.js';
 import { Row, Col } from 'reactstrap';
 
+
 const SCEtitle = 'Software and \n Computer \n Engineering \n Society';
 
 // The major block texts surrounding the sub block
 const blockOneText = [
+  { title: 'Our clubroom offers everything an SE ' },
   { title: 'or COMPE student could need.' },
   { title: <br /> },
   { title: 'We continue to offer resources to our  ' },
