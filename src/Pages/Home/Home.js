d5fb359d98bbfabd9f4d54a97ccf2112e225fc3a
surabhi gupta <surabhi2029@gmail.com>
2020-10-28 11:47:22 -0700

    Creates parallax scrolling effect for homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,6 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import 'animate.css/animate.min.css';
 import './home.css';
-<<<<<<< HEAD
 import Footer from '../../Components/Footer/Footer.js';
 import { Row, Col } from 'reactstrap';
 import { icons } from './IconConstants';
@@ -39,42 +38,7 @@ const subBlockThreeText =
   'Join us for fun and exciting social events for different occasions!' +
   '\n\nWe host potlucks, movie nights, game nights and much more!';
 
-=======
-//import Slideshow from '../../Components/Slideshow/Slideshow.js';
-import Footer from '../../Components/Footer/Footer.js';
-<<<<<<< HEAD
-//import Jumbotron from '../../Components/Jumbotron/Jumbotron.js';
-//import Iframe from 'react-iframe';
-import { Row, Col, Container } from 'reactstrap';
-=======
-import Jumbotron from '../../Components/Jumbotron/Jumbotron.js';
-import Iframe from 'react-iframe';
-import UserNavbar from '../../Components/Navbar/UserNavbar.js';
->>>>>>> Adds Group Pic and Title
-// import Calendar from '../../Calendar/App.js'; <Calendar/>
 
-/*
-  <Iframe url="https://calendar.google.com/calendar/embed?src=
-  llv828585faitko1m2nh39s3js%40group.calendar.google.co
-  m&ctz=America%2FLos_Angeles"
-        width="1000px"
-        height="800px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        allowFullScreen/>
-
-        <Iframe url="https://calendar.google.com/calendar/b/4/embed?showTitle=
-        0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;
-        height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=llv828585faitko1
-        m2nh39s3js%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=
-        America%2FLos_Angeles" style="border-width:0" width="1000"
-        height="850" frameborder="0" scrolling="no"/>
-        */
->>>>>>> Fixes lint error
-
-<<<<<<< HEAD
 const blockOneText = [
   { title: 'Our clubroom offers everything a software or computer' },
   { title: 'engineering student could need.' },
@@ -88,14 +52,6 @@ const blockTwoText = [
   { title: 'Instagram and more today, and connect with SCE members' },
 ];
 
-=======
-const sceTitle = [
-  { title: "Software and \n" },
-  { title: "Computer" },
-  { title: "Engineering" },
-  { title: "Society" }
-]
->>>>>>> Adds Group Pic and Title
 const text = 'Software and \n Computer \n Engineering \n Society';
 
 class Home extends Component {
@@ -108,14 +64,8 @@ class Home extends Component {
 
           {/* This container is for the SCE offciers picture and title */}
           <div class="groupPicContainer">
-<<<<<<< HEAD
             <img class="groupictureStyles" src='images/officers2019_2.jpg'
               alt="officersPic" style={{ width: '100%' }}></img>
-=======
-<<<<<<< HEAD
-            <img class="groupPictureStyles" src='images/officers2019_2.jpg'
-              alt="officersPic" style={{ width: '105%' }}></img>
->>>>>>> Adds Group Pic and Title
           </div>
           <div class="sceLogo" data-aos="fade-down" data-aos-duration="1000" >
             <div className="img-container">
@@ -135,82 +85,7 @@ class Home extends Component {
          what we have to offer and social media links */}
           <div class="contentContainer">
 
-<<<<<<< HEAD
             {/* The first major block: "Need a place to get things done?" */}
-=======
-            <div className="eventsContainer">
-              <Container>
-                <h1>------------------------</h1>
-                <h1>EVENTS!!!!</h1>
-                <h1>------------------------</h1>
-
-              </Container>
-            </div>
-            {/* <div class="sceTitle">
-              {sceTitle.map((item) => {
-                return (
-                  <text>{item.title + "\n"}</text>
-                );
-              })}
-            </div> */}
-
-            {/* <div className='text-center'>
-=======
-            <img src='images/officers2019.jpg' alt="officersPic" style={{ width: "100%" }}></img>
-            <div class="sceLogo">
-              <img id="logo-image" src="/static/media/sce_logo.1f644918.png" alt="sce logo" style={{ width: "50%" }}></img>
-            </div>
-            <div className="sceTitle">
-              {text}
-            </div>
-            {/* <div class="sceTitle">
-              {text.split("\n").map((item) => {
-                return (
-                  <text>{item}</text>
-                );
-              })}
-            </div> */}
-          </div>
-          <div class="contentContainer">
-            <div className='text-center'>
->>>>>>> Adds Group Pic and Title
-              <h1 className='display-4'>SCE Events Calendar</h1>{' '}
-            </div>
-            <p className='lead text-center'>
-              {' '}
-            Add SCE Calendar to your own by clicking the Google Calendar icon on
-<<<<<<< HEAD
-            the bottom right side!</p> */}
-
-            {/* <div className='outer-div'>
-=======
-            the bottom right side!</p>
-
-            <div className='outer-div'>
->>>>>>> Adds Group Pic and Title
-              <div className='inner-div'>
-                <Iframe
-                  // Unable to breka up iframe url
-                  // eslint-disable-next-line
-<<<<<<< HEAD
-                  url='https://calendar.google.com/calendar/b
-                  /4/embed?showTitle=0&amp;showPrint=0&amp;showTabs=
-                  0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp
-                  ;wkst=1&amp;bgcolor=%23ffffff&amp;src=llv828585faitko
-                  1m2nh39s3js%40group.calendar.google.com&amp;color=%
-                  23182C57&amp;ctz=America%2FLos_Angeles'
-=======
-                  url='https://calendar.google.com/calendar/b/4/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=llv828585faitko1m2nh39s3js%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FLos_Angeles'
->>>>>>> Adds Group Pic and Title
-                  className='calendar'
-                  allowFullScreen
-                />
-              </div>
-<<<<<<< HEAD
-            </div> */}
-            <div className="block-space"></div>
-
->>>>>>> Adds Group Pic and Title
             <div className="block-one">
               <Row>
                 <Col>
@@ -319,19 +194,7 @@ class Home extends Component {
                 </Col>
               </Row>
             </div>
-<<<<<<< HEAD
             <div className="block-space" />
-=======
-            <div className="clubRoomContainer">
-              <img class="groupPictureStyles" src='images/emptyclubroom.png'
-                alt="officersPic" style={{ width: '105%' }}></img>
-            </div>
-
-
-=======
-            </div>
->>>>>>> Adds Group Pic and Title
->>>>>>> Adds Group Pic and Title
             <Footer />
           </div >
         </div >
