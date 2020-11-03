837346d3eaf484b14583e4df8d54a0c63b7a7c8e
Habib Sorathia <36381158+hsorathia@users.noreply.github.com>
2020-11-03 02:18:44 -0800

    added new dependabots (#690)

--- src/Pages/MembershipApplication/membershipApplication.jsx
+++ src/Pages/MembershipApplication/membershipApplication.jsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react'
-import './register-page.css'
-import MembershipPlan from './MembershipPlan.js'
-import { memberApplicationState } from '../../Enums'
-import MembershipForm from './MembershipForm'
-import ConfirmationPage from './ConfirmationPage'
-import { CSSTransition } from 'react-transition-group'
+import React, { useState } from 'react';
+import './register-page.css';
+import MembershipPlan from './MembershipPlan.js';
+import { memberApplicationState } from '../../Enums';
+import MembershipForm from './MembershipForm';
+import ConfirmationPage from './ConfirmationPage';
+import { CSSTransition } from 'react-transition-group';
 
 export default function MembershipApplication() {
   const [membershipState, setMembershipState] =
-    useState(memberApplicationState.SELECT_MEMBERSHIP_PLAN)
-  const [selectedPlan, setSelectedPlan] = useState()
+    useState(memberApplicationState.SELECT_MEMBERSHIP_PLAN);
+  const [selectedPlan, setSelectedPlan] = useState();
   const appProps = {
     setMembershipState,
     selectedPlan,
     setSelectedPlan
-  }
+  };
   const membershipArray = [
     {
-      isShown: membershipState === memberApplicationState.SELECT_MEMBERSHIP_PLAN,
+      isShown:
+        membershipState=== memberApplicationState.SELECT_MEMBERSHIP_PLAN,
       Component: <MembershipPlan {...appProps} />
     },
     {
@@ -28,7 +29,7 @@ export default function MembershipApplication() {
       isShown: membershipState === memberApplicationState.CONFIRMATION,
       Component: <ConfirmationPage />
     }
-  ]
+  ];
 
   return (
     <div>
@@ -45,8 +46,8 @@ export default function MembershipApplication() {
               </div>
             </CSSTransition>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
