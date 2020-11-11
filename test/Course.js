573bb65a6053638918d0c2e194f7619896946ff9
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-11-10 22:43:37 -0800

    Completed lesson and course tests

---
+++ test/Course.js
@@ -0,0 +1,215 @@
+/* global describe it before after */
+// During the test the env variable is set to test
+process.env.NODE_ENV = "test";
+const Course = require("../api/main_endpoints/models/Course");
+const User = require("../api/main_endpoints/models/User");
+// Require the dev-dependencies
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const constants = require("../api/util/constants");
+const { OK, BAD_REQUEST, UNAUTHORIZED, NOT_FOUND } = constants.STATUS_CODES;
+const SceApiTester = require("./util/tools/SceApiTester");
+
+let app = null;
+let test = null;
+const expect = chai.expect;
+// tools for testing
+const tools = require("./util/tools/tools.js");
+const {
+  initializeMock,
+  setTokenStatus,
+  resetMock,
+  restoreMock
+} = require("./util/mocks/TokenValidFunctions");
+
+chai.should();
+chai.use(chaiHttp);
+
+describe("Course", () => {
+  before(done => {
+    initializeMock();
+    app = tools.initializeServer(
+      __dirname + "/../api/main_endpoints/routes/Course.js"
+    );
+    test = new SceApiTester(app);
+    // Before each test we empty the database
+    tools.emptySchema(Course);
+    tools.emptySchema(User);
+    done();
+  });
+
+  after(done => {
+    restoreMock();
+    tools.terminateServer(done);
+  });
+
+  beforeEach(() => {
+    setTokenStatus(false);
+  });
+
+  afterEach(() => {
+    resetMock();
+  });
+
+  const token = "";
+  let courseId = "";
+  const VALID_NEW_COURSE = {
+    title: "intro to react",
+    author: "Big Chungus",
+    description: "a workshop about react",
+    // what do i need to include here? should i import lessons?
+    lessons: [hooks, frontend],
+    link: "https://google.com"
+  };
+  const COURSE_WITH_INVALID_TOKEN = {
+    token: "invalid"
+  };
+  const COURSE_WITHOUT_REQUIRED_FIELDS = {
+    title: "intro to react"
+  };
+  const COURSE_WITH_INVALID_ID = {
+    id: "strawberry"
+  };
+  const UPDATED_COURSE = {
+    title: "intro to react",
+    author: "Big Chungus",
+    description: "a workshop about react",
+    // what do i need to include here? should i import lessons?
+    lessons: [hooks, frontend],
+    link: "https://google.com"
+  };
+
+  describe("/POST createCourse", () => {
+    it("Should return 403 when an invalid token is supplied", async () => {
+      const result = await test.sendPostRequest(
+        "/api/course/createCourse",
+        COURSE_WITH_INVALID_TOKEN
+      );
+      expect(result).to.have.status(UNAUTHORIZED);
+    });
+    it("Should return 400 when the required fields aren't filled in", async () => {
+      setTokenStatus(true);
+      const result = await test.sendPostRequestWithToken(
+        token,
+        "/api/course/createCourse",
+        COURSE_WITHOUT_REQUIRED_FIELDS
+      );
+      expect(result).to.have.status(BAD_REQUEST);
+    });
+    it(
+      "Should return statusCode 200 when all required " +
+        "fields are filled in",
+      async () => {
+        setTokenStatus(true);
+        const result = await test.sendPostRequestWithToken(
+          token,
+          "/api/course/createCourse",
+          VALID_NEW_COURSE
+        );
+        expect(result).to.have.status(OK);
+      }
+    );
+  });
+
+  describe("/GET getCourses", () => {
+    it("Should return an object of all courses", async () => {
+      setTokenStatus(true);
+      const result = await test.sendGetRequest("/api/course/getCourses");
+      expect(result).to.have.status(OK);
+      const getCoursesResponse = result.body;
+      getCoursesResponse.should.be.a("array");
+      expect(getCoursesResponse).to.have.length(1);
+      expect(getCoursesResponse[0].title).to.equal(VALID_NEW_COURSE.title);
+      expect(getCoursesResponse[0].author).to.equal(VALID_NEW_COURSE.author);
+      expect(getCoursesResponse[0].description).to.equal(
+        VALID_NEW_COURSE.description
+      );
+      expect(getCoursesResponse[0].lessons).to.equal(VALID_NEW_COURSE.lessons);
+      expect(getCoursesResponse[0].link).to.equal(VALID_NEW_COURSE.link);
+      courseId = getCoursesResponse[0]._id;
+    });
+  });
+
+  describe("/POST editCourse", () => {
+    it("Should return 403 when an invalid token is supplied", async () => {
+      const result = await test.sendPostRequestWithToken(
+        token,
+        "/api/course/editCourse",
+        COURSE_WITH_INVALID_TOKEN
+      );
+      expect(result).to.have.status(UNAUTHORIZED);
+    });
+    it(
+      "Should return 404 when a course by an " + "invalid id isn't found",
+      async () => {
+        setTokenStatus(true);
+        const result = await test.sendPostRequestWithToken(
+          token,
+          "/api/course/editCourse",
+          COURSE_WITH_INVALID_ID
+        );
+        expect(result).to.have.status(NOT_FOUND);
+      }
+    );
+    it("Should return 200 when a course is sucessfully updated", async () => {
+      setTokenStatus(true);
+      const result = await test.sendPostRequestWithToken(
+        token,
+        "/api/course/editCourse",
+        { id: courseId, ...UPDATED_COURSE }
+      );
+      expect(result).to.have.status(OK);
+    });
+    it("The update should be reflected in the database", async () => {
+      setTokenStatus(true);
+      const result = await test.sendGetRequest("/api/course/getCourses");
+      expect(result).to.have.status(OK);
+      const getCoursesResponse = result.body;
+      expect(getCoursesResponse).to.have.length(1);
+      expect(getCoursesResponse[0].title).to.equal(UPDATED_COURSE.title);
+      expect(getCoursesResponse[0].author).to.equal(UPDATED_COURSE.author);
+      expect(getCoursesResponse[0].description).to.equal(
+        UPDATED_COURSE.description
+      );
+      expect(getCoursesResponse[0].lessons).to.equal(UPDATED_COURSE.lessons);
+      expect(getCoursesResponse[0].link).to.equal(UPDATED_COURSE.link);
+    });
+  });
+
+  describe("/POST deleteCourse", () => {
+    it("Should return 403 when an invalid token is supplied", async () => {
+      const result = await test.sendPostRequestWithToken(
+        token,
+        "/api/course/deleteCourse",
+        COURSE_WITH_INVALID_TOKEN
+      );
+      expect(result).to.have.status(UNAUTHORIZED);
+    });
+    it("Should return 400 when a course is unsucessfully deleted", async () => {
+      setTokenStatus(true);
+      const result = await test.sendPostRequestWithToken(
+        token,
+        "/api/course/deleteCourse",
+        COURSE_WITH_INVALID_ID
+      );
+      expect(result).to.have.status(BAD_REQUEST);
+    });
+    it("Should return 200 when a course is sucessfully deleted", async () => {
+      setTokenStatus(true);
+      const result = await test.sendPostRequestWithToken(
+        token,
+        "/api/course/deleteCourse",
+        { id: courseId }
+      );
+      expect(result).to.have.status(OK);
+    });
+    it("The deleted item should be reflected in the database", async () => {
+      setTokenStatus(true);
+      const result = await test.sendGetRequest("/api/course/getCourses");
+      expect(result).to.have.status(OK);
+      const getCoursesResponse = result.body;
+      getCoursesResponse.should.be.a("array");
+      expect(getCoursesResponse).to.have.length(0);
+    });
+  });
+});
--- test/Lesson.js
+++ test/Lesson.js
@@ -1,39 +1,39 @@
 /* global describe it before after */
 // During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
-const Event = require('../api/main_endpoints/models/Event');
-const User = require('../api/main_endpoints/models/User');
+process.env.NODE_ENV = "test";
+const Lesson = require("../api/main_endpoints/models/Lesson");
+const User = require("../api/main_endpoints/models/User");
 // Require the dev-dependencies
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const constants = require('../api/util/constants');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const constants = require("../api/util/constants");
 const { OK, BAD_REQUEST, UNAUTHORIZED, NOT_FOUND } = constants.STATUS_CODES;
-const { DEFAULT_PHOTO_URL } = constants;
-const SceApiTester = require('./util/tools/SceApiTester');
+const SceApiTester = require("./util/tools/SceApiTester");
 
 let app = null;
 let test = null;
 const expect = chai.expect;
 // tools for testing
-const tools = require('./util/tools/tools.js');
+const tools = require("./util/tools/tools.js");
 const {
   initializeMock,
   setTokenStatus,
   resetMock,
-  restoreMock,
-} = require('./util/mocks/TokenValidFunctions');
+  restoreMock
+} = require("./util/mocks/TokenValidFunctions");
 
 chai.should();
 chai.use(chaiHttp);
 
-describe('Event', () => {
+describe("Lesson", () => {
   before(done => {
     initializeMock();
     app = tools.initializeServer(
-      __dirname + '/../api/main_endpoints/routes/Event.js');
+      __dirname + "/../api/main_endpoints/routes/Lesson.js"
+    );
     test = new SceApiTester(app);
     // Before each test we empty the database
-    tools.emptySchema(Event);
+    tools.emptySchema(Lesson);
     tools.emptySchema(User);
     done();
   });
@@ -51,159 +51,147 @@ describe('Event', () => {
     resetMock();
   });
 
-  const token = '';
-  let eventId = '';
-  const VALID_NEW_EVENT = {
-    title: 'ros masters united',
-    eventLocation: 'SU 4A',
-    eventDate: new Date('5/25/20'),
-    startTime: '12:00',
-    endTime: '13:00',
-    eventCategory: 'workshop'
+  const token = "";
+  let lessonId = "";
+  const VALID_NEW_LESSON = {
+    title: "intro to react",
+    link: "https://google.com"
   };
-  const EVENT_WITH_INVALID_TOKEN = {
-    token: 'invalid'
+  const LESSON_WITH_INVALID_TOKEN = {
+    token: "invalid"
   };
-  const EVENT_WITHOUT_REQUIRED_FIELDS = {
-    eventDate: new Date('5/25/20')
+  const LESSON_WITHOUT_REQUIRED_FIELDS = {
+    title: "intro to react"
   };
-  const EVENT_WITH_INVALID_ID = {
-    id: 'strawberry'
+  const LESSON_WITH_INVALID_ID = {
+    id: "strawberry"
   };
-  const UPDATED_EVENT = {
-    title: 'ros masters divided',
-    eventLocation: 'SU 4B',
-    eventDate: new Date('5/27/20'),
-    startTime: '13:00',
-    endTime: '14:00',
-    eventCategory: 'game night',
-    imageURL: 'https://link.to/pdf'
+  const UPDATED_LESSON = {
+    title: "intro to react",
+    link: "https://google.com"
   };
 
-  describe('/POST createEvent', () => {
-    it('Should return 403 when an invalid token is supplied', async () => {
+  describe("/POST createLesson", () => {
+    it("Should return 403 when an invalid token is supplied", async () => {
       const result = await test.sendPostRequest(
-        '/api/event/createEvent', EVENT_WITH_INVALID_TOKEN);
+        "/api/lesson/createLesson",
+        LESSON_WITH_INVALID_TOKEN
+      );
       expect(result).to.have.status(UNAUTHORIZED);
     });
-    it('Should return 400 when the required fields aren\'t filled in',
+    it("Should return 400 when the required fields aren't filled in", async () => {
+      setTokenStatus(true);
+      const result = await test.sendPostRequestWithToken(
+        token,
+        "/api/lesson/createLesson",
+        LESSON_WITHOUT_REQUIRED_FIELDS
+      );
+      expect(result).to.have.status(BAD_REQUEST);
+    });
+    it(
+      "Should return statusCode 200 when all required " +
+        "fields are filled in",
       async () => {
         setTokenStatus(true);
         const result = await test.sendPostRequestWithToken(
-          token, '/api/event/createEvent', EVENT_WITHOUT_REQUIRED_FIELDS);
-        expect(result).to.have.status(BAD_REQUEST);
-      });
-    it('Should return statusCode 200 when all required ' +
-       'fields are filled in', async () => {
+          token,
+          "/api/lesson/createLesson",
+          VALID_NEW_LESSON
+        );
+        expect(result).to.have.status(OK);
+      }
+    );
+  });
+
+  describe("/GET getLessons", () => {
+    it("Should return an object of all lessons", async () => {
       setTokenStatus(true);
-      const result = await test.sendPostRequestWithToken(
-        token, '/api/event/createEvent', VALID_NEW_EVENT);
+      const result = await test.sendGetRequest("/api/lesson/getLessons");
       expect(result).to.have.status(OK);
+      const getLessonsResponse = result.body;
+      getLessonsResponse.should.be.a("array");
+      expect(getLessonsResponse).to.have.length(1);
+      expect(getLessonsResponse[0].title).to.equal(VALID_NEW_LESSON.title);
+      expect(getLessonsResponse[0].link).to.equal(VALID_NEW_LESSON.link);
+      lessonId = getLessonsResponse[0]._id;
     });
   });
 
-  describe('/GET getEvents', () => {
-    it('Should return an object of all events', async () => {
-      setTokenStatus(true);
-      const result = await test.sendGetRequest(
-        '/api/event/getEvents');
-      expect(result).to.have.status(OK);
-      const getEventsResponse = result.body;
-      getEventsResponse.should.be.a('array');
-      expect(getEventsResponse).to.have.length(1);
-      expect(getEventsResponse[0].title).to.equal(VALID_NEW_EVENT.title);
-      expect(getEventsResponse[0].eventLocation).to.equal(
-        VALID_NEW_EVENT.eventLocation
-      );
-      expect(getEventsResponse[0].eventDate).to.equal(
-        VALID_NEW_EVENT.eventDate.toISOString()
-      );
-      expect(getEventsResponse[0].startTime).to.equal(
-        VALID_NEW_EVENT.startTime
-      );
-      expect(getEventsResponse[0].endTime).to.equal(
-        VALID_NEW_EVENT.endTime
-      );
-      expect(getEventsResponse[0].eventCategory).to.equal(
-        VALID_NEW_EVENT.eventCategory
-      );
-      expect(getEventsResponse[0].imageURL).to.equal(DEFAULT_PHOTO_URL);
-      eventId = getEventsResponse[0]._id;
-    });
-  });
-
-  describe('/POST editEvent', () => {
-    it('Should return 403 when an invalid token is supplied', async () => {
+  describe("/POST editLesson", () => {
+    it("Should return 403 when an invalid token is supplied", async () => {
       const result = await test.sendPostRequestWithToken(
-        token, '/api/event/editEvent', EVENT_WITH_INVALID_TOKEN);
+        token,
+        "/api/lesson/editLesson",
+        LESSON_WITH_INVALID_TOKEN
+      );
       expect(result).to.have.status(UNAUTHORIZED);
     });
-    it('Should return 404 when an event by an ' +
-       'invalid id isn\'t found', async () => {
+    it(
+      "Should return 404 when a lesson by an " + "invalid id isn't found",
+      async () => {
+        setTokenStatus(true);
+        const result = await test.sendPostRequestWithToken(
+          token,
+          "/api/lesson/editLesson",
+          LESSON_WITH_INVALID_ID
+        );
+        expect(result).to.have.status(NOT_FOUND);
+      }
+    );
+    it("Should return 200 when a lesson is sucessfully updated", async () => {
       setTokenStatus(true);
       const result = await test.sendPostRequestWithToken(
-        token, '/api/event/editEvent', EVENT_WITH_INVALID_ID);
-      expect(result).to.have.status(NOT_FOUND);
-    });
-    it('Should return 200 when an event is sucessfully updated', async () => {
-      setTokenStatus(true);
-      const result = await test.sendPostRequestWithToken(
-        token, '/api/event/editEvent', {id: eventId, ...UPDATED_EVENT});
+        token,
+        "/api/lesson/editLesson",
+        { id: lessonId, ...UPDATED_LESSON }
+      );
       expect(result).to.have.status(OK);
     });
-    it('The update should be reflected in the database', async () => {
+    it("The update should be reflected in the database", async () => {
       setTokenStatus(true);
-      const result = await test.sendGetRequest(
-        '/api/event/getEvents');
+      const result = await test.sendGetRequest("/api/lesson/getLessons");
       expect(result).to.have.status(OK);
-      const getEventsResponse = result.body;
-      expect(getEventsResponse).to.have.length(1);
-      expect(getEventsResponse[0].title).to.equal(UPDATED_EVENT.title);
-      expect(getEventsResponse[0].eventLocation).to.equal(
-        UPDATED_EVENT.eventLocation
-      );
-      expect(getEventsResponse[0].eventDate).to.equal(
-        UPDATED_EVENT.eventDate.toISOString()
-      );
-      expect(getEventsResponse[0].startTime).to.equal(
-        UPDATED_EVENT.startTime
-      );
-      expect(getEventsResponse[0].endTime).to.equal(UPDATED_EVENT.endTime);
-      expect(getEventsResponse[0].eventCategory).to.equal(
-        UPDATED_EVENT.eventCategory
-      );
-      expect(getEventsResponse[0].imageURL).to.equal(
-        UPDATED_EVENT.imageURL
-      );
+      const getLessonsResponse = result.body;
+      expect(getLessonsResponse).to.have.length(1);
+      expect(getLessonsResponse[0].title).to.equal(UPDATED_LESSON.title);
+      expect(getLessonsResponse[0].link).to.equal(UPDATED_LESSON.link);
     });
   });
 
-  describe('/POST deleteEvent', () => {
-    it('Should return 403 when an invalid token is supplied', async () => {
+  describe("/POST deleteLesson", () => {
+    it("Should return 403 when an invalid token is supplied", async () => {
       const result = await test.sendPostRequestWithToken(
-        token, '/api/event/deleteEvent', EVENT_WITH_INVALID_TOKEN);
+        token,
+        "/api/lesson/deleteLesson",
+        LESSON_WITH_INVALID_TOKEN
+      );
       expect(result).to.have.status(UNAUTHORIZED);
     });
-    it('Should return 400 when an event is unsucessfully deleted', async () => {
+    it("Should return 400 when a lesson is unsucessfully deleted", async () => {
       setTokenStatus(true);
       const result = await test.sendPostRequestWithToken(
-        token, '/api/event/deleteEvent', EVENT_WITH_INVALID_ID);
+        token,
+        "/api/lesson/deleteLesson",
+        LESSON_WITH_INVALID_ID
+      );
       expect(result).to.have.status(BAD_REQUEST);
     });
-    it('Should return 200 when an event is sucessfully deleted', async () => {
+    it("Should return 200 when a lesson is sucessfully deleted", async () => {
       setTokenStatus(true);
       const result = await test.sendPostRequestWithToken(
-        token, '/api/event/deleteEvent', {id: eventId});
+        token,
+        "/api/lesson/deleteLesson",
+        { id: lessonId }
+      );
       expect(result).to.have.status(OK);
     });
-    it('The deleted item should be reflected in the database', async () => {
+    it("The deleted item should be reflected in the database", async () => {
       setTokenStatus(true);
-      const result = await test.sendGetRequest(
-        '/api/event/getEvents');
+      const result = await test.sendGetRequest("/api/lesson/getLessons");
       expect(result).to.have.status(OK);
-      const getEventsResponse = result.body;
-      getEventsResponse.should.be.a('array');
-      expect(getEventsResponse).to.have.length(0);
+      const getLessonsResponse = result.body;
+      getLessonsResponse.should.be.a("array");
+      expect(getLessonsResponse).to.have.length(0);
     });
   });
 });
