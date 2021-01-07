2628efd339a022a2b4c55b5e11050f0ca36b0721
Farah Masood <farah.masood@sjsu.edu>
2021-01-07 12:58:02 -0800

    Zipping files with date

--- api/weekly_dump.sh
+++ api/weekly_dump.sh
@@ -4,5 +4,8 @@ export PATH=/bin:/usr/bin:/usr/local/bin
 DATABASE_NAMES='ALL'
 if [ ${DATABASE_NAMES} = "ALL" ]; then
  echo "Backing up databases"
- mongodump
+ mkdir /Users/farahmasood/Desktop/dump/$(date +"%d%b%Y")
+ mongodump -o /Users/farahmasood/Desktop/dump/$(date +"%d%b%Y")
+ zip -r $(date +"%d%b%Y").zip /Users/farahmasood/Desktop/dump/$(date +"%d%b%Y")
+ mv /Users/farahmasood/Documents/GitHub/Core-v4/07Jan2021.zip /Users/farahmasood/Desktop/dump
 fi
