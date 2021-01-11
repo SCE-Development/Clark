c874394fdd4c50cb63a21838645a84677bc3fd46
isita <isita.bagayatkar@gmail.com>
2021-01-11 13:51:48 -0800

    add description

--- api/weekly_dump.sh
+++ api/weekly_dump.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 export PATH=/bin:/usr/bin:/usr/local/bin
-# change DUMP_DIR to the absolute path to mongo dumps directory
+# change DUMP_DIR
 DUMP_DIR="/Users/admin/Desktop/sce/dbdump"
-PROJECT_DIR=$PWD
 DB_NAME="sce_core"
 echo "Backing up databases"
-mongodump --db $DB_NAME --gzip --archive > $DUMP_DIR/$(date +"%d%b%Y")_$DB_NAME.gzip
+mongodump --db $DB_NAME --gzip --archive > $DUMP_DIR/$(date +"%d%b%Y")_$DB_NAME.gz
 echo "Done backing up sce_core"
+# restore command
+# mongorestore --gzip --archive=$DUMP_DIR/<dumpfilename> --db sce_core --noIndexRestore --drop
+# example: mongorestore --gzip --archive=/Users/admin/Desktop/sce/dbdump/11Jan2021.gz --db sce_core --noIndexRestore --drop
+# 
+# Running this script weekly using a crontab:
+# command line: sudo crontab -u <user> -e               # edit crontab
+# inside crontab: 0 0 * * 0 root <pathtothisscript>     # runs script on Sundays at 00:00 
