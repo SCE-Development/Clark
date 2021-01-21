589b6b521d91ae85d8fcdbf485b9b3c77d8306ef
surabhi gupta <surabhi2029@gmail.com>
2021-01-20 18:40:42 -0800

    Npm lint

--- api/cloud_api/server.js
+++ api/cloud_api/server.js
@@ -2,7 +2,7 @@ const { model } = require('mongoose');
 const { SceHttpServer } = require('../util/SceHttpServer');
 
 function main() {
-  const API_ENDPOINTS =  __dirname + '/routes/';
+  const API_ENDPOINTS = __dirname + '/routes/';
   const cloudServer = new SceHttpServer(API_ENDPOINTS, 8082, '/cloudapi/');
   cloudServer.initializeEndpoints().then(() => {
     cloudServer.openConnection();
