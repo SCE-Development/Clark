7cbf4c15626435416747f225674f4ff8d49225c4
Justin Zhu <justin.zhu@sjsu.edu>
2021-03-21 12:17:46 -0700

    Fixed table responsiveness

--- src/Pages/Inventory/InventoryRow.js
+++ src/Pages/Inventory/InventoryRow.js
@@ -5,25 +5,25 @@ export default class InventoryRow extends Component {
   render() {
     return (
       <tr>
-        <td>
+        <td className="row-image">
           <img
             className='item-image'
             src={this.props.item.picture} alt=''
           />
         </td>
-        <td>
+        <td className="row-name">
           {this.props.item.name}
         </td>
-        <td>
+        <td className="row-category">
           {this.props.item.category}
         </td>
-        <td>
+        <td className="row-price">
           {'$'+this.props.item.price.toFixed(2)}
         </td>
-        <td>
+        <td className="row-quantity">
           {this.props.item.stock}
         </td>
-        <td>
+        <td className = "row-description">
           {this.props.item.description}
         </td>
         <td>
