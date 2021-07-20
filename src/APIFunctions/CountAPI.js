4c400d8db19d7440d525bc84c92b5a302c3545cc
laurenmacpherson <lauren.macpherson@sjsu.edu>
2021-07-19 17:08:39 -0700

    update

--- src/APIFunctions/CountAPI.js
+++ src/APIFunctions/CountAPI.js
@@ -17,11 +17,26 @@
 /** 
  * Creates a JSON object when an API endpoint is called 
  * @param {string} name the name of the API that was called
- * @param {bool} error whether the API caused an error or not 
- * @param {string} userID the ID of the user that called the function
- * @param {string} description description of what the API does?
+ * @param {bool} err whether the API caused an error or not 
+ * @param {string} ID the ID of the user that called the function
+ * @param {string} descr description of what the API does?
  * @returns {object} a json object containing all the relevant raw data 
 */
-export async function dataAPI(){
+export async function dataAPI(name, err, ID, descr){
     //find a way to catch the current date and time when this function is called 
+    let formatDay = new Date(); 
+    let date = formatDay.getFullYear()+'-'+(formatDay.getMonth()+1)+'-'+formatDay.getDate();
+
+    let time = formatDay.getHours() + ":" + formatDay.getMinutes() + ":" + formatDay.getSeconds();
+
+    const {
+        api_name, 
+        error, 
+        date, 
+        time, 
+        userID, 
+        description
+    } = myJSON; //TODO: Change name of this object 
+
+    return myJSON;
 }
