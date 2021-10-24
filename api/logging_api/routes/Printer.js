fcb3cccdf20862d8f1a22f1b96426fe16bd3fcf5
Allen Tran <allentran08@gmail.com>
2021-10-24 10:41:02 -0700

    lint fixes

--- api/logging_api/routes/Printer.js
+++ api/logging_api/routes/Printer.js
@@ -1,33 +1,29 @@
-const fs = require('fs');
 const express = require('express');
-const { exec } = require('child_process');
 const router = express.Router();
-const axios = require('axios')
-const { OK, BAD_REQUEST } = require('../../util/constants').STATUS_CODES;
+const axios = require('axios');
+const {OK} = require('../../util/constants').STATUS_CODES;
 
 router.post('/healthCheck', async (req, res) => {
-    await axios.get("http://localhost:8000" + '/healthCheck')
+  await axios.get('http://localhost:8000' + '/healthCheck')
     .then(result => {
       reponseData = res.data;
-      res.sendStatus(200);
+      res.sendStatus(OK);
     })
     .catch(err => {
-      console.error('Health check error: ', err);
       responseData = err;
       error = true;
     });
 });
 
 router.post('/sendPrintRequest', async (req, res) => {
-  await axios.post("http://localhost:8000" + '/sendPrintRequest', req.body)
+  await axios.post('http://localhost:8000' + '/sendPrintRequest', req.body)
     .then(result => {
       reponseData = res.data;
-      res.sendStatus(200);
+      res.sendStatus(OK);
     })
     .catch(err => {
-      console.error('Print request error: ', err);
       responseData = err;
       error = true;
     });
-  });
+});
 module.exports = router;
