aa01febfde3cb0f152100c556d49dc6b1bba41c0
Allen Tran <63386979+allen-tran@users.noreply.github.com>
2021-12-14 22:23:55 -0800

    Removes Deployment to Treescale File (#917)
    * Removal of deployment to treescale file
    
    * Oops
    
    * accident
    
    * Update package-lock.json
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    Co-authored-by: Allen Tran <allentran@allens-macbook.local>
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

--- deploy_to_treescale.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Push to TreeScale, env vars are found in travis-ci.com as secret env vars.
-docker push repo.treescale.com/devscesjsu/sce-core-backend-dev:staging
-docker push repo.treescale.com/devscesjsu/sce-core-frontend-dev:staging
