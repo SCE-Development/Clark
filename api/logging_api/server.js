c66f3d27df2af08135b20401a0ba5dcc273da7c9
Allen Tran <63386979+allen-tran@users.noreply.github.com>
2021-12-17 12:01:23 -0800

    Printing SQS Write + S3 File Upload (#907)

--- api/logging_api/server.js
+++ api/logging_api/server.js
@@ -4,9 +4,11 @@ function main() {
   const API_ENDPOINTS = [
     __dirname + '/routes/ErrorLog.js',
     __dirname + '/routes/PrintLog.js',
-    __dirname + '/routes/SignLog.js'
+    __dirname + '/routes/SignLog.js',
+    __dirname + '/routes/Printer.js'
+
   ];
-  const loggingServer = new SceHttpServer(API_ENDPOINTS, 8081, '/logapi/');
+  const loggingServer = new SceHttpServer(API_ENDPOINTS, 8081, '/logging_api/');
   loggingServer.initializeEndpoints().then(() => {
     loggingServer.openConnection();
   });
