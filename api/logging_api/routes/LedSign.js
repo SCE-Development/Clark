7e1b084453282659f7fcc346a4ea7c7f0c70bf2a
andrewtedi <40414974+andrewtedi@users.noreply.github.com>
2021-12-22 14:19:01 -0800

    Using SQS to send writing requests to LED Sign (#908)
    * created routes for LedSign through http requests
    
    * oops forgot linting exists
    
    * some edit to ledsign.js
    
    * oops all linting errors
    
    * sqs temp files
    
    * removed rebase conflicts
    
    * finished rebasing
    
    * well there happened to be more rebasing
    
    * revert package.json edits
    
    * remove unneccessary files
    
    * Run github actions on merge
    
    Similar to [SCE-discord-bot](https://github.com/SCE-Development/SCE-discord-bot), it would be nice to have a green check to show off our code on dev as passing. As of now, we only run the GitHub Actions build when a PR is opened. This changes it to also have the build be ran when dev is updated.
    
    * removed space around dev
    
    * put spaces back
    
    * moved token-functions to util folder
    
    * i messed up
    
    * updated paths +mocha tests pass
    
    * grrrrr
    
    * GRRRRR
    
    * just let me please
    
    * Passes all API tests. Reset package-lock + package to dev branch.
    
    * created routes for LedSign through http requests
    
    * oops forgot linting exists
    
    * some edit to ledsign.js
    
    * oops all linting errors
    
    * sqs temp files
    
    * minor edits
    
    * package changes
    
    * remove unneccessary files
    
    * rebase changes
    
    * more rebasing
    
    * some edit to ledsign.js
    
    * oops all linting errors
    
    * sqs temp files
    
    * removed rebase conflicts
    
    * finished rebasing
    
    * more package stuff lol
    
    * remove unneccessary files
    
    * fixed linting errors
    
    * removed excess code
    
    * added error checking in updateSignText
    
    * fixing lint
    
    * added temporary fix to health check
    
    * removed extra code
    
    * used ledSignUrl in config.json instead of config.js
    
    * removed axios from ledSign
    
    Co-authored-by: Allen Tran <allentran@allens-macbook.local>

---
+++ api/logging_api/routes/LedSign.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const { OK, BAD_REQUEST } = require('../../util/constants').STATUS_CODES;
+const AWS = require('aws-sdk');
+const { ledSqsKeys, ledSignUrl } = require('../../config/config.json');
+
+creds = new AWS.Credentials(ledSqsKeys.CLIENT_ID, ledSqsKeys.CLIENT_SECRET);
+AWS.config.update({
+  region: 'us-west-1',
+  endpoint: ledSignUrl,
+  credentials: creds
+});
+
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+const queueUrl =
+'https://sqs.us-west-2.amazonaws.com/'
++ ledSqsKeys.ACCOUNT_ID + '/' + ledSqsKeys.QUEUE_NAME;
+
+router.get('/healthCheck', (req, res) => {
+  res.sendStatus(OK);
+});
+
+router.post('/updateSignText', (req, res) => {
+  const sqsParams = {
+    MessageBody: JSON.stringify(
+      req.body
+    ),
+    QueueUrl: queueUrl
+  };
+  sqs.sendMessage(sqsParams, function(err, data) {
+    if(err) {
+      res.sendStatus(BAD_REQUEST);
+    } else {
+      res.sendStatus(OK);
+    }
+  });
+});
+
+
+module.exports = router;
