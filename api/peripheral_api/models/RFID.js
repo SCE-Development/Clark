fba9db9d56de0cbddfff8bb82b3b8a2cff281531
Neel Patel <np.patelneel@gmail.com>
2022-01-27 13:50:20 -0800

    Add methods to only store a hash of the RFID identifier and add compare method

--- api/peripheral_api/models/RFID.js
+++ api/peripheral_api/models/RFID.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const RFIDSchema = new Schema(
+const RfidSchema = new Schema(
   {
     name: {
       type: String,
       required: true,
     },
+    // User's RFID card's indentifier a.k.a byte
     byte: {
       type: String,
       unique: true,
@@ -24,4 +25,31 @@ const RFIDSchema = new Schema(
   { collection: 'RFID' }
 );
 
-module.exports = mongoose.model('RFID', RFIDSchema);
+RfidSchema.pre('save', function(next) {
+  if (this.isModified('byte') || this.isNew) {
+    bcrypt.genSalt(10, function(error, salt) {
+      if (error) {
+        return next(error);
+      }
+      bcrypt.hash(this.byte, salt, function(error, hash) {
+        if (error) {
+          return next(error);
+        }
+        this.byte = hash;
+      });
+    });
+  }
+  return next();
+});
+
+RfidSchema.methods.compareByte = function(byte, callback) {
+  bcrypt.compare(byte, this.byte, function(error, isMatch) {
+    if (error) {
+      return callback(error);
+    }
+
+    callback(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('RFID', RfidSchema);
