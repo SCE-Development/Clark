baa5701d16e92a688e5613c80bbdc0ffb9a7e1b2
Neel Patel <np.patelneel@gmail.com>
2022-01-27 12:51:49 -0800

    created RFID schema

---
+++ api/peripheral_api/models/RFID.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RFIDSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    byte: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+    lastScanned : {
+      type: Date,
+      default: Date.now,
+    }
+  },
+  { collection: 'RFID' }
+);
+
+module.exports = mongoose.model('RFID', RFIDSchema);
