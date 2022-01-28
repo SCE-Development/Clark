a99b92c6b424391616d5c723227ab2cf41078826
andrewtedi <andrew.tedijanto@gmail.com>
2022-01-27 17:37:37 -0800

    Made changes for setting up Linux and Mac

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@ import subprocess
 import platform
 import os
 
-def write_alias_to_file(file_name):
-    try:
-        subprocess.check_call(
-            f'grep -rl \"alias ESLINT_NO_DEV_ERRORS=true\"',
-            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-    except subprocess.CalledProcessError:
-        with open(file_name, 'a') as file:
-            file.write('\n')
-            file.write(f"export ESLINT_NO_DEV_ERRORS=true")
-            file.write('\n')
+def write_to_file(file_name):
+    with open(file_name, 'a') as file:
+        file.write('\n')
+        file.write("export ESLINT_NO_DEV_ERRORS=true")
+        file.write('\n')
+    print(f"\n{file_name} written! " +
+        "Open a new terminal after completing setup for changes to be in effect.")
 
 def add_alias_unix():
     HOME_PATH = os.environ["HOME"]
@@ -19,15 +16,15 @@ def add_alias_unix():
     if platform.system() == "Darwin":
         ZSHRC_PATH = f"{HOME_PATH}/.zshrc"
         if os.path.isfile(BASHRC_PATH):
-            write_alias_to_file(BASHRC_PATH)
+            write_to_file(BASHRC_PATH)
         if os.path.isfile(ZSHRC_PATH):
-            write_alias_to_file(ZSHRC_PATH)
+            write_to_file(ZSHRC_PATH)
     elif platform.system() == "Linux":
         BASH_PROFILE_PATH = f"{HOME_PATH}/.bash_profile"
         if os.path.isfile(BASHRC_PATH):
-            write_alias_to_file(BASHRC_PATH)
+            write_to_file(BASHRC_PATH)
         elif os.path.isfile(BASH_PROFILE_PATH):
-            write_alias_to_file(BASH_PROFILE_PATH)
+            write_to_file(BASH_PROFILE_PATH)
 
 def add_alias_windows():
     subprocess.check_call("setx ESLINT_NO_DEV_ERRORS true",
