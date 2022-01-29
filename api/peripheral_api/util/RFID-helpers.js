c1bca96adc03734d00d6f873617ba428b373c2c5
Neel Patel <np.patelneel@gmail.com>
2022-01-28 19:15:41 -0800

    fix lint errors

--- api/peripheral_api/util/RFID-helpers.js
+++ api/peripheral_api/util/RFID-helpers.js
@@ -2,13 +2,12 @@ const bcrypt = require('bcryptjs');
 const { RFID_BCRYPT_SALT } = require('../../config/config.json');
 function hashedByte(byte) {
   return new Promise((resolve, reject) => {
-    bcrypt.genSalt(10, function (error, salt) {
+    bcrypt.genSalt(10, function(error, salt) {
       if (error) {
         resolve(null);
       }
-      bcrypt.hash(byte, RFID_BCRYPT_SALT, function (hashError, hash) {
+      bcrypt.hash(byte, RFID_BCRYPT_SALT, function(hashError, hash) {
         if (hashError) {
-          console.log(hashError)
           resolve(null);
         }
         resolve(hash);
