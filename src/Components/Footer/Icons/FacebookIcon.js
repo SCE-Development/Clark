dc02d1c362ef8bedb39db66ce2c18a8c1ab83caa
Neel Patel <np.patelneel@gmail.com>
2022-04-15 21:25:13 -0700

    svgs --> react component

---
+++ src/Components/Footer/Icons/FacebookIcon.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function FacebookIcon() {
+  return (
+    <svg width='50' height='50' viewBox='0 0 50 50'>
+      <path
+        fill='#3F51B5'
+        d='M42 37a5 5 0 01-5 5H11a5 5 0 01-5-5V11a5 5 0 015-5h26a5 5 0
+        015 5v26z'
+      ></path>
+      <path
+        fill='#FFF'
+        d='M34.368 25H31v13h-5V25h-3v-4h3v-2.41c.002-3.508 1.459-5.59 5.592
+        -5.59H35v4h-2.287C31.104 17 31 17.6 31 18.723V21h4l-.632 4z'
+      ></path>
+    </svg>
+  );
+}
+
+export default FacebookIcon;
