fe524826fbfd7958ec8bc9bb6e7726cb864f0bec
Neel Patel <np.patelneel@gmail.com>
2022-04-15 20:43:07 -0700

    remove changes in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,132 +8,56 @@ export default () => {
       <Row className='footer-text'>
         <Col>
           <b>Software & Computer Engineering Society</b>
-          <ul>
-            <li>
-              San Jose State University
-            </li>
-            <li>
-              Charles W. Davidson College of Engineering
-            </li>
-            <li>
-              Room: ENGR 294
-            </li>
-          </ul>
+          <p>San Jose State University</p>
+          <p>Charles W. Davidson College of Engineering</p>
+          <p>Room: ENGR 294</p>
         </Col>
         <Col>
           <b>Contact</b>
-          <ul>
-            <li>
-              General: <a href='mailto:asksce@gmail.com'>asksce@gmail.com</a>
-            </li>
-            <li>President:{' '}
-              <a href='mailto:sce.sjsu@gmail.com'>sce.sjsu@gmail.com</a>
-            </li>
-            <li>
-              Vice-President:{' '}
-              <a href='mailto:sce.sjsu@gmail.com'>sce.sjsu@gmail.com</a>
-            </li>
-            <li>
-              Public Relations:{' '}
-              <a href='mailto:pr.sce.sjsu@gmail.com'>pr.sce.sjsu@gmail.com</a>
-            </li>
-          </ul>
+          <p>
+            General: <a href='mailto:asksce@gmail.com'>asksce@gmail.com</a>
+          </p>
+          <p>
+            President:{' '}
+            <a href='mailto:sce.sjsu@gmail.com'>sce.sjsu@gmail.com</a>
+          </p>
+          <p>
+            Vice-President:{' '}
+            <a href='mailto:sce.sjsu@gmail.com'>sce.sjsu@gmail.com</a>
+          </p>
+          <p>
+            Public Relations:{' '}
+            <a href='mailto:pr.sce.sjsu@gmail.com'>pr.sce.sjsu@gmail.com</a>
+          </p>
         </Col>
-        <Col id='social-media'>
+        <Col>
           <a href='https://www.linkedin.com/company/sjsu-software
           -computer-engineering-society/'>
-            {/* eslint-disable */}
-            <svg width='50' height='50' viewBox='0 0 50 50'>
+            <svg width='50px' height='50px' viewBox='0 0 24 24'>
               <path
-                fill='#0078d4'
-                d='M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5	V37z' />
-              <path
-                d='M30,37V26.901c0-1.689-0.819-2.698-2.192-2.698c-0.815,0-1.414,0.459-1.779,1.364	c-0.017,0.064-0.041,0.325-0.031,1.114L26,37h-7V18h7v1.061C27.022,18.356,28.275,18,29.738,18c4.547,0,7.261,3.093,7.261,8.274	L37,37H30z M11,37V18h3.457C12.454,18,11,16.528,11,14.499C11,12.472,12.478,11,14.514,11c2.012,0,3.445,1.431,3.486,3.479	C18,16.523,16.521,18,14.485,18H18v19H11z' opacity='.05' />
-              <path
-                d='M30.5,36.5v-9.599c0-1.973-1.031-3.198-2.692-3.198c-1.295,0-1.935,0.912-2.243,1.677	c-0.082,0.199-0.071,0.989-0.067,1.326L25.5,36.5h-6v-18h6v1.638c0.795-0.823,2.075-1.638,4.238-1.638	c4.233,0,6.761,2.906,6.761,7.774L36.5,36.5H30.5z M11.5,36.5v-18h6v18H11.5z M14.457,17.5c-1.713,0-2.957-1.262-2.957-3.001	c0-1.738,1.268-2.999,3.014-2.999c1.724,0,2.951,1.229,2.986,2.989c0,1.749-1.268,3.011-3.015,3.011H14.457z'
-                opacity='.07' />
-              <path
-                fill='#fff'
-                d='M12,19h5v17h-5V19z M14.485,17h-0.028C12.965,17,12,15.888,12,14.499C12,13.08,12.995,12,14.514,12	c1.521,0,2.458,1.08,2.486,2.499C17,15.887,16.035,17,14.485,17z M36,36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698	c-1.501,0-2.313,1.012-2.707,1.99C24.957,25.543,25,26.511,25,27v9h-5V19h5v2.616C25.721,20.5,26.85,19,29.738,19	c3.578,0,6.261,2.25,6.261,7.274L36,36L36,36z' />
+                fill='#E1F5FE'
+                d='M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,
+                19V5A2,2 0 0,1 5,3H19M18.5,18.5V13.2A3.26,3.26 0 0,0 15.24,
+                9.94C14.39,9.94 13.4,10.46 12.92,
+                11.24V10.13H10.13V18.5H12.92V13.57C12.92,12.8 13.54,
+                12.17 14.31,12.17A1.4,1.4 0 0,1 15.71,13.57V18.5H18.5M6.88,
+                8.56A1.68,1.68 0 0,0 8.56,6.88C8.56,5.95 7.81,5.19 6.88,
+                5.19A1.69,1.69 0 0,0 5.19,6.88C5.19,7.81 5.95,8.56 6.88,
+                8.56M8.27,18.5V10.13H5.5V18.5H8.27Z'
+              />
             </svg>
           </a>
           <a href='https://www.facebook.com/sjsusce/'>
-            <svg width='50px' height='50px' viewBox='0 0 50 50'>
-              <path fill='#3F51B5' d='M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z' />
-              <path fill='#FFF' d='M34.368,25H31v13h-5V25h-3v-4h3v-2.41c0.002-3.508,1.459-5.59,5.592-5.59H35v4h-2.287C31.104,17,31,17.6,31,18.723V21h4L34.368,25z' />
-            </svg>
-          </a>
-          <a href='https://www.instagram.com/sjsusce/'>
-            <svg
-              width="50"
-              height="50"
-              viewBox="0 0 48 48"
-            >
-              <radialGradient
-                id="yOrnnhliCrdS2gy~4tD8ma"
-                cx="19.38"
-                cy="42.035"
-                r="44.899"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop offset="0" stopColor="#fd5" />
-                <stop offset="0.328" stopColor="#ff543f" />
-                <stop offset="0.348" stopColor="#fc5245" />
-                <stop offset="0.504" stopColor="#e64771" />
-                <stop offset="0.643" stopColor="#d53e91" />
-                <stop offset="0.761" stopColor="#cc39a4" />
-                <stop offset="0.841" stopColor="#c837ab" />
-              </radialGradient>
+            <svg width='50px' height='50px' viewBox='0 0 24 24'>
               <path
-                fill="url(#yOrnnhliCrdS2gy~4tD8ma)"
-                d="M34.017 41.99l-20 .019c-4.4.004-8.003-3.592-8.008-7.992l-.019-20c-.004-4.4 3.592-8.003 7.992-8.008l20-.019c4.4-.004 8.003 3.592 8.008 7.992l.019 20c.005 4.401-3.592 8.004-7.992 8.008z"
-              />
-              <radialGradient
-                id="yOrnnhliCrdS2gy~4tD8mb"
-                cx="11.786"
-                cy="5.54"
-                r="29.813"
-                gradientTransform="matrix(1 0 0 .6663 0 1.849)"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop offset="0" stopColor="#4168c9" />
-                <stop offset="0.999" stopColor="#4168c9" stopOpacity="0" />
-              </radialGradient>
-              <path
-                fill="url(#yOrnnhliCrdS2gy~4tD8mb)"
-                d="M34.017 41.99l-20 .019c-4.4.004-8.003-3.592-8.008-7.992l-.019-20c-.004-4.4 3.592-8.003 7.992-8.008l20-.019c4.4-.004 8.003 3.592 8.008 7.992l.019 20c.005 4.401-3.592 8.004-7.992 8.008z"
-              />
-              <path
-                fill="#fff"
-                d="M24 31c-3.859 0-7-3.14-7-7s3.141-7 7-7 7 3.14 7 7-3.141 7-7 7zm0-12c-2.757 0-5 2.243-5 5s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z"
-              />
-              <circle cx="31.5" cy="16.5" r="1.5" fill="#fff" />
-              <path
-                fill="#fff"
-                d="M30 37H18c-3.859 0-7-3.14-7-7V18c0-3.86 3.141-7 7-7h12c3.859 0 7 3.14 7 7v12c0 3.86-3.141 7-7 7zM18 13c-2.757 0-5 2.243-5 5v12c0 2.757 2.243 5 5 5h12c2.757 0 5-2.243 5-5V18c0-2.757-2.243-5-5-5H18z"
+                fill='#E1F5FE'
+                d='M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,
+                19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,
+                8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z'
               />
             </svg>
           </a>
-          <a href="https://discord.gg/KZCKCEz5YA">
-            <svg
-              width="48"
-              height="37.18"
-              viewBox="0 0 71 55"
-            >
-              <g clipPath="url(#clip0)">
-                <path
-                  fill="#5865F2"
-                  d="M60.105 4.898A58.55 58.55 0 0045.653.415a.22.22 0 00-.233.11 40.784 40.784 0 00-1.8 3.697c-5.456-.817-10.886-.817-16.23 0-.485-1.164-1.201-2.587-1.828-3.697a.228.228 0 00-.233-.11 58.386 58.386 0 00-14.451 4.483.207.207 0 00-.095.082C1.578 18.73-.944 32.144.293 45.39a.244.244 0 00.093.167c6.073 4.46 11.955 7.167 17.729 8.962a.23.23 0 00.249-.082 42.08 42.08 0 003.627-5.9.225.225 0 00-.123-.312 38.772 38.772 0 01-5.539-2.64.228.228 0 01-.022-.378c.372-.279.744-.569 1.1-.862a.22.22 0 01.23-.03c11.619 5.304 24.198 5.304 35.68 0a.219.219 0 01.233.027c.356.293.728.586 1.103.865a.228.228 0 01-.02.378 36.384 36.384 0 01-5.54 2.637.227.227 0 00-.121.315 47.249 47.249 0 003.624 5.897.225.225 0 00.249.084c5.801-1.794 11.684-4.502 17.757-8.961a.228.228 0 00.092-.164c1.48-15.315-2.48-28.618-10.497-40.412a.18.18 0 00-.093-.084zm-36.38 32.427c-3.497 0-6.38-3.211-6.38-7.156 0-3.944 2.827-7.156 6.38-7.156 3.583 0 6.438 3.24 6.382 7.156 0 3.945-2.827 7.156-6.381 7.156zm23.593 0c-3.498 0-6.38-3.211-6.38-7.156 0-3.944 2.826-7.156 6.38-7.156 3.582 0 6.437 3.24 6.38 7.156 0 3.945-2.798 7.156-6.38 7.156z"
-                ></path>
-              </g>
-              <defs>
-                <clipPath id="clip0">
-                  <path fill="#fff" d="M0 0H71V55H0z"></path>
-                </clipPath>
-              </defs>
-            </svg>
-            {/* eslint-enable */}
-          </a>
+
           <p>Brought to you by SCE Dev-Team</p>
         </Col>
       </Row>
