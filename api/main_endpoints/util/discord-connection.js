a03cca6e23eb6865c8109116eec2b956371a8d79
globalupright <evanuxd@gmail.com>
2022-04-29 18:35:00 -0700

    shoutout neel frog and allen

--- api/main_endpoints/util/discord-connection.js
+++ api/main_endpoints/util/discord-connection.js
@@ -11,13 +11,11 @@ async function loginWithDiscord(code, email, discordRedirectUri) {
       return resolve(true);
     }
     axios
-      .post('https://discord.com/api/oauth2/token', {
-        'client_id': discordApiKeys.CLIENT_ID,
-        'client_secret': discordApiKeys.CLIENT_SECRET,
-        'grant_type': 'authorization_code',
-        code,
-        'redirect_uri': encodeURIComponent(discordRedirectUri),
-      })
+      .post('https://discord.com/api/oauth2/token',
+        `&client_id=${discordApiKeys.CLIENT_ID}` +
+        `&client_secret=${discordApiKeys.CLIENT_SECRET}` +
+        `&grant_type=authorization_code&code=${code}` +
+        `&redirect_uri=${encodeURIComponent(discordRedirectUri)}`)
       .then(response => {
         axios.get('https://discord.com/api/users/@me', {
           headers: {
