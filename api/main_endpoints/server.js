829346ffcd61fd5f43519cd730043f1534c99b66
Truong Phu Vu <truongphu.vu@sjsu.edu>
2022-07-05 22:55:11 -0700

    fixed line's length

--- api/main_endpoints/server.js
+++ api/main_endpoints/server.js
@@ -4,7 +4,9 @@ function main() {
   const API_ENDPOINTS = [
     __dirname + '/routes/'
   ];
-  const mainEndpointServer = new SceHttpServer(API_ENDPOINTS, 8080, '/mainendpoints/');
+  // eslint-disable-next-line max-len
+  const mainEndpointServer =
+  new SceHttpServer(API_ENDPOINTS, 8080, '/mainendpoints/');
   mainEndpointServer.initializeEndpoints().then(() => {
     mainEndpointServer.openConnection();
   });
