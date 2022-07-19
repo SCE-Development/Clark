83df2a96557ed0b9784556710612cb9501c2e5bc
Sunidhi Thakker <sunidhithakker@gmail.com>
2022-07-18 22:01:41 -0700

    Added image and CSS to Sponsor Us Section

--- src/Pages/Home/Components/SponsorUs.js
+++ src/Pages/Home/Components/SponsorUs.js
@@ -4,21 +4,21 @@ import { Button } from 'reactstrap';
 
 const SponsorUs = () => {
   return (
-    <div>
-      <div className='sponsors'>
-        <h1 >Our Sponsors make our club possible</h1>
-      </div>
-      <div>
-        <img src='' alt='SCE Sponsors Background'/>
-      </div>
-      <div className='sponsor-us-button'>
-        <a href='/register'>
-          <Button outline color='primary' id='Sponsor-us-btn'
-            // copied from home
-            style={{width:'170px', border:'3px solid',
-              borderRadius:8, fontSize:'20px'}}>Sponsor Us</Button>
-        </a>
-      </div>
+    <div className='sponsor-us'>
+        <img className='sponsors-img' 
+          src='images/what-we-do-image.png' 
+          alt='SCE Sponsors Background'/>
+        <div className='sponsors-header'>
+          <h1>Our Sponsors make our club possible</h1>
+        </div>
+        <div className='sponsor-us-button'>
+          <a href='/register'>
+            <Button outline color='primary' id='Sponsor-us-btn'
+              // copied from home
+              style={{width:'170px', border:'3px solid',
+                borderRadius:8, fontSize:'20px'}}>Sponsor Us</Button>
+          </a>
+        </div>
     </div>
   );
 };
