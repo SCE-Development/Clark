e8afe338d09a129077807d89e4f71e83cb48e6c4
Sunidhi Thakker <sunidhithakker@gmail.com>
2022-07-22 18:21:35 -0700

    Changed images for What We Do and Sponsor Us sections, css styling

--- src/Pages/Home/Components/SponsorUs.js
+++ src/Pages/Home/Components/SponsorUs.js
@@ -6,13 +6,13 @@ const SponsorUs = () => {
   return (
     <div className='sponsor-us'>
         <img className='sponsors-img' 
-          src='images/what-we-do-image.png' 
+          src='images/sponsor-us-img.jpg' 
           alt='SCE Sponsors Background'/>
         <div className='sponsors-header'>
           <h1>Our Sponsors make our club possible</h1>
         </div>
         <div className='sponsor-us-button'>
-          <a href='/register'>
+          <a href='https://www.canva.com/design/DAE2elflots/share/preview?token=_b1m7H5G7FeykfkUaR9JOw&role=EDITOR&utm_content=DAE2elflots&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton#2'>
             <Button outline color='primary' id='Sponsor-us-btn'
               // copied from home
               style={{width:'170px', border:'3px solid',
