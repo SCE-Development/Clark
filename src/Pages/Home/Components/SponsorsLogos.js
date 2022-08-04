495320b131b044fb052caea5431d34db8c88f851
Sunidhi Thakker <sunidhithakker@gmail.com>
2022-08-04 12:44:05 -0700

    Media queries for Sponsor Logos

--- src/Pages/Home/Components/SponsorsLogos.js
+++ src/Pages/Home/Components/SponsorsLogos.js
@@ -6,31 +6,41 @@ const SponsorsLogos = () => {
     <div className='sponosors-logos'>
       <div className='container-sponsors-logos'>
         <div className='sponsors-row-1'>
-          <img className='cisco-img'
-            src='images/cisco.png'
-            alt='Sponsor Cisco'/>
-          <img className='echo3d-img'
-            src='images/echo3D.png'
-            alt='Sponsor Echo3D'/>
-          <img className='google-img'
-            src='images/google.png'
-            alt='Sponsor Google'/>
+          <div className='cisco-img'>
+            <img src='images/cisco.png'
+              alt='Sponsor Cisco'/>
+          </div>
+          <div className='echo3d-img'>
+            <img src='images/echo3D.png'
+              alt='Sponsor Echo3D'
+              className='hide-images'/>
+          </div>
+          <div className='google-img'>
+            <img src='images/google.png'
+              alt='Sponsor Google'/>
+          </div>
         </div>
         <div className='sponsors-row-2'>
-          <img className='balsamiq-img'
-            src='images/balsamiq.png'
-            alt='Sponsor Balsamiq'/>
-          <img className='xircle-img'
-            src='images/xircle.png'
-            alt='Sponsor Xircle'/>
+          <div className='balsamiq-img'>
+            <img src='images/balsamiq.png'
+              alt='Sponsor Balsamiq'
+              className='hide-images'/>
+          </div>
+          <div className='xircle-img'>
+            <img src='images/xircle.png'
+              alt='Sponsor Xircle'
+              className='hide-images'/>
+          </div>
         </div>
         <div className='sponsors-row-3'>
-          <img className='microsoft-img'
-            src='images/microsoft.png'
-            alt='Sponsor Microsoft'/>
-          <img className='tesla-img'
-            src='images/tesla.png'
-            alt='Sponsor Tesla'/>
+          <div className='microsoft-img'>
+            <img src='images/microsoft.png'
+              alt='Sponsor Microsoft'/>
+          </div>
+          <div className='tesla-img'>
+            <img src='images/tesla.png'
+              alt='Sponsor Tesla'/>
+          </div>
         </div>
       </div>
     </div>
