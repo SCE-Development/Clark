2da937673676d117382e025e07b627d83a7f4b6b
David Tan <davidtanline@gmail.com>
2022-08-05 12:55:41 -0700

    remove Overview folder and other unnecessary comments

--- src/Components/Routing/PrivateRoute.js
+++ src/Components/Routing/PrivateRoute.js
@@ -1,50 +1,18 @@
-import React, { Component } from 'react';
-// import { Route, Redirect } from 'react-router-dom';
-import { Route, Navigate, Outlet } from 'react-router-dom';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 export default function PrivateRoute(
-  {component: Component, appProps, ...props}) {
+  { component: Component, appProps, ...props }) {
   return (
     appProps.allowed ?
       <Component {...appProps} {...props} />
-      // <Outlet />
       : <Navigate
         to={{
-          pathname: appProps.redirect ? appProps.redirect : '/login'}}
+          pathname: appProps.redirect ? appProps.redirect : '/login'
+        }}
         state={{ from: props.location }}
         replace
       />
-
-  // <Route
-  //   {...params}
-  //   element={props =>
-  //     appProps.allowed ?
-  //       (
-  //         <Component {...appProps} {...props} />
-  //       ) :
-  //       (
-  //         <Route
-  //           element={props => (
-  //             <Navigate
-  //               to={{
-  //                 pathname: appProps.redirect ? appProps.redirect : '/login',
-  //                 state: { from: props.location }
-  //               }}
-  //             />
-  //           )}
-  //         />
-  //       )}
-  // />
-
-
-  // appProps.allowed ? (<Component {...appProps} {...props} />) : (
-  //   <Navigate
-  //     to={{
-  //       pathname: appProps.redirect ? appProps.redirect : '/login',
-  //       state: { from: props.location }
-  //     }}
-  //   />
-  // )
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-// import { withRouter } from 'react-router-dom';
 import './index.css';
 
 import Routing from './Routing';
