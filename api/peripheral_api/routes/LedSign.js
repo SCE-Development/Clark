99c9be2056e7b25207935ca572574019df267fdd
CCLDArjun <ccldarjun@icloud.com>
2022-08-16 12:54:40 -0700

    make ledsign handler ping actual ledsign healthcheck

--- api/peripheral_api/routes/LedSign.js
+++ api/peripheral_api/routes/LedSign.js
@@ -16,7 +16,18 @@ const {
 const SqsHandler = new SceSqsApiHandler(AWS.Queue.LED_QUEUE_NAME);
 
 router.get('/healthCheck', (req, res) => {
-  res.sendStatus(OK);
+  if (process.env.NODE_ENV !== 'production') {
+     return res.sendStatus(OK);
+   }
+
+   await axios
+     .get('http://host.docker.internal:11000/api/health-check')
+     .then(() => {
+       return res.sendStatus(OK);
+     })
+     .catch((err) => {
+       return res.sendStatus(NOT_FOUND);
+     });
 });
 
 router.post('/updateSignText', async (req, res) => {
