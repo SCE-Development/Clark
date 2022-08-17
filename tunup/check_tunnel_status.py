34606cb5471c428b710a1abd62eea19d17269ba4
Arjun <ccldarjun@icloud.com>
2022-08-17 14:59:15 -0700

    use argparse

--- tunup/check_tunnel_status.py
+++ tunup/check_tunnel_status.py
@@ -2,8 +2,31 @@ import logging
 from time import sleep, gmtime
 import requests
 import sys
+import argparse
 
-t = int(sys.argv[1])
+parser = argparse.ArgumentParser()
+parser.add_argument(
+  "--request_interval_seconds",
+  type=int,
+  default=5,
+  help="Interval to query hosts through SSH tunnel"
+)
+parser.add_argument(
+  "--hosts",
+  type=str,
+  required=True,
+  help="comma separated values for hosts to send requests to \
+    i.e. http://host.docker.internal:11000/api/health-check,http://host.docker.internal:14000/healthcheck/printer"
+)
+
+args = parser.parse_args()
+
+hosts_list = [host.strip() for host in args.hosts.split(',')]
+print(hosts_list)
+
+# iterate through hosts list and u can ping it easy
+
+t = int(args.request_interval_seconds)
 
 logging.Formatter.converter = gmtime
 logging.basicConfig(
@@ -13,21 +36,15 @@ logging.basicConfig(
 )
 
 logging.info("Starting tunnel routine...")
-def ping():
-    req = None
-
+def ping(host):
+    print(host)
     try:
-        req = requests.get("http://host.docker.internal:11000/api/health-check")
-        logging.info("Success connecting: %s", req)
+        req = requests.get(host)
+        logging.info(f"Success connecting to {host}: %s", req)
     except Exception as e:
-        logging.error("Error connecting: %s", e)
+        logging.error(f"Could not reach {host}: %s", e)
 
-    try:
-        req = requests.get("http://host.docker.internal:14000/healthcheck/printer")
-        logging.info("Success connecting: %s", req)
-    except Exception as e:
-        logging.error("Error connecting: %s", e)
 while True:
-    ping()
+    [ping(host) for host in hosts_list]
     sleep(t)
 
