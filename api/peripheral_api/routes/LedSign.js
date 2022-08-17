819fe24011b3f93c5cc543f13afa3ff6cb3f3614
Arjun <ccldarjun@icloud.com>
2022-08-17 14:16:02 -0700

    For new tunnel from led sign to corev4 (#1137)
    * make ledsign handler ping actual ledsign healthcheck
    
    * made it async
    
    * run lint
    
    * added how these work
    
    * added in wrong place previously fixed it
    
    Co-authored-by: akshtshrma24 <akshitsharmaa65@gmail.com>

--- api/peripheral_api/routes/LedSign.js
+++ api/peripheral_api/routes/LedSign.js
@@ -15,8 +15,22 @@ const {
 
 const SqsHandler = new SceSqsApiHandler(AWS.Queue.LED_QUEUE_NAME);
 
-router.get('/healthCheck', (req, res) => {
-  res.sendStatus(OK);
+router.get('/healthCheck', async (req, res) => {
+  /*
+  * How these work with Quasar:
+  * https://github.com/SCE-Development/Quasar/wiki/How-do-Health-Checks-Work%3F
+  */
+  if (process.env.NODE_ENV !== 'production') {
+    return res.sendStatus(OK);
+  }
+  await axios
+    .get('http://host.docker.internal:11000/api/health-check')
+    .then(() => {
+      return res.sendStatus(OK);
+    })
+    .catch((err) => {
+      return res.sendStatus(NOT_FOUND);
+    });
 });
 
 router.post('/updateSignText', async (req, res) => {
