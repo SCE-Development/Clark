d35fd8874485c54445d55035ce2837d391a72dff
David Tan <davidtanline@gmail.com>
2022-08-20 16:55:43 -0700

    opps

--- api/main_endpoints/routes/User.js
+++ api/main_endpoints/routes/User.js
@@ -302,12 +302,12 @@ router.post('/updatePagesPrintedFromDiscord', (req, res) => {
   if(!checkDiscordKey(apiKey)){
     return res.sendStatus(UNAUTHORIZED);
   }
-  User.updateOne( { discordID }, { pagesPrinted },
+  User.updateOne( { discordID }, {pagesPrinted},
     (error, result) => {
       let status = OK;
       if(error){
         status = BAD_REQUEST;
-      } else if (result.n === 0) {
+      } else if (result.n === 0){
         status = NOT_FOUND;
       }
       return res.sendStatus(status);
