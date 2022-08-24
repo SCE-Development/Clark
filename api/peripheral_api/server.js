7fccc04facb4919885ee74bb0a7040d9f2883b42
akshtshrma24 <akshitsharmaa65@gmail.com>
2022-08-24 16:43:59 -0700

    Removed Printing Logs

--- api/peripheral_api/server.js
+++ api/peripheral_api/server.js
@@ -5,7 +5,6 @@ function main() {
   try {
     logger.info('Starting server...');
     const API_ENDPOINTS = [
-      __dirname + '/routes/PrintLog.js',
       __dirname + '/routes/LedSign.js',
       __dirname + '/routes/Printer.js',
       __dirname + '/routes/RFID.js'
