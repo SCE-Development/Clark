860bdb520a90ce3f5fdfe882e84fc7438815ee05
evan ugarte <evanuxd@gmail.com>
2022-08-24 18:24:31 -0700

    properly send back json from health check, i tested this in prod

--- api/peripheral_api/routes/LedSign.js
+++ api/peripheral_api/routes/LedSign.js
@@ -32,12 +32,11 @@ router.get('/healthCheck', async (req, res) => {
   if (runningInDevelopment) {
     return res.sendStatus(OK);
   }
-  const isUp = await healthCheck();
-  let status = OK;
-  if(!isUp) {
-    status = SERVER_ERROR;
+  const dataFromSign = await healthCheck();
+  if(!dataFromSign) {
+    return res.sendStatus(SERVER_ERROR);
   }
-  return res.sendStatus(status);
+  return res.status(OK).json(dataFromSign);
 });
 
 router.post('/updateSignText', async (req, res) => {
@@ -56,7 +55,9 @@ router.post('/updateSignText', async (req, res) => {
   let result = false;
   if (req.body.ledIsOff) {
     result = await turnOffSign();
+    logger.info("turning sign off!");
   } else {
+    logger.info("updating sign with:", req.body);
     result = await updateSign(req.body);
   }
   let status = OK;
