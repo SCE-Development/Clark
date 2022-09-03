3dbd424e9151737c3afddf6530a296a5efca3516
David Tan <davidtanline@gmail.com>
2022-09-03 15:22:59 -0700

    render button test with variable changed

--- src/Pages/Overview/PageNumbers.js
+++ src/Pages/Overview/PageNumbers.js
@@ -6,8 +6,9 @@ function PageNumbers(props) {
   // for example, given numberOfButtons = 3:
   // this generates [0,1,2,3]
   const pageNumbers = [...Array(numberOfButtons + 1).keys()].slice(1);
-  const params = props.parseQuery();
-  const currentPage = params.page ? parseInt(params.page) : 1;
+  // const params = props.parseQuery();
+  // const currentPage = params.page ? parseInt(params.page) : 1;
+  const currentPage = props.pageNumber;
   return (
     <nav className='user-page-numbers'>
       <ul>
@@ -28,4 +29,3 @@ function PageNumbers(props) {
 }
 
 export default PageNumbers;
-
---
+++ test/frontend/PageNumbers.test.js
@@ -0,0 +1,41 @@
+// https://masteringjs.io/tutorials/sinon/stub-called-with
+/* global describe it */
+import 'jsdom-global/register';
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import PageNumbers from '../../src/Pages/Overview/PageNumbers';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<PageNumbers />', () => {
+
+  let pageNumber = 1;
+  let count = 10;
+  let usersPerPage = 4;
+
+  const object = {
+    paginate: (newPage) => {
+      pageNumber = newPage;
+    }
+  };
+
+  const wrapper = mount(
+    <PageNumbers
+      paginate={object.paginate}
+      pageNumber={pageNumber}
+      count={count}
+      usersPerPage={usersPerPage}
+    />
+  );
+
+  // should render expeced number of buttons given props.count / props.usersPerPage
+  it('Should render 3 <button /> components', () => {
+    expect(wrapper.find('button')).to.have.lengthOf(3);
+  });
+  // should call paginate with correct number when button clicked, do this with evan later
+
+});
