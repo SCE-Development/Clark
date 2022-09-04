c3a8531d2c011dcff3b2e62521dd240b208a7980
evan ugarte <evanuxd@gmail.com>
2022-09-04 14:17:15 -0700

    Add tests for ExpirationDropdown

--- test/util/mocks/Date.js
+++ test/util/mocks/Date.js
@@ -4,11 +4,11 @@ const sinon = require('sinon');
 let clock = null;
 
 /**
- * Mock current time to desired month
+ * Mock current time to desired month and year
  * @param {Number} month - month to mock
+ * @param {Number} year - month to mock
  */
-function mockMonth(month) {
-  const year = new Date().getFullYear();
+function mockMonthAndYear(month, year) {
   clock = sinon.useFakeTimers({
     now: new Date(year, month),
     toFake: [
@@ -19,10 +19,19 @@ function mockMonth(month) {
 }
 
 /**
+ * Mock current time to desired month. The year of the mocked date
+ * is whatever the current year was when this function was called.
+ * @param {Number} month - month to mock
+ */
+function mockMonth(month) {
+  mockMonthAndYear(month, new Date().getFullYear());
+}
+
+/**
  * Reset clock, use after mock
  */
 function revertClock() {
   if (clock) clock.restore();
 }
 
-module.exports = {mockMonth, revertClock};
+module.exports = { mockMonthAndYear, mockMonth, revertClock };
