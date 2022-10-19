d1f3679ea63b6a7b4607fc8090036954ea696d77
Cool Guy Awal <63530023+akshtshrma24@users.noreply.github.com>
2022-10-19 10:23:44 -0700

    Remove Profile Image (#1174)

--- src/Components/Navbar/UserNavbar.js
+++ src/Components/Navbar/UserNavbar.js
@@ -85,17 +85,7 @@ export default function UserNavBar(props) {
               {props.authenticated && props.user ? (
                 <div className='dropdown-submenu drp-item'>
                   <DropdownItem className='drp-item' id='btndrp-text'>
-                    <svg className='profile-image' viewBox='0 0 24 24'>
-                      <path
-                        fill='gray'
-                        d='M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,
-                          12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,
-                          17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,
-                          3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,
-                          2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,
-                          0 22,12C22,6.47 17.5,2 12,2Z'
-                      />
-                    </svg>
+
                     {props.user.firstName}
                   </DropdownItem>
                   <DropdownMenu right className='drp-menu'>
@@ -153,17 +143,6 @@ export default function UserNavBar(props) {
                 <div className='profile'>
                   <UncontrolledDropdown nav inNavbar>
                     <DropdownToggle className='text-white' nav caret>
-                      <svg className='profile-image' viewBox='0 0 24 24'>
-                        <path
-                          fill='white'
-                          d='M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,
-                          12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,
-                          17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,
-                          8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,
-                          5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,
-                          10 0 0,0 22,12C22,6.47 17.5,2 12,2Z'
-                        />
-                      </svg>
                       {props.user.firstName}
                     </DropdownToggle>
                     <DropdownMenu right>
