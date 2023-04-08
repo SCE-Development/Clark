20e8a4367e054f91124510de6b4df03e595ebedc
Mau <79354255+Luceium@users.noreply.github.com>
2023-04-08 02:43:57 -0700

    adds more props and gets rid of clipping that cut off part of the svg

--- src/Components/Background/background.jsx
+++ src/Components/Background/background.jsx
@@ -7,7 +7,7 @@ export default function background(props) {
         <div className='background-parent'>
             <svg width={props.size} height={props.size} viewBox={`0 0 ${props.size} ${props.size}`} fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g>
-                    <g clipPath="url(#clip0_1_2)" transform={`translate(${props.size / 2}, ${props.size / 2})`}>
+                    <g transform={`translate(${props.size / 2}, ${props.size / 2})`}>
                         <g id="boxes" transform="rotate(45 0 0)">
                             <rect x={-1 * props.outerRadius} y={-1 * props.outerRadius} width={2 * props.outerRadius} height={2 * props.outerRadius} rx={props.radius} strokeWidth={props.size * 0.002}/>
                             <rect x={-1 * props.innerRadius} y={-1 * props.innerRadius} width={2 * props.innerRadius} height={2 * props.innerRadius} rx={props.radius} strokeWidth={props.size * 0.002}/>
@@ -20,36 +20,32 @@ export default function background(props) {
                         <circle cx="-382" cy="175" r="7" stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
                     </g>
                     <g id="electricity" transform={`translate(${props.size / 2}, ${props.size / 2})`} class="path" strokeWidth={props.size * 0.006} fill="none" fillRule="evenodd"  strokeLinejoin="round" strokeDasharray="30,150" strokeDashoffset="2000">
-                    <polyline id="path" points="88 -22 175 -109 375 -109"></polyline>
-                        {circuit()}
+                        {circuit(props)}
                     </g>
                     <g id="circuit" transform={`translate(${props.size / 2}, ${props.size / 2})`} strokeWidth={props.size * 0.002}>
-                        <polyline id="path" points="88 -22 175 -109 375 -109"></polyline>
-                        {circuit()}
+                        {circuit(props)}
                     </g>
                 </g>
-                <defs>
-                    <clipPath id="clip0_1_2">
-                        <rect width={props.size} height={props.size} fill="black"/>
-                    </clipPath>
-                </defs>
             </svg>
         </div>
     );
 }
 
 //abstracts circuit
-function circuit() {
+function circuit(props) {
+    var radiusDifference = props.outerRadius - props.innerRadius;
+    var spread = props.lineSpread * (2 * props.innerRadius);
+    
     return (
         <>
-            <polyline id="path" points="88 -22 175 -109 375 -109"></polyline>
-            <polyline id="path" points="88 22 175 109 375 109"></polyline>
-            <polyline id="path" points="22 88 109 175 109 375"></polyline>
-            <polyline id="path" points="-22 88 -109 175 -375 175"></polyline>
-            <polyline id="path" points="-88 22 -175 109 -375 109"></polyline>
-            <polyline id="path" points="-88 -22 -175 -109 -375 -109"></polyline>
-            <polyline id="path" points="-22 -88 -109 -175 -109 -375"></polyline>
-            <polyline id="path" points="22 -88 109 -175 375 -175"></polyline>
+            <polyline id="path" points={`22 -88 109 -175 375 -175`}></polyline>
+            <polyline id="path" points={`88 -22 175 -109 375 -109`}></polyline>
+            <polyline id="path" points={`88 22 175 109 375 109`}></polyline>
+            <polyline id="path" points={`22 88 109 175 109 375`}></polyline>
+            <polyline id="path" points={`-22 88 -109 175 -375 175`}></polyline>
+            <polyline id="path" points={`-88 22 -175 109 -375 109`}></polyline>
+            <polyline id="path" points={`-88 -22 -175 -109 -375 -109`}></polyline>
+            <polyline id="path" points={`-22 -88 -109 -175 -109 -375`}></polyline>
         </>
     );
 
