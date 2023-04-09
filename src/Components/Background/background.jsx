9a80855ea326b2cb82893c11ace00dad5713c166
Mau <79354255+Luceium@users.noreply.github.com>
2023-04-09 01:20:22 -0700

    makes all props optional by adding default props that are all interdependent

--- src/Components/Background/background.jsx
+++ src/Components/Background/background.jsx
@@ -2,40 +2,48 @@ import React, { Component } from 'react';
 import './background.css';
 
 export default function background(props) {
-    var lineStart = (props.innerRadius - props.lineStartOffset ) / Math.sqrt(2);
-    var lineMid = props.outerRadius / Math.sqrt(2);
-    var lineEnd = props.outerRadius * props.lineLength;
-    var spread = (props.lineSpread * props.innerRadius) / Math.sqrt(2);
-    var circleRadius = props.size * 0.0035;
+    //used for default props
+    var size = props.size || Math.min(window.innerWidth, window.innerHeight);
+    var innerRadius = props.innerRadius || size * 0.125;
+    var outerRadius = props.outerRadius || size * 0.2;
+    var lineSpread = props.lineSpread || 0.4;
+    var endDistance = props.endDistance || 1.85;
+    var radius = props.radius || 0;
+
+    //used for circles
+    var lineMid = outerRadius / Math.sqrt(2);
+    var lineEnd = outerRadius * endDistance;
+    var spread = (lineSpread * innerRadius) / Math.sqrt(2);
+    var circleRadius = size * 0.007;
     
     return (
         <div className='background-parent'>
-            <svg width={props.size} height={props.size} viewBox={`0 0 ${props.size} ${props.size}`} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`} fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g>
-                    <g transform={`translate(${props.size / 2}, ${props.size / 2})`}>
+                    <g transform={`translate(${size / 2}, ${size / 2})`}>
                         <g id="boxes" transform="rotate(45 0 0)">
-                            <rect x={-1 * props.outerRadius} y={-1 * props.outerRadius} width={2 * props.outerRadius} height={2 * props.outerRadius} rx={props.radius} strokeWidth={props.size * 0.002}/>
-                            <rect x={-1 * props.innerRadius} y={-1 * props.innerRadius} width={2 * props.innerRadius} height={2 * props.innerRadius} rx={props.radius} strokeWidth={props.size * 0.002}/>
+                            <rect x={-1 * outerRadius} y={-1 * outerRadius} width={2 * outerRadius} height={2 * outerRadius} rx={radius} strokeWidth={size * 0.004}/>
+                            <rect x={-1 * innerRadius} y={-1 * innerRadius} width={2 * innerRadius} height={2 * innerRadius} rx={radius} strokeWidth={size * 0.004}/>
                         </g>
                         <g>
-                            <circle cx={lineEnd + circleRadius} cy={-lineMid - spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
-                            <circle cx={lineEnd + circleRadius} cy={-lineMid + spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
+                            <circle cx={lineEnd + circleRadius} cy={-lineMid - spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={size * 0.004}/>
+                            <circle cx={lineEnd + circleRadius} cy={-lineMid + spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={size * 0.004}/>
                             
-                            <circle cx={lineEnd + circleRadius} cy={lineMid - spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
-                            <circle cx={lineMid - spread} cy={lineEnd + circleRadius} r={circleRadius} stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
+                            <circle cx={lineEnd + circleRadius} cy={lineMid - spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={size * 0.004}/>
+                            <circle cx={lineMid - spread} cy={lineEnd + circleRadius} r={circleRadius} stroke="#B8B8B8" strokeWidth={size * 0.004}/>
 
-                            <circle cx={-lineEnd - circleRadius} cy={lineMid + spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
-                            <circle cx={-lineEnd - circleRadius} cy={lineMid - spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
+                            <circle cx={-lineEnd - circleRadius} cy={lineMid + spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={size * 0.004}/>
+                            <circle cx={-lineEnd - circleRadius} cy={lineMid - spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={size * 0.004}/>
 
-                            <circle cx={-lineEnd - circleRadius} cy={-lineMid + spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
-                            <circle cx={-lineMid + spread} cy={-lineEnd - circleRadius} r={circleRadius} stroke="#B8B8B8" strokeWidth={props.size * 0.002}/>
+                            <circle cx={-lineEnd - circleRadius} cy={-lineMid + spread} r={circleRadius} stroke="#B8B8B8" strokeWidth={size * 0.004}/>
+                            <circle cx={-lineMid + spread} cy={-lineEnd - circleRadius} r={circleRadius} stroke="#B8B8B8" strokeWidth={size * 0.004}/>
                         </g>
                         
                     </g>
-                    <g id="electricity" transform={`translate(${props.size / 2}, ${props.size / 2})`} class="path" strokeWidth={props.size * 0.006} fill="none" fillRule="evenodd"  strokeLinejoin="round" strokeDasharray="30,150" strokeDashoffset="2000">
+                    <g id="electricity" transform={`translate(${size / 2}, ${size / 2})`} className="path" strokeWidth={size * 0.004} fill="none" fillRule="evenodd"  strokeLinejoin="round" strokeDasharray="30,150" strokeDashoffset="2000">
                         {circuit(props)}
                     </g>
-                    <g id="circuit" transform={`translate(${props.size / 2}, ${props.size / 2})`} strokeWidth={props.size * 0.002}>
+                    <g id="circuit" transform={`translate(${size / 2}, ${size / 2})`} strokeWidth={size * 0.004}>
                         {circuit(props)}
                     </g>
                 </g>
@@ -46,15 +54,24 @@ export default function background(props) {
 
 //abstracts circuit
 function circuit(props) {
-    var lineStart = (props.innerRadius - props.lineStartOffset ) / Math.sqrt(2);
-    var lineMid = props.outerRadius / Math.sqrt(2);
-    var lineEnd = props.outerRadius * props.lineLength;
-    var spread = (props.lineSpread * props.innerRadius) / Math.sqrt(2);
+    //used for default props
+    var size = props.size || Math.min(window.innerWidth, window.innerHeight);
+    var innerRadius = props.innerRadius || size * 0.125;
+    var outerRadius = props.outerRadius || size * 0.2;
+    var lineSpread = props.lineSpread || 0.4;
+    var endDistance = props.endDistance || 1.85;
+    var lineStartOffset = props.lineStartOffset || 0;
+
+    //placement variables
+    var lineStart = (innerRadius - lineStartOffset ) / Math.sqrt(2);
+    var lineMid = outerRadius / Math.sqrt(2);
+    var lineEnd = outerRadius * endDistance;
+    var spread = (lineSpread * innerRadius) / Math.sqrt(2);
     
     return (
         <>
             {/* <xDir,yDir>, <spreadXDir, spreadYDir>, <endXDir, endYDir> */}
-            {/* (+/- lineStart  +/- spread), (+/- props.outerRadius +/- spread), (+/- (2 * props.outerRadius) +/- spread||0, +/-props.outerRadius +/- spread||0)  */}
+            {/* (+/- lineStart  +/- spread), (+/- outerRadius +/- spread), (+/- (2 * outerRadius) +/- spread||0, +/-outerRadius +/- spread||0)  */}
             {/*(+,-),(-,-),(+,0)*/}
             <polyline id="path" points={`${lineStart - spread} ${-lineStart - spread} ${lineMid - spread} ${-lineMid - spread} ${lineEnd} ${-lineMid - spread}`}></polyline> 
             {/*(+,-),(+,+),(+,0)*/}
