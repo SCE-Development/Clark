aea8e719b64d8f2b14b5413d94ce216f4b1de228
Cool Guy Awal <63530023+akshtshrma24@users.noreply.github.com>
2023-04-08 21:28:56 -0700

    Rebuild Home page (#1237)
    * Built HomePage
    
    * Deleted Files
    
    * Mobile device compatibility
    
    * Ran Lint
    
    * Bobo said to change the front end test not me
    
    * Mobile Compatability
    
    * Mobile Compatability
    
    * Images to CDN

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import { Row, Col, Button } from 'reactstrap';
+import { Row } from 'reactstrap';
 import './footer.css';
 import DiscordIcon from './Icons/DiscordIcon';
-import LinkedinIcon from './Icons/LinkedinIcon';
-import FacebookIcon from './Icons/FacebookIcon';
 import InstagramIcon from './Icons/InstagramIcon';
 import GitHubIcon from './Icons/GitHubIcon';
-import YouTubeIcon from './Icons/YouTubeIcon';
-import FooterModal from './FooterModal';
+
+
+
 
 export default () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,43 +15,92 @@ export default () => {
     setIsOpen(!isOpen);
   };
 
+  let getInTouch = 'https://user-images.githubusercontent.com' +
+    '/63530023/230752230-f8c42972-a0cc-4dfb-96fb-f44bb7d935c4.png';
+
   return (
-    <footer className='footer-wrap bg-dark text-white p-4'>
-      <Row className='footer-text align-items-center justify-content-center'>
-        <Col id='left-col'>
-          <b>Software & Computer Engineering Society</b>
-          <ul>
-            <li>
-              San Jose State University
-            </li>
-            <li>
-              Charles W. Davidson College of Engineering
-            </li>
-            <li>
-              Room: ENGR 294
+    <>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+        </link>
+      </head>
+      <div className='text-box'>
+        <h1 className='text-box-header'>
+          Contact Us<br></br>
+          <ul className='list'>
+            <li className='mail'>
+
+              {/* Span Is to display an svg of an envelope  */}
+              <span class="svg"><i class="fas fa-envelope"></i></span>
+
+              General Inquries
+              <li className='sub-list'>
+                <p
+                  style=
+                    {{color: 'rgb(172, 172, 172)'
+                      , fontWeight: 'bold'}}>
+                    Akshit Sharma
+                </p>
+                <p style={{fontSize: '0.75rem'}}>
+                  <a
+                    style={{color:'gray'}}
+                    href="mailto:akshit.sharma@sjsu.edu">
+                      akshit.sharma@sjsu.edu
+                  </a>
+                </p>
+              </li>
+
+              {/* Span Is to display an svg of a suitcase  */}
+              <span class="svg"><i class="fas fa-suitcase"></i></span>
+
+              Sponsor and Relations
+              <li className='sub-list'>
+                <p style={{color: 'rgb(172, 172, 172)', fontWeight: 'bold'}}>Jessabelle Ramos</p>
+                <p style={{fontSize: '0.75rem'}}>
+                  <a
+                    href="mailto:jessabelle.ramos@sjsu.edu"
+                    style={{color:'gray'}}>
+                      jessabelle.ramos@sjsu.edu
+                  </a>
+                </p>
+              </li>
+              <li className='sub-list'>
+                <p style={{color: 'rgb(172, 172, 172)', fontWeight: 'bold'}}>Abbigel Palad</p>
+                <p style={{fontSize: '0.75rem'}}>
+                  <a
+                    href="mailto:abegail.palad@sjsu.edu"
+                    style={{color:'gray'}}>
+                      abegail.palad@sjsu.edu
+                  </a>
+                </p>
+              </li>
+
             </li>
           </ul>
-        </Col>
-        <div className='contact-us'>
-          <Button
-            outline color="primary"
-            className='contact-us-btn'
-            onClick={togglePopup}
-          >Contact Us</Button>
-        </div>
-        {
-          isOpen && <FooterModal handleClose={togglePopup} />
-        }
-        <div className='social-media'>
-          <LinkedinIcon/>
-          <FacebookIcon/>
-          <InstagramIcon/>
-          <DiscordIcon/>
-          <GitHubIcon/>
-          <YouTubeIcon/>
-        </div>
-        <p id='footer-note'>Brought to you by SCE Dev-Team</p>
-      </Row>
-    </footer>
+        </h1>
+      </div>
+      <img
+        src={getInTouch}
+
+        alt="SCE What We Do"
+        className='get-in-touch'
+      />
+      <footer className='footer-wrap bg-dark text-white p-4'>
+        <Row  className='footer-text align-items-center justify-content-center'>
+          <div className='social-media'>
+            <DiscordIcon/>
+            <InstagramIcon/>
+            <GitHubIcon/>
+          </div>
+          <p
+            id='footer-note'
+            style={{marginTop: '1rem', fontSize: '0.8rem'}}>
+              Brought to you by SCE Dev-Team
+          </p>
+        </Row>
+      </footer>
+    </>
   );
 };
