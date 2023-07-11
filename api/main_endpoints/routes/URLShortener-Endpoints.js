c68dc7adce3fe32ac4653592d2095b880bc8d567
pnavab <114110926+pnavab@users.noreply.github.com>
2023-07-10 23:17:13 -0700

    Fix lint issues

--- api/main_endpoints/routes/URLShortener-Endpoints.js
+++ api/main_endpoints/routes/URLShortener-Endpoints.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const axios = require("axios");
+const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 
-let URL_SHORTENER_BASE_URL = "http://localhost:8000";
+let URL_SHORTENER_BASE_URL = 'http://localhost:8000';
 
-router.get("/listAll", async (req, res) => {
+router.get('/listAll', async (req, res) => {
   try {
-    const response = await axios.get(URL_SHORTENER_BASE_URL + "/list");
+    const response = await axios.get(URL_SHORTENER_BASE_URL + '/list');
     const data = response.data;
     res.json(data);
-  }
-  catch (err) {
-    res.status(500).json({ error: "Failed to list URLs" });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to list URLs' });
   }
 });
 
-router.post("/createURL", async (req, res) => {
+router.post('/createURL', async (req, res) => {
   const { url, alias } = req.body;
 
   const response = await axios
-    .post(URL_SHORTENER_BASE_URL + "/create_url", { "url": url, "alias": alias })
+    .post(URL_SHORTENER_BASE_URL + '/create_url', { 'url': url, 'alias': alias })
     .then(response => {
-      res.json({ status: response.status })
+      res.json({ status: response.status });
     })
     .catch(err => {
       console.log(err.response.status);
@@ -29,17 +28,17 @@ router.post("/createURL", async (req, res) => {
     });
 });
 
-router.post("/deleteURL", async (req, res) => {
+router.post('/deleteURL', async (req, res) => {
   const { alias } = req.body;
 
   const response = await axios
-    .post(URL_SHORTENER_BASE_URL + "/delete/" + alias)
+    .post(URL_SHORTENER_BASE_URL + '/delete/' + alias)
     .then(response => {
-      res.json({ status: response.status })
+      res.json({ status: response.status });
     })
     .catch(err => {
       console.log(err.response.status);
-      res.json({ error: err.response.status })
+      res.json({ error: err.response.status });
     });
 });
 
