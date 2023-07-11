bc44cd280c619f52d0a50d7ac74d3093cf6829fc
pnavab <114110926+pnavab@users.noreply.github.com>
2023-07-10 21:46:27 -0700

    Add route endpoints for talking to cleezy

---
+++ api/main_endpoints/routes/URLShortener-Endpoints.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+let URL_SHORTENER_BASE_URL = "http://localhost:8000";
+
+router.get("/listAll", async (req, res) => {
+  try {
+    const response = await axios.get(URL_SHORTENER_BASE_URL + "/list");
+    const data = response.data;
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json({ error: "Failed to list URLs" });
+  }
+});
+
+router.post("/createURL", async (req, res) => {
+  const { url, alias } = req.body;
+
+  const response = await axios
+    .post(URL_SHORTENER_BASE_URL + "/create_url", { "url": url, "alias": alias })
+    .then(response => {
+      res.json({ status: response.status })
+    })
+    .catch(err => {
+      console.log(err.response.status);
+      res.json({ error: err.response.status });
+    });
+});
+
+router.post("/deleteURL", async (req, res) => {
+  const { alias } = req.body;
+
+  const response = await axios
+    .post(URL_SHORTENER_BASE_URL + "/delete/" + alias)
+    .then(response => {
+      res.json({ status: response.status })
+    })
+    .catch(err => {
+      console.log(err.response.status);
+      res.json({ error: err.response.status })
+    });
+});
+
+module.exports = router;
