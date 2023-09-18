995dd61ed38b5e9f0f54792395fa79557b11e741
pnavab <114110926+pnavab@users.noreply.github.com>
2023-09-18 15:51:59 -0700

    Fix Speaker.js Api Functions

--- src/APIFunctions/Speaker.js
+++ src/APIFunctions/Speaker.js
@@ -5,15 +5,19 @@ const config  = require('../config/config.json');
 const speakerPort = config.speakerPort;
 
 let SPEAKER_API_URL = `http://localhost:${speakerPort}`;
+console.debug(SPEAKER_API_URL);
 
 export async function addUrl(url) {
   let status = new ApiResponse();
+  console.debug(url);
   await axios
     .get(SPEAKER_API_URL + `/stream/?url=${url}`)
+    // .get(`http://localhost:16000/?`)
     .then(res => {
       status = res.data;
     })
     .catch(err => {
+      console.debug(err);
       status.responseData = err;
       status.error = true;
     });
