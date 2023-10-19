f500e162dbbef3d116dbcaa981d03af7aaf9b5a1
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-10-19 16:17:57 -0700

    ran lint

--- src/Pages/Speaker/Speaker.js
+++ src/Pages/Speaker/Speaker.js
@@ -6,7 +6,7 @@ import { queued, addUrl, pause, resume, skip } from '../../APIFunctions/Speaker'
 import './speaker.css';
 
 function SpeakersPage(props) {
-  
+
   const [url, setUrl] = useState('');
   const [queuedSongs, setQueuedSongs] = useState([]);
   const [error, setError] = useState();
@@ -63,54 +63,54 @@ function SpeakersPage(props) {
             <Col>
               <Input placeholder='Enter YouTube Link' onChange=
                 {(e) => setUrl(e.target.value)}
-                className="sign-input"
-                style={{width : "100%", height : "2rem"}}
-                >
-                </Input>
+              className="sign-input"
+              style={{width : '100%', height : '2rem'}}
+              >
+              </Input>
             </Col>
           </Row>
           <Col>
+            <Row>
+              <Col>
+                <Button className="sign-input" onClick={pauseSong}>Puase</Button>
+              </Col>
+              <Col>
+                <Button className="sign-input" onClick={resumeSong}>Resume</Button>
+              </Col>
+              <Col>
+                <Button className="sign-input" onClick={playSong}>Play</Button>
+              </Col>
               <Row>
-                <Col>
-                  <Button className="sign-input" onClick={pauseSong}>Puase</Button>
-                </Col>
-                <Col>
-                  <Button className="sign-input" onClick={resumeSong}>Resume</Button>
-                </Col>
-                <Col>
-                  <Button className="sign-input" onClick={playSong}>Play</Button>
-                </Col>
-                <Row>
-                  <Button onClick={skipSong} className="sign-input" 
-                  style={{marginTop : "0.5rem", 
-                  background : "red",
-                  borderColor : "red",
-                  marginLeft : "0.75rem",
-                  height : '5rem',
-                  fontWeight : '900',
-                  fontSize : '40px'
+                <Button onClick={skipSong} className="sign-input"
+                  style={{marginTop : '0.5rem',
+                    background : 'red',
+                    borderColor : 'red',
+                    marginLeft : '0.75rem',
+                    height : '5rem',
+                    fontWeight : '900',
+                    fontSize : '40px'
                   }}>Skip</Button>
-                </Row>
-                <div style={{marginTop : "5rem", alignItems : 'center'}}>
-                  <h2 style={{textAlign : 'center'}}>Queued</h2> 
-                  <table>
-                    <thead>
-                      <th>Position</th>
-                      <th style={{textAlign : "left", paddingLeft : '2rem'}}>Name</th>
-                    </thead>
-                    <tbody>
-                      {
-                        queuedSongs.map((song,index) => (
+              </Row>
+              <div style={{marginTop : '5rem', alignItems : 'center'}}>
+                <h2 style={{textAlign : 'center'}}>Queued</h2>
+                <table>
+                  <thead>
+                    <th>Position</th>
+                    <th style={{textAlign : 'left', paddingLeft : '2rem'}}>Name</th>
+                  </thead>
+                  <tbody>
+                    {
+                      queuedSongs.map((song, index) => (
                         <tr key={index}>
                           <td>{index}</td>
                           <td style={{paddingLeft : '2rem'}}><a href={song}>{song}</a></td>
                         </tr>
-                        ))
-                      }  
-                    </tbody>
-                  </table>
-                </div>
-              </Row>
+                      ))
+                    }
+                  </tbody>
+                </table>
+              </div>
+            </Row>
           </Col>
         </div>
       </Container>
