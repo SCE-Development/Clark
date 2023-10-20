5b41610ec8492b6f1ea612644dd9659511573357
evan ugarte <evanuxd@gmail.com>
2023-10-19 19:30:37 -0700

    lint

--- api/peripheral_api/util/Speaker.js
+++ api/peripheral_api/util/Speaker.js
@@ -4,18 +4,18 @@ const logger = require('../../util/logger');
 let SPEAKER_URL = process.env.SPEAKER_URL
   || 'http://localhost:8000';
 
-async function sendSpeakerRequest(path, body={}) {
+async function sendSpeakerRequest(path, body = {}) {
   return new Promise((resolve) => {
     axios
-    .post(SPEAKER_URL + path, body)
-    .then(() => {
-      resolve(true);
-    })
-    .catch((err) => {
-      logger.error(`${path} had an error:`, err);
-      resolve(false);
-    });
-  })
+      .post(SPEAKER_URL + path, body)
+      .then(() => {
+        resolve(true);
+      })
+      .catch((err) => {
+        logger.error(`${path} had an error:`, err);
+        resolve(false);
+      });
+  });
 }
 
 const getQueued = () => axios.get(SPEAKER_URL + '/queued');
