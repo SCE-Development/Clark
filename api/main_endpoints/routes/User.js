de0a0f22a24d9f5dd2283a8cbd914cad32f90c18
evan ugarte <evanuxd@gmail.com>
2023-11-05 01:01:33 -0700

    contitionally use  filters

--- api/main_endpoints/routes/User.js
+++ api/main_endpoints/routes/User.js
@@ -203,18 +203,24 @@ router.post('/users', async function(req, res) {
   } else if (!checkIfTokenValid(req)) {
     return res.sendStatus(UNAUTHORIZED);
   }
-  const or = ['firstName', 'lastName', 'email'].map((fieldName) => ({
-    [fieldName]: {
-      // req.body, req.query, req.body.query, oh man
-      $regex: RegExp(req.body.query, 'i'),
+  let maybeOr = {};
+  if (req.body.query) {
+    maybeOr = {
+      $or: ['firstName', 'lastName', 'email'].map((fieldName) => ({
+        [fieldName]: {
+          // req.body, req.query, req.body.query, oh man
+          $regex: RegExp(req.body.query, 'i'),
+        }
+      }))
     }
-  }));
+  }
+
   // make sure that the page we want to see is 0 by default
   // and avoid negative page numbers
   let skip = Math.max(Number(req.body.page) || 0, 0);
   skip *= ROWS_PER_PAGE;
-  const total = await User.count({ $or: or, });
-  User.find({ $or: or, }, { password: 0, }, { skip, limit: 20, })
+  const total = await User.count(maybeOr);
+  User.find(maybeOr, { password: 0, }, { skip, limit: 20, })
     .sort({ joinDate: -1 })
     .then(items => {
       res.status(OK).send({ items, total, rowsPerPage: ROWS_PER_PAGE, });
