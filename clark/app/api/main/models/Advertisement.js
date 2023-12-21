ca165a59f4686026a619e94abde409a5650407f1
pnavab <114110926+pnavab@users.noreply.github.com>
2023-12-20 22:49:38 -0800

    backend structure skeleton

---
+++ clark/app/api/main/models/Advertisement.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AdvertisementSchema = new Schema(
+  {
+    pictureUrl: {
+      type: String,
+      required: true
+    },
+    createDate: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    expireDate: {
+      type: Date
+    }
+  },
+  { collection: 'Advertisements' }
+);
+
+module.exports = mongoose.model('Advertisement', AdvertisementSchema);
