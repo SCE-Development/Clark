f19aba37edf7efff532933c218f6129581cf1af4
evan ugarte <evanuxd@gmail.com>
2024-01-03 22:10:21 -0800

    lint

--- src/Pages/URLShortener/URLShortener.js
+++ src/Pages/URLShortener/URLShortener.js
@@ -17,7 +17,7 @@ export default function URLShortenerPage(props) {
   const [successMessage, setSuccessMessage] = useState();
   const [aliasTaken, setAliasTaken] = useState();
 
-  const INPUT_CLASS = "indent-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white";
+  const INPUT_CLASS = 'indent-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white';
 
   /**
    * Cleezy page is disabled by default since you have to run the Cleezy server
