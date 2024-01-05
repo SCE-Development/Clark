09764d16a17dd4111c44ee467ebb13031d0dda5b
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2024-01-04 22:31:02 -0800

    Trim user input, correct text color on url shortener page (#1345)

--- src/Pages/URLShortener/URLShortener.js
+++ src/Pages/URLShortener/URLShortener.js
@@ -17,6 +17,9 @@ export default function URLShortenerPage(props) {
   const [successMessage, setSuccessMessage] = useState();
   const [aliasTaken, setAliasTaken] = useState();
 
+  const INPUT_CLASS = 'indent-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white';
+  const LABEL_CLASS = 'block text-sm font-medium leading-6 text-gray-300';
+
   /**
    * Cleezy page is disabled by default since you have to run the Cleezy server
    * separately. To enable, go to config.json and set ENABLED under Cleezy to true
@@ -33,7 +36,11 @@ export default function URLShortenerPage(props) {
   }
 
   async function handleCreateUrl() {
-    const response = await createUrl(url, alias, props.user.token);
+    const response = await createUrl(
+      url.trim(),
+      alias.trim(),
+      props.user.token
+    );
     if (!response.error) {
       setAllUrls([response.responseData, ...allUrls]);
       setAliasTaken(false);
@@ -51,10 +58,10 @@ export default function URLShortenerPage(props) {
     }
   }
 
-  async function maybeSubmitUrl(url) {
+  async function maybeSubmitUrl() {
     const regex =
       /^(http(s)?:\/\/)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.+~#?&//=]*)$/;
-    if (regex.test(url)) {
+    if (regex.test(url.trim())) {
       setInvalidUrl(false);
       handleCreateUrl();
     } else {
@@ -75,7 +82,7 @@ export default function URLShortenerPage(props) {
   }, []);
 
   useEffect(() => {
-    if (!useGeneratedAlias) {
+    if (useGeneratedAlias) {
       setAlias('');
     }
   }, [useGeneratedAlias]);
@@ -90,6 +97,10 @@ export default function URLShortenerPage(props) {
   useEffect(() => {
     setInvalidUrl(false);
   }, [url]);
+
+  useEffect(() => {
+    setAliasTaken(false);
+  }, [alias]);
 
   function maybeRenderErrorAlert() {
     if (invalidUrl) {
@@ -127,14 +138,14 @@ export default function URLShortenerPage(props) {
     return (<div>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">
+          <h2 className="text-base font-semibold leading-7 text-gray-300">
             Create a new link
           </h2>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 grid-cols-full sm:grid-cols-6">
             {maybeRenderErrorAlert()}
             <div className="col-span-full sm:col-span-4">
-              <label htmlFor="url" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="url" className={LABEL_CLASS}>
                 Original URL
               </label>
               <div className="mt-2">
@@ -145,7 +156,7 @@ export default function URLShortenerPage(props) {
                   placeholder="https://example.com"
                   value={url}
                   onChange={e => setUrl(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className={INPUT_CLASS}
                 />
               </div>
             </div>
@@ -161,7 +172,7 @@ export default function URLShortenerPage(props) {
             {!useGeneratedAlias && (
 
               <div className="sm:col-span-4">
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="email" className={LABEL_CLASS}>
                   Alias
                 </label>
                 <div className="mt-2">
@@ -170,7 +181,7 @@ export default function URLShortenerPage(props) {
                     name="alias"
                     value={alias}
                     onChange={e => setAlias(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className={INPUT_CLASS}
                   />
                 </div>
               </div>
@@ -181,7 +192,7 @@ export default function URLShortenerPage(props) {
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <button
-          onClick={() => setShowUrlInput(false)} type="button" className="text-sm font-semibold leading-6 text-gray-900">
+          onClick={() => setShowUrlInput(false)} type="button" className="text-sm font-semibold leading-6 text-gray-300">
           Cancel
         </button>
         <button
@@ -189,7 +200,7 @@ export default function URLShortenerPage(props) {
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           // disable button if the url is blank or if we are using an alias and its blank
           disabled={!url || (!useGeneratedAlias && !alias)}
-          onClick={() => maybeSubmitUrl(url)}
+          onClick={() => maybeSubmitUrl()}
         >
           Save
         </button>
