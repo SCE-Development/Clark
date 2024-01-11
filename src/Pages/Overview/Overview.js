cafe8e23f883e5dbb6a7b218b6b88f751d3cf6f2
Joshua <116464215+joshua-demo@users.noreply.github.com>
2024-01-10 16:22:47 -0800

    New User Manager UI (#1352)
    * initial commit
    
    * fixed mobile view layout

--- src/Pages/Overview/Overview.js
+++ src/Pages/Overview/Overview.js
@@ -140,13 +140,13 @@ export default function Overview(props) {
     const endingElementNumber = amountOfUsersOnCurrentPage + pageOffset;
     if (users.length) {
       return (
-        <nav className='flex justify-start mt-2 mb-6 mx-6'>
-          <div className='navbar-start flex items-center'>
+        <nav className='flex justify-start py-6'>
+          <div className='flex items-center navbar-start'>
             <span>
               {loading ? '...' : paginationText}
             </span>
           </div>
-          <div className='navbar-end flex justify-end space-x-3'>
+          <div className='flex justify-end space-x-3 navbar-end'>
             <button
               className='btn'
               onClick={() => setPage(page - 1)}
@@ -169,7 +169,7 @@ export default function Overview(props) {
   }
 
   return (
-    <div className='overview-container'>
+    <div className='overview-container bg-gradient-to-r from-gray-800 to-gray-600 min-h-[100dvh]'>
       <ConfirmationModal {... {
         headerText: `Delete ${userToDelete.firstName} ${userToDelete.lastName} ?`,
         bodyText: `Are you sure you want to delete 
@@ -184,90 +184,100 @@ export default function Overview(props) {
         open: toggleDelete
       }
       } />
-      <div className='mx-6'>
-        <div className='overview-search-container mt-5 mb-7 mx-6'>
-          <label className="form-control w-full">
-            <div className="label">
-              <span className="label-text text-md">Type a search, followed by the enter key</span>
-            </div>
-            <input
-              className="input input-bordered w-full"
-              type="text"
-              placeholder="search by 'first name, last name, or email'"
-              onKeyDown={(event) => {
-                if (event.key === 'Enter') {
-                  // instead of calling the backend directory, set
-                  // the page we are on to zero if the current page
-                  // we are on isn't the first page (value of 0).
-                  // by doing this, the useEffect will call the backend
-                  // for us with the correct page and query.
-                  if (page) {
-                    setPage(0);
-                  } else {
-                    callDatabase();
+      <div className='px-4'>
+        <div className='px-6 mt-8 border rounded-lg border-white/10'>
+          <div className='py-6'>
+            <label className="w-full form-control">
+              <div className="label">
+                <span className="label-text text-md">Type a search, followed by the enter key</span>
+              </div>
+              <input
+                className="w-full text-sm input input-bordered sm:text-base"
+                type="text"
+                placeholder="search by first name, last name, or email"
+                onKeyDown={(event) => {
+                  if (event.key === 'Enter') {
+                    // instead of calling the backend directory, set
+                    // the page we are on to zero if the current page
+                    // we are on isn't the first page (value of 0).
+                    // by doing this, the useEffect will call the backend
+                    // for us with the correct page and query.
+                    if (page) {
+                      setPage(0);
+                    } else {
+                      callDatabase();
+                    }
                   }
-                }
-              }}
-              onChange={event => {
-                setQuery(event.target.value);
-              }}
-            />
-          </label>
-        </div>
-        <table className='table mx-3'>
-          <thead>
-            <tr>
-              {[
-                { title: 'Name', className: 'name-cell' },
-                { title: 'Email', className: 'email-cell' },
-                { title: 'Printing', className: 'printing-cell hidden md:flex' },
-                { title: 'Verified', className: 'verified-cell' },
-                { title: 'Membership', className: 'membership-cell hidden sm:flex' },
-                { title: 'Delete', className: 'delete-cell' },
-              ].map(({ title, className }) => {
-                return (<th
-                  className={`${className}`}
-                  key={title}
-                >
-                  {title}
-                </th>);
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => {
-              return (
-                <tr className='tr break-all md:break-keep' key={user.email}>
-                  <td className='td name-cell'>
-                    <span className='text-blue-600 hover:underline'>
-                      <a target="_blank" rel="noopener noreferrer" href={`/user/edit/${user._id}`}>
-                        {formatFirstAndLastName(user)}
-                      </a>
-                    </span>
+                }}
+                onChange={event => {
+                  setQuery(event.target.value);
+                }}
+              />
+            </label>
+          </div>
+          <table className='table px-3'>
+            <thead>
+              <tr>
+                {[
+                  { title: 'Name/Email', className: 'text-base text-white/70' },
+                  { title: 'Printing', className: 'text-base text-white/70 hidden text-center md:table-cell' },
+                  { title: 'Verified', className: 'text-base text-white/70 text-center hidden sm:table-cell' },
+                  { title: 'Membership', className: 'text-base text-white/70 hidden text-center sm:table-cell' },
+                  { title: 'Delete', className: 'text-base text-white/70 text-center' },
+                ].map(({ title, className }) => (
+                  <th
+                    className={`${className}`}
+                    key={title}
+                  >
+                    {title}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr className='break-all !rounded md:break-keep hover:bg-white/10' key={user.email}>
+                  <td className=''>
+                    <a className='link link-hover link-info' target="_blank" rel="noopener noreferrer" href={`/user/edit/${user._id}`}>
+                      {formatFirstAndLastName(user)}
+                    </a>
+                    <p>{user.email}</p>
                   </td>
-                  <td className='td email-cell'>{user.email}</td>
-                  <td className='td printing-cell overview-center printing-cell hidden md:flex'>{user.pagesPrinted}/30</td>
-                  <td className='td verified-cell overview-center'>{mark(user.emailVerified)}</td>
-                  <td className='td break-keep hidden sm:flex'>
-                    {enums.membershipStateToString(user.accessLevel)}
+                  <td className='hidden md:table-cell'>
+                    <div className='flex items-center justify-center'>
+                      {user.pagesPrinted}/30
+                    </div>
                   </td>
-                  <td className='td delete-cell overview-center'>
-                    <button
-                      className='overview-icon'
-                      onClick={() => {
-                        setToggleDelete(!toggleDelete);
-                        setUserToDelete(user);
-                      }}
-                    >
-                      {svg.trashcanSymbol()}
-                    </button>
+                  <td className='hidden sm:table-cell'>
+                    <div className='flex items-center justify-center'>
+                      {mark(user.emailVerified)}
+                    </div>
+                  </td>
+                  <td className='hidden sm:table-cell'>
+                    <div className='flex items-center justify-center'>
+                      {enums.membershipStateToString(user.accessLevel)}
+                    </div>
+                  </td>
+                  <td>
+                    <div className='flex items-center justify-center'>
+                      <button
+                        className='p-2 hover:bg-white/30 rounded-xl'
+                        onClick={() => {
+                          setToggleDelete(!toggleDelete);
+                          setUserToDelete(user);
+                        }}
+                      >
+                        {svg.trashcanSymbol()}
+                      </button>
+                    </div>
                   </td>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        {maybeRenderPagination()}
+              ))}
+
+            </tbody>
+          </table>
+          {maybeRenderPagination()}
+        </div>
       </div>
     </div>
   );
