c182ba1ffb9f03867edb2dc21a7d92b83b2ac72c
Stephanie <syng828@gmail.com>
2024-02-10 02:13:37 -0800

    added delete confirmation for url shortener

--- src/Pages/URLShortener/URLShortener.js
+++ src/Pages/URLShortener/URLShortener.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 import { getAllUrls, createUrl, deleteUrl } from '../../APIFunctions/Cleezy';
 import { trashcanSymbol } from '../Overview/SVG';
+import ConfirmationModal from '../../Components/DecisionModal/ConfirmationModal.js';
 
 export default function URLShortenerPage(props) {
   const [isCleezyDisabled, setIsCleezyDisabled] = useState(false);
@@ -22,6 +23,8 @@ export default function URLShortenerPage(props) {
   const [searchQuery, setSearchQuery] = useState(null);
   const [invalidSearch, setInvalidSearch] = useState(false);
   const [errorAlertMessage, setErrorAlertMessage] = useState('');
+  const [urlToDelete, setUrlToDelete] = useState({});
+  const [toggleDelete, setToggleDelete] = useState(false);
 
   const INPUT_CLASS = 'indent-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white';
   const LABEL_CLASS = 'block text-sm font-medium leading-6 text-gray-300';
@@ -96,6 +99,7 @@ export default function URLShortenerPage(props) {
   }
 
   async function handleDeleteUrl(alias) {
+
     const response = await deleteUrl(alias, props.user.token);
     if (!response.error) {
       setAllUrls(allUrls.filter(url => url.alias !== alias));
@@ -344,81 +348,100 @@ export default function URLShortenerPage(props) {
   // the below input layout is taken from
   // https://tailwindui.com/components/application-ui/forms/form-layouts
     <div className='overview-container bg-gradient-to-r from-gray-800 to-gray-600 min-h-[100dvh]'>
-      <div className='px-4'>
-        {!loading && (
-          <div className='body-container'>
-            {maybeRenderErrorAlert()}
-            {successMessage &&
-              <div>
-                <div role="alert" className="alert alert-success mt-6">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="text-white fill-none stroke-current shrink-0 h-6 w-6" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                  <span className='text-white'>{successMessage}</span>
+      <ConfirmationModal {... {
+        headerText: `Delete ${urlToDelete.alias} for ${urlToDelete.url} ?`,
+        bodyText: `Are you sure you want to delete 
+          ${urlToDelete.alias}? It'll be gone forever if you do.`,
+        confirmText: `Yes, goodbye ${urlToDelete.alias}`,
+        cancelText: 'No, it\'s chill',
+        confirmClassAddons: 'bg-red-600 hover:bg-red-500',
+        handleConfirmation: () => {
+          handleDeleteUrl(urlToDelete.alias);
+          setToggleDelete(!toggleDelete);
+        },
+        open: toggleDelete
+      }
+      } />
+      <div className='overview-container bg-gradient-to-r from-gray-800 to-gray-600 min-h-[100dvh]'>
+        <div className='px-4'>
+          {!loading && (
+            <div className='body-container'>
+              {maybeRenderErrorAlert()}
+              {successMessage &&
+                <div>
+                  <div role="alert" className="alert alert-success mt-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="text-white fill-none stroke-current shrink-0 h-6 w-6" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                    <span className='text-white'>{successMessage}</span>
+                  </div>
+                </div>
+              }
+              <div className='p-6 mt-6 border rounded-lg border-white/10'>
+                {renderUrlButtonOrForm()}
+              </div>
+              <div className='px-6 mt-6 border rounded-lg border-white/10'>
+                <div className='py-6'>
+                  {maybeRenderSearch()}
+                </div>
+                <div className='overflow-x-auto transition'>
+                  <table className='table px-3'>
+                    <thead>
+                      <tr>
+                        {[
+                          { title: 'URL', className: 'text-base text-white/70' },
+                          { title: 'Created At', className: 'text-base text-white/70 hidden text-center sm:table-cell' },
+                          { title: 'Delete', className: 'text-base text-white/70 text-center' },
+                        ].map(({ title, className }) => (
+                          <th
+                            className={`${className}`}
+                            key={title}
+                          >
+                            {title}
+                          </th>
+                        ))}
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      {allUrls.map((url, index) => {
+                        return (
+                          <tr className='break-all !rounded md:break-keep hover:bg-white/10' key={index}>
+                            <td className=''>
+                              <a className='link link-hover link-info' target="_blank" rel="noopener noreferrer" href={`${url.link}`}>
+                                {url.alias}
+                              </a>
+                              <p>{url.url.length > 60 ? url.url.slice(0, 50) + '...' : url.url}</p>
+                            </td>
+                            <td className='hidden md:table-cell'>
+                              <div className='flex items-center justify-center'>
+                                {new Date(url.created_at).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}                            </div>
+                            </td>
+                            <td>
+                              <div className='flex items-center justify-center'>
+                                <button
+                                  className ='p-2 hover:bg-white/30 rounded-xl'
+                                  onClick={() => {
+                                    setUrlToDelete(url); setToggleDelete(!toggleDelete);
+                                  }}>
+                                  {trashcanSymbol()}
+                                </button>
+                              </div>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                  {allUrls.length === 0 && (
+                    <div className='flex flex-row w-100 justify-center'>
+                      <p className='text-lg text-white/70 mt-5 mb-5'>No results found!</p>
+                    </div>
+                  )}
+                  {maybeRenderPagination()}
                 </div>
               </div>
-            }
-            <div className='p-6 mt-6 border rounded-lg border-white/10'>
-              {renderUrlButtonOrForm()}
             </div>
-            <div className='px-6 mt-6 border rounded-lg border-white/10'>
-              <div className='py-6'>
-                {maybeRenderSearch()}
-              </div>
-              <div className='overflow-x-auto transition'>
-                <table className='table px-3'>
-                  <thead>
-                    <tr>
-                      {[
-                        { title: 'URL', className: 'text-base text-white/70' },
-                        { title: 'Created At', className: 'text-base text-white/70 hidden text-center sm:table-cell' },
-                        { title: 'Delete', className: 'text-base text-white/70 text-center' },
-                      ].map(({ title, className }) => (
-                        <th
-                          className={`${className}`}
-                          key={title}
-                        >
-                          {title}
-                        </th>
-                      ))}
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    {allUrls.map((url, index) => {
-                      return (
-                        <tr className='break-all !rounded md:break-keep hover:bg-white/10' key={index}>
-                          <td className=''>
-                            <a className='link link-hover link-info' target="_blank" rel="noopener noreferrer" href={`${url.link}`}>
-                              {url.alias}
-                            </a>
-                            <p>{url.url.length > 60 ? url.url.slice(0, 50) + '...' : url.url}</p>
-                          </td>
-                          <td className='hidden md:table-cell'>
-                            <div className='flex items-center justify-center'>
-                              {new Date(url.created_at).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}                            </div>
-                          </td>
-                          <td>
-                            <div className='flex items-center justify-center'>
-                              <button
-                                onClick={() => handleDeleteUrl(url.alias)}>
-                                {trashcanSymbol()}
-                              </button>
-                            </div>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-                {allUrls.length === 0 && (
-                  <div className='flex flex-row w-100 justify-center'>
-                    <p className='text-lg text-white/70 mt-5 mb-5'>No results found!</p>
-                  </div>
-                )}
-                {maybeRenderPagination()}
-              </div>
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
