a7831357fe9418f5c30352b52c8d42bc884386e0
joshua-demo <116464215+joshua-demo@users.noreply.github.com>
2024-02-21 16:09:13 -0800

    correct image link using discord

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -46,8 +46,7 @@ class Home extends Component {
               animate={{ opacity: 1}}
               transition={{ duration: 1.5, delay: 0.5, ease: [0.7, 0, 0.58, 1]}}
               className="w-full mx-auto transform md:w-4/5 rounded-xl"
-              src="https://private-user-images.githubusercontent.com/116464215/306809558-65bb6628-7b22-488c-89ca-fd6a9811f6d5.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDg1NjA1MDEsIm5iZiI6MTcwODU2MDIwMSwicGF0aCI6Ii8xMTY0NjQyMTUvMzA2ODA5NTU4LTY1YmI2NjI4LTdiMjItNDg4Yy04OWNhLWZkNmE5ODExZjZkNS5qcGc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMjIyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDIyMlQwMDAzMjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1lMmE2NDFlMzUxZWM0NzYxMTJkMWM0OTk1NGUxNzcxYmZiODY2MDQ3NGE0NTA2OGJlNWU5NDA1NGJiODRkOGRkJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.Jc9BgCscAosTdgjawgU12FGymmzardnlCYGOnrwRRy0"
-            />
+              src="https://cdn.discordapp.com/attachments/692922348810272829/1210015190708650055/sce_home_page_photo.jpg?ex=65e90576&is=65d69076&hm=edf8812a334ffed9fbfac95b588c41be66bb96d786501391e9886256e7edc71a&"/>
           </div>
 
         </div>
