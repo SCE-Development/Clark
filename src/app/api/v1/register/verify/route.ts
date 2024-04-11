d1a0b72e8c17d927b7d68e92442752a9ee309772
bork <bork.andrew.ryan@gmail.com>
2024-04-11 01:36:02 -0700

    Prevent verification if email is already in the database

--- src/app/api/v1/register/verify/route.ts
+++ src/app/api/v1/register/verify/route.ts
@@ -5,6 +5,7 @@ import { MEMBERSHIP_STATE } from "@/util/Constants";
 import Database from "@/util/MongoHelper";
 import Registration from "@/util/Registration";
 import BadRequest from "@/util/responses/BadRequest";
+import EmailConflict from "@/util/responses/EmailConflict";
 import InternalServerError from "@/util/responses/InternalServerError";
 
 
@@ -12,6 +13,8 @@ import InternalServerError from "@/util/responses/InternalServerError";
  * Address verification endpoint.
  * Once this endpoint is visited, the user will be added to the database.
  * This endpoint does NOT requires authentication.
+ * 
+ * @throws {EmailConflict} if the email is already in the database. THIS SHOULD NOT HAPPEN.
  * 
  * @param req 
  * @returns 
@@ -23,6 +26,13 @@ export async function GET(req: Request) {
         if(!token) throw new BadRequest();
 
         const payload = await Registration.token.decode(token);
+
+        await Database.connect();
+        const result = await UserModel.findOne({ email: payload.email }).catch((e) => { throw new InternalServerError(); });
+        
+        // SOMETHING BAD HAS HAPPENED. USER VERIFIED TWICE? WHY WAS A VERIFICATION EMAIL SENT IF THE EMAIL IS ALREADY REGISTERED?
+        if(result) throw new EmailConflict();
+
         const user = new UserModel({
             email: payload.email,
             password: payload.encryptedPassword,
@@ -31,9 +41,7 @@ export async function GET(req: Request) {
             firstName: payload.firstName,
             emailVerified: true
         });
-        await Database.connect();
-        
-        const result = await user.save().catch((e : any) => { console.log(e); throw new InternalServerError(); });
+        await user.save().catch((e : any) => { console.log(e); throw new InternalServerError(); });
         return Response.json({ success: true });
     }catch(response) {
         return response;
