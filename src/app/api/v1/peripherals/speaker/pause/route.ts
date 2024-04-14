bc5f817a597b1fbc325eed340134530b014db04e
Andrew Lin <bork.andrew.ryan@gmail.com>
2024-04-13 21:45:25 -0700

    Worked on Speakers endpoints

--- src/app/api/v1/peripherals/speaker/pause/route.ts
+++ src/app/api/v1/peripherals/speaker/pause/route.ts
@@ -1,3 +1,10 @@
+import { Session } from "@/util/Authenticate";
+import { Speaker } from "@/util/Config";
+import { MEMBERSHIP_STATE } from "@/util/Constants";
+import { parseJSON } from "@/util/ResponseHelpers";
+import Ok from "@/util/responses/Ok";
+import PeripheralNotAvaliable from "@/util/responses/PeripheralNotAvaliable";
+import { NextRequest } from "next/server";
 
 router.post('/pause', async (req, res) => {
     const { path } = req.route;
@@ -15,3 +22,34 @@ router.post('/pause', async (req, res) => {
     }
     return res.sendStatus(SERVER_ERROR);
   });
+
+
+
+
+export async function POST(req: NextRequest) {
+  try {
+    if(!Speaker.enabled) throw new PeripheralNotAvaliable();
+
+    const body = await parseJSON(req);
+    const tokenPayload = Session.authenticate(req, body, MEMBERSHIP_STATE.MEMBER);
+
+
+    const url = new URL("/print", Printer.url);
+
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        raw, copies, pageRanges, sides
+      })
+    }).catch(() => {
+      throw new InternalServerError();
+    });
+
+    return new Ok();
+  }catch(response) {
+    return response;
+  }
+}
--- src/util/Config.ts
+++ src/util/Config.ts
@@ -1,3 +1,22 @@
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY ?? "debug-secret-key";
 export const CLOUD_API_SERVER = process.env.CLOUD_API_SERVER ?? "http://localhost:4234/";
 export const BASE_URL = process.env.BASE_URL ?? "http://localhost:3000/";
+
+export const Printer = {
+    enabled: false,
+    url: "fdjsklafjdksa"
+};
+export const Speaker = {
+    enabled: false,
+    url: "dsajfkldsajflkdsajl"
+}
+
+export const LedSign = {
+    enabled: false,
+    url: "fdasjklfdjsalkfjdsla"
+}
+
+export const Cleezy = {
+    enabled: false,
+    url: "dsjfkdasljfldksajf"
+};
