ec9dfc0fafc2c40fa2de84d1237d8bc01ebe895d
weslayer <wesleywong1245@yahoo.com>
2024-04-27 14:42:00 -0700

    columns to rows

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -155,7 +155,7 @@ export default function ProjectsPage() {
           <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Recent Projects</h2>
           <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">The SCE Development Team is open to all students, no prior experience is required.</p>
         </div>
-        <div className="grid gap-8 lg:grid-cols-2 w-auto md:w-full">
+        <div className="grid gap-8 lg:grid-rows-2 w-auto md:w-full">
           {projectData.map((project) => (
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
               <div className="flex justify-between items-center mb-5 text-gray-500">
