6c9582dcc029af2c90f4650c1b6a3eef02c0ea92
weslayer <wesleywong1245@yahoo.com>
2024-05-24 23:51:20 -0700

    fix Id -> ID

--- src/Pages/UserManager/EditUserInfo.js
+++ src/Pages/UserManager/EditUserInfo.js
@@ -97,7 +97,7 @@ export default function EditUserInfo(props) {
       major,
       numberOfSemestersToSignUpFor,
       doorCode,
-      discordId: discordId,
+      discordID: discordId,
       pagesPrinted,
       accessLevel,
       emailVerified,
