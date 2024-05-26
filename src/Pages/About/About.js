a5e6ae85da14f827062d3f5d431083cc8e5edf82
weslayer <wesleywong1245@yahoo.com>
2024-05-25 20:49:16 -0700

    fix color of text on about page when on light mode (#1408)

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -13,13 +13,13 @@ export default function AboutPage() {
               <div className="absolute inset-0 bg-gray-900 opacity-80 rounded-md">
 
               </div>
-              <div className="absolute inset-0 flex items-center justify-center">
+              <div className="absolute inset-0 flex items-center justify-center text-gray-200">
                 <h1
                   className="w-1/2 text-xl md:text-4xl font-bold text-left px-5 md:px-10"
                 >
                   The Next Frontier of Innovation at San José State.
                 </h1>
-                <h3 className="w-1/2 text-left text-xs md:text-lg font-bold text-darkGrayishBlue px-5 md:px-10">
+                <h3 className="w-1/2 text-left text-xs md:text-lg font-bold px-5 md:px-10">
                   The Software and Computer Engineering Society aims
                   to provide students with sense of community, industry-relevant
                   experience and mentorship.
