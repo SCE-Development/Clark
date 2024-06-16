a8a8827ca684b37632e1368bfc8c67fb1a5fbad7
kevintsoii <kevin.tsoi44@gmail.com>
2024-06-15 18:15:17 -0700

    style change for verifyCaptcha

--- api/main_endpoints/util/captcha.js
+++ api/main_endpoints/util/captcha.js
@@ -7,9 +7,10 @@ async function verifyCaptcha(responseToken) {
   const secretKey = config.googleApiKeys.SECRET_KEY;
 
   try {
-    const response = await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${responseToken}`,
-    );
+    const url = new URL('https://www.google.com/recaptcha/api/siteverify');
+    url.searchParams.append('secret', secretKey);
+    url.searchParams.append('response', responseToken);
+    const response = await axios.post(url.href);
     return { success: response.data.success };
   } catch (error) {
     logger.error('Error verifying captcha:', error);
