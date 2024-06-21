d2756d6a3b25070a90dc56ab8874124b8c0e15a1
Jonathan Nguyen <jonathanguven@gmail.com>
2024-06-20 17:57:27 -0700

    fix foods annoying lint issue

--- api/main_endpoints/routes/Messages.js
+++ api/main_endpoints/routes/Messages.js
@@ -1,6 +1,6 @@
 const {
-    UNAUTHORIZED,
-    FORBIDDEN
+  UNAUTHORIZED,
+  FORBIDDEN
 } = require('../../util/constants').STATUS_CODES;
 const express = require('express');
 const router = express.Router();
@@ -9,65 +9,65 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 const apiKeys = {
-    user1: 'apikey1',
-    user2: 'apiKey2',
-    user3: 'apiKey3',
+  user1: 'apikey1',
+  user2: 'apiKey2',
+  user3: 'apiKey3',
 };
 
 const clients = {};
 
 const writeMessaage = ((roomId, message) => {
-    if (clients[roomId]) {
-        clients[roomId].forEach(client => client.res.write(`${JSON.stringify(message)}\n\n`));
-    }
-})
+  if (clients[roomId]) {
+    clients[roomId].forEach(client => client.res.write(`${JSON.stringify(message)}\n\n`));
+  }
+});
 
 router.post('/send', (req, res) => {
-    const {apiKey, message, id} = req.body;
-    if(!apiKey || !message || !id){
-        res.sendStatus(FORBIDDEN);
-        return;
-    }else if(!Object.values(apiKeys).includes(apiKey)){
-        res.sendStatus(UNAUTHORIZED);
-        return;
-    }
-    writeMessaage(id, message);
-    return res.json({status: 'Message sent'});
+  const {apiKey, message, id} = req.body;
+  if(!apiKey || !message || !id){
+    res.sendStatus(FORBIDDEN);
+    return;
+  }else if(!Object.values(apiKeys).includes(apiKey)){
+    res.sendStatus(UNAUTHORIZED);
+    return;
+  }
+  writeMessaage(id, message);
+  return res.json({status: 'Message sent'});
 
 });
 
 router.get('/listen', (req, res) => {
-    const {apiKey, id} = req.query;
-    if(!apiKey || !id){
-        res.sendStatus(FORBIDDEN);
-        return;
-    } else if(!Object.values(apiKeys).includes(apiKey)){
-        res.sendStatus(UNAUTHORIZED);
-        return;
+  const {apiKey, id} = req.query;
+  if(!apiKey || !id){
+    res.sendStatus(FORBIDDEN);
+    return;
+  } else if(!Object.values(apiKeys).includes(apiKey)){
+    res.sendStatus(UNAUTHORIZED);
+    return;
+  }
+
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache',
+  };
+
+  res.writeHead(200, headers);
+
+  if(!clients[id]){
+    clients[id] = [];
+  }
+
+  clients[id].push({res});
+
+  req.on('close', () => {
+    if(clients[id]){
+      clients[id] = clients[id].filter(client => client !== res);
     }
-
-    const headers = {
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache',
-    };
-
-    res.writeHead(200, headers);
-
-    if(!clients[id]){
-        clients[id] = [];
+    if(clients[id].length === 0){
+      delete clients[id];
     }
-
-    clients[id].push({res});
-
-    req.on('close', () => {
-        if(clients[id]){
-            clients[id] = clients[id].filter(client => client !== res);
-        }
-        if(clients[id].length === 0){
-            delete clients[id];
-        }
-    });
+  });
 
 });
 
