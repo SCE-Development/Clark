b459be999d2459b492dd47d7c97e5856f7102de3
jask1m <jkim94960@gmail.com>
2024-06-30 07:51:14 +0900

    add tests for modal

---
+++ test/frontend/DecisionModal.test.js
@@ -0,0 +1,31 @@
+import 'jsdom-global/register';
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+
+import ConfirmationModal from '../../src/Components/DecisionModal/ConfirmationModal';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<ConfirmationModal />', () => {
+  const modalProps = {
+    headerText: 'This the test header',
+    bodyText: 'test body',
+  }
+  const wrapper = mount(<ConfirmationModal {...modalProps} />);
+  it('Should render the headerText prop in a h3 tag', () => {
+    expect(wrapper.find('h3').text()).to.equal('This the test header');
+  });
+  it('Should render the bodyText prop in a p tag', () => {
+    expect(wrapper.find('p').text()).to.equal('test body');
+  });
+  it("Should render 'Confirm' when no prop is passed", () => {
+    const wrapper = mount(<ConfirmationModal />);
+    expect(wrapper.find('button').at(0).text()).to.equal('Confirm');
+  });
+  it("Should render 'Cancel' when no prop is passed", () => {
+    const wrapper = mount(<ConfirmationModal />);
+    expect(wrapper.find('button').at(1).text()).to.equal('Cancel');
+  });
+});
