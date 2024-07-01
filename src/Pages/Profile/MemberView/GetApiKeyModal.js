3beb1d62cd98b8efb7963ff267dea951152b2e40
Ryan <rqn.pham@gmail.com>
2024-06-30 21:19:54 -0700

    fix colors of buttons + alignment of cancel & generate buttons

--- src/Pages/Profile/MemberView/GetApiKeyModal.js
+++ src/Pages/Profile/MemberView/GetApiKeyModal.js
@@ -5,7 +5,7 @@ const GetApiKeyModal = (props) => {
   const { bannerCallback = () => {} } = props;
   const [apiKey, setApiKey] = useState('');
 
-  const INPUT_CLASS_NAME = 'indent-2 block w-full rounded-md border-0 py-1.5   shadow-sm ring-1 ring-inset ring-gray-300 placeholder:  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-white';
+  const INPUT_CLASS_NAME = 'indent-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:  focus:ring-2 focus:ring-insetfocus:ring-indigo-600 sm:text-sm sm:leading-6 text-white';
 
   async function generateKey() {
     const apiResponse = await getApiKey(props.user.token);
@@ -44,15 +44,15 @@ const GetApiKeyModal = (props) => {
 
 
         <div className="modal-action">
-          <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <div className="sm:flex sm:flex-row-reverse">
             <button
               onClick={() => generateKey()}
-              className={'btn inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto'}                    >
+              className={'bg-green-600 btn inline-flex w-full justify-center rounded-md px-3 py-2 text-sm text-white shadow-sm sm:ml-3 sm:w-auto'}                    >
                     Generate Key
             </button>
             <form method="dialog">
 
-              <button className="btn mt-3 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset sm:mt-0 sm:w-auto">
+              <button className="bg-red-600 btn mt-3 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm text-white shadow-sm ring-1 ring-inset sm:mt-0 sm:w-auto">
                   Cancel
               </button>
             </form>
