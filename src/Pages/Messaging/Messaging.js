4b6140cbdcdb83bdacaf1c0af7b3cff68f048c23
Ryan <rqn.pham@gmail.com>
2024-07-20 22:01:49 -0700

    change room added

--- src/Pages/Messaging/Messaging.js
+++ src/Pages/Messaging/Messaging.js
@@ -1,35 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { sendMessage, connectToRoom } from '../../APIFunctions/Messaging';
 import { useParams, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { use } from 'chai';
 
 export default function Messaging(props) {
   const { id } = useParams();
+  const history = useHistory();
+  const [roomIdInput, setRoomIdInput] = useState(id || '');
+  const [roomIdSubmit, setRoomIdSubmit] = useState(id || 'general');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setRoomIdSubmit(roomIdInput);
+  };
+
+  useEffect(() => {
+    if (roomIdSubmit) {
+      history.push(`/messaging/${roomIdSubmit || 'general'}`);
+    }
+  }, [roomIdSubmit]);
+
 
   return (
     <div className="w-full flex flex-col items-center justify-center h-full">
       <h1 className="text-3xl font-bold text-gray-200 mt-8">{id ? `room: ${id}` : 'no room connected'} </h1>
-      <Feed token={props.user.token} id={id} />
-      <MessagingForm token ={props.user.token} />
+      <form onSubmit={handleSubmit}
+        className="flex flex-row gap-x-2 justify-center items-center mt-1">
+        <input placeholder="Enter channel name" onChange={(e) => setRoomIdInput(e.target.value)} className="block w-2/3 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+        <button
+          type="submit"
+          className="w-1/10 px-4 h-10 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        >
+            Connect
+        </button>
+      </form>
+      <Feed token={props.user.token} id={roomIdSubmit || id} />
+      <MessagingForm token={props.user.token} id={roomIdSubmit || id} />
     </div>
   );
 }
 
 function MessagingForm(props) {
-  const [id, setId] = useState('');
-  const [apiKey, setApiKey] = useState('');
   const [message, setMessage] = useState('');
   const [sent, setSent] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const history = useHistory();
-  const { token } = props;
+  const { token, id } = props;
 
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    history.push(`/messaging/${id || 'general'}`);
-    console.log('token', token);  
     const status = await sendMessage(id, token, message);
 
     if (status.error) {
@@ -51,37 +72,7 @@ function MessagingForm(props) {
       onSubmit={handleSubmit}
       className="w-2/3 mx-auto p-4 bg-zinc-900 rounded-md shadow-lg"
     >
-      {/* <div className="mb-4">
-        <label htmlFor="room" className="block text-sm font-medium text-gray-400">
-          Room
-        </label>
-        <input
-          autoComplete="off"
-          id="room"
-          type="text"
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-          placeholder="Enter room name"
-        />
-      </div> */}
-
-      {/* <div className="mb-4">
-        <label htmlFor="key" className="block text-sm font-medium text-gray-400">
-          Key
-        </label>
-        <input
-          autoComplete="off"
-          id="key"
-          type="text"
-          value={apiKey}
-          onChange={(e) => setApiKey(e.target.value)}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-          placeholder="Enter your key"
-        />
-      </div> */}
-
-      <div className="flex flex-row items-center">
+      <div className="flex flex-row items-center gap-x-2">
         <textarea
           id="message"
           autoComplete="off"
@@ -96,27 +87,23 @@ function MessagingForm(props) {
           className="block h-10 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
           placeholder="Enter your message"
         ></textarea>
-        <button 
-        type="submit"
-        className="w-1/10 px-4 py-2 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-      >
-        Submit
-      </button>
+        <button
+          type="submit"
+          className="w-1/10 px-4 py-2 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        >
+          Submit
+        </button>
       </div>
 
-      
 
-      {sent && (
-        errorMessage ? (
+
+      {
+        sent && errorMessage && (
           <div className="mt-4 p-2 min-h-12 flex items-center justify-center bg-red-100 text-red-700 rounded-md shadow-md">
             {errorMessage}
           </div>
-        ) : (
-          <div className="mt-4 p-2 min-h-12 flex items-center justify-center bg-green-100 text-green-700 rounded-md shadow-md">
-            Message sent to room {`'${id || 'general'}'`}
-          </div>
         )
-      )}
+      }
     </form>
   );
 }
@@ -125,12 +112,14 @@ function Feed(props) {
   const { token, id } = props;
   const [messages, setMessages] = useState([]);
   const [error, setError] = useState('');
-  let eventSource;
 
   useEffect(() => {
     const roomId = id || 'general';
+    let eventSource;
 
     if (!token) return;
+
+    setMessages([]);
 
     const handleNewMessage = (data) => {
       setMessages(prevMessages => [...prevMessages, `${data}`]);
