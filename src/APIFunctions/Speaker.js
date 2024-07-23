f026a516c9b0eb96a04369472023bd36b2ebb383
Minh Phong Do <mipodo1052@gmail.com>
2024-07-23 11:46:15 -0700

    remove unrelated in package-lock.json

--- src/APIFunctions/Speaker.js
+++ src/APIFunctions/Speaker.js
@@ -1,28 +1,45 @@
 import axios from 'axios';
 import { ApiResponse } from './ApiResponses';
-import { BASE_API_URL } from '../Enums';
+
+let PERIPHERAL_API_URL = process.env.REACT_APP_PERIPHERAL_API_URL
+  || 'http://localhost:8081/peripheralapi';
 
 
 export async function queued(token) {
   let status = new ApiResponse();
-  const url = new URL('/api/Speaker/queued', BASE_API_URL);
-  await axios
-    .get(url.href, { params: { token } })
-    .then(res => {
-      status.responseData = res.data.queue;
-    })
-    .catch(err => {
-      status.responseData = err;
-      status.error = true;
-    });
+  // await axios
+  //   .get(PERIPHERAL_API_URL + '/Speaker/queued', { params: { token } })
+  //   .then(res => {
+  //     status.responseData = res.data.queue;
+  //   })
+  //   .catch(err => {
+  //     status.responseData = err;
+  //     status.error = true;
+  //   });
+  status.responseData = [
+    {
+      title: 'animation memes || (w.timestamps + credist in desc)',
+      url: 'https://www.youtube.com/watch?v=MsTuFY6jTxw',
+      thumbnail: 'https://i.ytimg.com/vi/MsTuFY6jTxw/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHoCYAC0AWKAgwIABABGH8gGygqMA8=&rs=AOn4CLDMpHhVVelTEyiPG9GY54XDy-StWzA'
+    },
+    {
+      title: 'Animations Memes Songs pt.2 (Sped up/Nightcore)',
+      url: 'https://www.youtube.com/watch?v=igYlPyEJMm4',
+      thumbnail: 'https://i.ytimg.com/vi/igYlPyEJMm4/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGH8gVygfMA8=&rs=AOn4CLArXF7ZZ8mwA6eyty_phswZOzO3y-A'
+    },
+    {
+      title: 'animation memes audios pt.4',
+      url: 'https://www.youtube.com/watch?v=d44RNQCovq0',
+      thumbnail: 'https://i.ytimg.com/vi/d44RNQCovq0/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHoCYAC0AWKAgwIABABGGUgSShFMA8=&rs=AOn4CLDKUwWRRBPfigoTsBL2k59hjdmk6Pg'
+    }
+  ];
   return status;
 }
 
-export async function addUrl(urlToAdd, token) {
+export async function addUrl(url, token) {
   let status = new ApiResponse();
-  const url = new URL('/api/Speaker/stream', BASE_API_URL);
   await axios
-    .post(url.href, {token, url: urlToAdd})
+    .post(PERIPHERAL_API_URL + '/Speaker/stream', {token, url})
     .then(res => {
       status = res.data;
     })
@@ -33,11 +50,23 @@ export async function addUrl(urlToAdd, token) {
   return status;
 }
 
+export async function setVolume(volume, token) {
+  let status = new ApiResponse();
+  await axios
+    .post(PERIPHERAL_API_URL + '/Speaker/volume', {token, volume})
+    .then(res => {
+      status = res.data;
+    })
+    .catch(err => {
+      status.responseData = err;
+      status.error = true;
+    });
+}
+
 export async function skip(token) {
   let status = new ApiResponse();
-  const url = new URL('/api/Speaker/skip');
   await axios
-    .post(url.href, {token})
+    .post(PERIPHERAL_API_URL + '/Speaker/skip', {token})
     .then(res => {
       status = res.data;
     })
@@ -50,9 +79,8 @@ export async function skip(token) {
 
 export async function pause(token) {
   let status = new ApiResponse();
-  const url = new URL('/api/Speaker/pause');
   await axios
-    .post(url.href, {token})
+    .post(PERIPHERAL_API_URL + '/Speaker/pause', {token})
     .then(res => {
       status = res.data;
     })
@@ -65,9 +93,8 @@ export async function pause(token) {
 
 export async function resume(token) {
   let status = new ApiResponse();
-  const url = new URL('/api/Speaker/resume');
   await axios
-    .post(url.href, {token})
+    .post(PERIPHERAL_API_URL + '/Speaker/resume', {token})
     .then(res => {
       status = res.data;
     })
--- src/Pages/Speaker/Speaker.js
+++ src/Pages/Speaker/Speaker.js
@@ -10,6 +10,10 @@ function SpeakersPage(props) {
   const [playbuttonColor, setPlaybuttonColor] = useState('secondary');
   const [queuedSongs, setQueuedSongs] = useState([]);
   const [error, setError] = useState();
+  const [queue, setQueue] = useState([]);
+  const [nextUpSong, setNextUpSong] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(true); // Track playback state
+  const [volume, setVolumeState] = useState(50); // Initial volume state
 
   const validateUrl = () => {
     setUrl(url.trim());
@@ -34,22 +38,48 @@ function SpeakersPage(props) {
     }
   };
 
+  const updateDisplay = (queuedSongs) => {
+    if (queuedSongs.length > 0) {
+      setNextUpSong(queuedSongs[0]);
+      setQueue(queuedSongs.slice(1));
+    } else {
+      setNextUpSong(null);
+      setQueue([]);
+    }
+  };
+
   const getQueuedSongs = async () => {
     const songList = await queued(props.user.token);
     if (songList.error) {
       setError('Unable to reach speaker: ' + String(songList.responseData));
     }
     if (Array.isArray(songList.responseData)) {
-      setQueuedSongs(songList.responseData);
+      let q = songList.responseData;
+      setQueuedSongs(q);
+      updateDisplay(q);
     }
   };
-
 
   const modifySpeakerWrapper = async (modifier) => {
     const result = await modifier(props.user.token);
     if (result.error) {
       setError(String(result.responseData));
     }
+  };
+
+  const togglePlayback = async () => {
+    if (isPlaying) {
+      await modifySpeakerWrapper(pause);
+    } else {
+      await modifySpeakerWrapper(resume);
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleVolumeChange = async (volume) => {
+    setVolumeState(volume); // Update local volume state
+    const result = await setVolume(volume, props.user.token);
+    // console.log('Volume set to: ' + volume);
   };
 
   useEffect(() => {
@@ -60,27 +90,17 @@ function SpeakersPage(props) {
     <div>
       <div className='flex justify-center pt-10'>
         <div className="md:w-1/2">
-          <input placeholder='Enter YouTube Link' onChange=
-            {(e) => setUrl(e.target.value)}
-          className="sign-input indent-2 w-full h-8"
-          >
-          </input>
-          {
-            error && <p style={{ color: 'red', paddingTop: '7px' }}>{error}</p>
-          }
-          <div className='w-full py-4'>
+          <div className='flex justify-center'>
+            <input placeholder='Enter YouTube Link' onChange=
+              {(e) => setUrl(e.target.value)}
+            className="sign-input indent-2 w-full h-8 inline-block"
+            >
+            </input>
+            {
+              error && <p style={{ color: 'red', paddingTop: '7px' }}>{error}</p>
+            }
             <button
-              className="btn w-1/3"
-              onClick={() => modifySpeakerWrapper(pause)}>
-                Pause
-            </button>
-            <button
-              className="btn w-1/3"
-              onClick={() => modifySpeakerWrapper(resume)}>
-                Resume
-            </button>
-            <button
-              className="btn w-1/3"
+              className="inline-block btn w-1/3"
               onClick={playSong}
               disabled={!url}
               color={playbuttonColor}
@@ -88,22 +108,58 @@ function SpeakersPage(props) {
               {playText}
             </button>
           </div>
-          <button onClick={() => modifySpeakerWrapper(skip)} className="btn w-full bg-red-600 hover:bg-red-700 text-white text-3xl">
-                Skip
-          </button>
-          <div className='mt-20 items-center'>
+          <div className="flex items-center w-full py-4">
+            <button
+              className="btn w-1/3 bg-gray-500 text-white hover:bg-black-600 mr-2"
+              onClick={togglePlayback}>
+              {isPlaying ? 'Pause' : 'Resume'}
+            </button>
+            <button onClick={() => modifySpeakerWrapper(skip)} className="btn w-1/3 bg-gray-500 hover:bg-black-600 text-white mr-2">
+              Skip
+            </button>
+            <div className="flex items-center w-1/3">
+              <span className="mr-2">Volume</span>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={volume}
+                onChange={(e) => handleVolumeChange(e.target.value)}
+                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center items-center flex-col">
+            <div><h2 className='text-center m-2'>Next Up</h2></div>
+            <div>
+              {nextUpSong && (
+                <div className='flex items-center flex-col'>
+                  <img src={nextUpSong.thumbnail} alt={nextUpSong.title} className='w-3/4 h-3/4 m-4' />
+                  <div>
+                    <a href={nextUpSong.url} target="_blank" rel="noopener noreferrer">
+                      {nextUpSong.title}
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className='mt-10 items-center'>
             <h2 className='text-center' >Queued</h2>
-            <table>
+            <table className="table-auto border-collapse w-full border-separate border-spacing-x-10 border-spacing-y-5">
               <thead>
                 <th>Position</th>
                 <th className='text-left pl-2'>Name</th>
               </thead>
               <tbody>
                 {
-                  queuedSongs.map((song, index) => (
+                  queue.map((song, index) => (
                     <tr key={index}>
-                      <td>{index}</td>
-                      <td className='pl-8'><a href={song}>{song}</a></td>
+                      <td>{index + 1}</td>
+                      <td className='pl-2'><a href={song.url}>{song.title}</a></td>
+                      <img
+                        src = {song.thumbnail}
+                      />
                     </tr>
                   ))
                 }
