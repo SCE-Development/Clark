d9d9479444796bcc6b25a8c144a5ff170bd5c44f
Minh Phong Do <mipodo1052@gmail.com>
2024-07-23 11:59:49 -0700

    Change setVolume function to match new API

--- src/APIFunctions/Speaker.js
+++ src/APIFunctions/Speaker.js
@@ -1,47 +1,20 @@
 import axios from 'axios';
 import { ApiResponse } from './ApiResponses';
-
-let PERIPHERAL_API_URL = process.env.REACT_APP_PERIPHERAL_API_URL
-  || 'http://localhost:8081/peripheralapi';
+import { BASE_API_URL } from '../Enums';
 
 
 export async function queued(token) {
   let status = new ApiResponse();
-  // await axios
-  //   .get(PERIPHERAL_API_URL + '/Speaker/queued', { params: { token } })
-  //   .then(res => {
-  //     status.responseData = res.data.queue;
-  //   })
-  //   .catch(err => {
-  //     status.responseData = err;
-  //     status.error = true;
-  //   });
-  status.responseData = [
-    {
-      title: 'animation memes || (w.timestamps + credist in desc)',
-      url: 'https://www.youtube.com/watch?v=MsTuFY6jTxw',
-      thumbnail: 'https://i.ytimg.com/vi/MsTuFY6jTxw/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHoCYAC0AWKAgwIABABGH8gGygqMA8=&rs=AOn4CLDMpHhVVelTEyiPG9GY54XDy-StWzA'
-    },
-    {
-      title: 'Animations Memes Songs pt.2 (Sped up/Nightcore)',
-      url: 'https://www.youtube.com/watch?v=igYlPyEJMm4',
-      thumbnail: 'https://i.ytimg.com/vi/igYlPyEJMm4/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGH8gVygfMA8=&rs=AOn4CLArXF7ZZ8mwA6eyty_phswZOzO3y-A'
-    },
-    {
-      title: 'animation memes audios pt.4',
-      url: 'https://www.youtube.com/watch?v=d44RNQCovq0',
-      thumbnail: 'https://i.ytimg.com/vi/d44RNQCovq0/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHoCYAC0AWKAgwIABABGGUgSShFMA8=&rs=AOn4CLDKUwWRRBPfigoTsBL2k59hjdmk6Pg'
+  const url = new URL('/api/Speaker/queued', BASE_API_URL);
+  await axios
+    .get(url.href, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
     }
-  ];
-  return status;
-}
-
-export async function addUrl(url, token) {
-  let status = new ApiResponse();
-  await axios
-    .post(PERIPHERAL_API_URL + '/Speaker/stream', {token, url})
+    )
     .then(res => {
-      status = res.data;
+      status.responseData = res.data.queue;
     })
     .catch(err => {
       status.responseData = err;
@@ -50,10 +23,18 @@ export async function addUrl(url, token) {
   return status;
 }
 
-export async function setVolume(volume, token) {
+export async function addUrl(urlToAdd, token) {
   let status = new ApiResponse();
+  const url = new URL('/api/Speaker/stream', BASE_API_URL);
   await axios
-    .post(PERIPHERAL_API_URL + '/Speaker/volume', {token, volume})
+    .post(url.href, {
+      url: urlToAdd
+    }, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    )
     .then(res => {
       status = res.data;
     })
@@ -61,12 +42,19 @@ export async function setVolume(volume, token) {
       status.responseData = err;
       status.error = true;
     });
+  return status;
 }
 
 export async function skip(token) {
   let status = new ApiResponse();
+  const url = new URL('/api/Speaker/skip');
   await axios
-    .post(PERIPHERAL_API_URL + '/Speaker/skip', {token})
+    .post(url.href, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    )
     .then(res => {
       status = res.data;
     })
@@ -79,8 +67,14 @@ export async function skip(token) {
 
 export async function pause(token) {
   let status = new ApiResponse();
+  const url = new URL('/api/Speaker/pause');
   await axios
-    .post(PERIPHERAL_API_URL + '/Speaker/pause', {token})
+    .post(url.href, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    )
     .then(res => {
       status = res.data;
     })
@@ -93,8 +87,14 @@ export async function pause(token) {
 
 export async function resume(token) {
   let status = new ApiResponse();
+  const url = new URL('/api/Speaker/resume');
   await axios
-    .post(PERIPHERAL_API_URL + '/Speaker/resume', {token})
+    .post(url.href, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    )
     .then(res => {
       status = res.data;
     })
@@ -103,4 +103,25 @@ export async function resume(token) {
       status.error = true;
     });
   return status;
+}
+
+export async function setVolume(volumeToSet, token) {
+  let status = new ApiResponse();
+  const url = new URL('/api/Speaker/volume', BASE_API_URL);
+  await axios
+    .post(url.href, {
+      volume: volumeToSet
+    }, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    )
+    .then(res => {
+      status = res.data;
+    })
+    .catch(err => {
+      status.responseData = err;
+      status.error = true;
+    });
 }
