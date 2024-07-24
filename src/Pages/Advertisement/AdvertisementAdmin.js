05578f64fc82f7fb1e7ab5fc07fbaed14d5539c7
Ethan Le <ethanisverybusyrightnow@gmail.com>
2024-07-24 10:10:16 -0700

    Created new Advertisement admin page and connected to routing and admin navbar

---
+++ src/Pages/Advertisement/AdvertisementAdmin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AdvertisementAdmin() {
+  return (
+    <div className='m-10'>
+      <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Welcome to the Advertisement Admin Page!!
+      </h1>
+    </div>
+  );
+}
