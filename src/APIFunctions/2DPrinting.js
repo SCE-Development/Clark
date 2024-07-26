78b60e5afef0eb586a5a06d582bc6db65da31fd9
evan ugarte <evanuxd@gmail.com>
2024-07-25 20:55:31 -0700

    pass in token again

--- src/APIFunctions/2DPrinting.js
+++ src/APIFunctions/2DPrinting.js
@@ -75,13 +75,13 @@ export function parseRange(pages, maxPages) {
  * @returns {ApiResponse} - Containing information for if
  * the page successfully printed
  */
-export async function printPage(data) {
+export async function printPage(data, token) {
   let status = new ApiResponse();
   const url = new URL('/api/Printer/sendPrintRequest', BASE_API_URL);
   await axios.post(url.href, data, {
     headers: {
       'Content-Type': 'multipart/form-data',
-      'Authorization': `Bearer ${data.token}`
+      'Authorization': `Bearer ${token}`
     }
   })
     .then(response => {
--- src/Pages/2DPrinting/2DPrinting.js
+++ src/Pages/2DPrinting/2DPrinting.js
@@ -190,8 +190,7 @@ export default function Printing(props) {
     data.append('file', PdfFile);
     data.append('sides', sides);
     data.append('copies', copies);
-    data.append('token', props.user.token);
-    let status = await printPage(data);
+    let status = await printPage(data, props.user.token);
 
     if (!status.error) {
       editUser(
