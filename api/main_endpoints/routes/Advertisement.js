b9505723c24171ef388dbcbb27d4637229855732
Ethan Le <77454689+cuzethan@users.noreply.github.com>
2024-07-28 20:28:17 -0700

    Ethan admin page for ads (#1490)
    * created homepage text on display, grabbed from api that returns hardcoded set of phrases
    
    * fixed lint error and deleted unused api tests
    
    * fixed logic errors
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    * Added admin page to control homepage message
    
    * remove redundant checkIfEmailExists on user registration, fix user registration endpoint url (#1486)
    
    * remove redundant checkIfEmailExists on user registration, fix user registration endpoint url
    
    * update user tests: remove checkIfUserExists endpoint, refactor test user creation
    
    * chore: Fix error handling and message in user registration
    
    * created homepage text on display, grabbed from api that returns hardc… (#1489)
    
    * created homepage text on display, grabbed from api that returns hardcoded set of phrases
    
    * fixed lint error and deleted unused api tests
    
    * fixed logic errors
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    * fixed small mistake
    
    * fixed lint errors
    
    ---------
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    Co-authored-by: Oliver (Chia-Hong) Chou <49494541+knhn1004@users.noreply.github.com>

--- api/main_endpoints/routes/Advertisement.js
+++ api/main_endpoints/routes/Advertisement.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { OK, BAD_REQUEST } = require('../../util/constants').STATUS_CODES;
+const Advertisement = require('../models/Advertisement');
 
-router.get('/getAdvertisements', (req, res) => {
-  const newAd = [
-    'this is the best club ever ?XD',
-    'Join us for Pancake Thursdays at 11am!',
-    'Located at Engineering Building room 294!',
-    'Ethan was here kekekek ( ͡° ͜ʖ ͡°)',
-  ];
-  const index = Math.floor(Math.random() * (4));
-  res.send(newAd[index]);
+router.get('/getAllAdvertisements', (req, res) => {
+  Advertisement.find()
+    .then(items => res.status(OK).send(items))
+    .catch(error => {
+      res.sendStatus(BAD_REQUEST);
+    });
+});
+
+router.post('/createAdvertisement', (req, res) => {
+  const newAd = new Advertisement({
+    message: req.body.message,
+    expireDate: req.body.expireDate
+  });
+
+  Advertisement.create(newAd)
+    .then((post) => {
+      return res.json(post);
+    })
+    .catch(
+      (error) => res.sendStatus(BAD_REQUEST)
+    );
+});
+
+router.post('/deleteAdvertisement', (req, res) => {
+  Advertisement.deleteOne({ _id: req.body._id })
+    .then(result => {
+      if (result.n < 1) {
+        res.sendStatus(NOT_FOUND);
+      } else {
+        res.sendStatus(OK);
+      }
+    })
+    .catch(() => {
+      res.sendStatus(BAD_REQUEST);
+    });
 });
 
 module.exports = router;
