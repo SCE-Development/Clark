b0a0edb9fb3fe3d4cf247239eab1b5d29adf7fe8
Ryan <rqn.pham@gmail.com>
2024-08-13 12:02:29 -0700

    format it in 12 hour time instead of 24 hour time

--- src/Pages/Messaging/Messaging.js
+++ src/Pages/Messaging/Messaging.js
@@ -164,8 +164,7 @@ function Feed(props) {
                     month: '2-digit',
                     day: '2-digit',
                     hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: false,
+                    minute: '2-digit'
                   }
                 ).replace(',', ' ')}
             </div>
