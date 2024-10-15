670ca5dc71819dc85c2db80eca3432daac1fd106
Brendan Ly <brendanly4@gmail.com>
2024-10-15 02:12:29 -0700

    implemented expiration date setting

--- src/Pages/SCEatsAdmin/SCEatsAdmin.js
+++ src/Pages/SCEatsAdmin/SCEatsAdmin.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { getAllFoods, createFood, deleteFood, editFood } from '../../APIFunctions/Foods';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css"
 
 export default function SCEatsAdmin(props) {
   const [foods, setFoods] = useState([]);
@@ -10,6 +12,8 @@ export default function SCEatsAdmin(props) {
   const [quantity, setQuantity] = useState();
   const [expiration, setExpiration] = useState();
 
+  const [photoPreview, setPhotoPreview] = useState(null);
+
   const [editingId, setEditingId] = useState(null);
   const [editedFood, setEditedFood] = useState({
     name: "",
@@ -18,6 +22,18 @@ export default function SCEatsAdmin(props) {
     quantity: "",
     expiration: ""
   });
+
+  const handlePhotoChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setPhoto(file)
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPhotoPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   const handleCreate = async () => {
     await createFood({
@@ -70,8 +86,8 @@ export default function SCEatsAdmin(props) {
         SCEats Admin Page
       </h1>
 
-      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 grid-cols-full sm:grid-cols-6">
-        <div className="col-span-full sm:col-span-4">
+      <div className="mt-10 flex flex-col md:grid md:grid-cols-1 md:gap-x-6 md:gap-y-8 lg:flex lg:flex-row md:flex-wrap lg:gap-x-6 lg:gap-y-8">
+        <div className="flex-1 min-w-[200px]">
           <label htmlFor="name" className='block text-sm font-medium leading-6 text-gray-300'>
             Food Name
           </label>
@@ -87,7 +103,7 @@ export default function SCEatsAdmin(props) {
             />
           </div>
         </div>
-        <div className="col-span-full sm:col-span-4">
+        <div className="flex-1 min-w-[200px]">
           <label htmlFor="photo" className='block text-sm font-medium leading-6 text-gray-300'>
             Photo
           </label>
@@ -102,8 +118,7 @@ export default function SCEatsAdmin(props) {
             />
           </div>
         </div>
-
-        <div className="col-span-full sm:col-span-4">
+        <div className="flex-1 min-w-[200px]">
           <label htmlFor="price" className='block text-sm font-medium leading-6 text-gray-300'>
             Price
           </label>
@@ -118,7 +133,7 @@ export default function SCEatsAdmin(props) {
             />
           </div>
         </div>
-        <div className="col-span-full sm:col-span-4">
+        <div className="flex-1 min-w-[200px]">
           <label htmlFor="quantity" className='block text-sm font-medium leading-6 text-gray-300'>
             Quantity
           </label>
@@ -133,24 +148,30 @@ export default function SCEatsAdmin(props) {
             />
           </div>
         </div>
-
-        <div className="col-span-full sm:col-span-4">
+        <div className="flex-1 min-w-[200px]">
           <label htmlFor="expiration" className='block text-sm font-medium leading-6 text-gray-300'>
             Expiration Date
           </label>
           <div className="mt-2">
-            <input
+            {/* <input
               type="text"
               name="expiration"
               id="expiration"
               value={expiration}
               onChange={e => setExpiration(e.target.value)}
               className={INPUT_CLASS}
+            /> */}
+            <DatePicker
+              name="expiration"
+              id="expiration"
+              selected={expiration}
+              onChange={(date) => setExpiration(date)}
+              dateFormat="MM-dd-y"
+              className={INPUT_CLASS}
             />
           </div>
         </div>
-
-        <div className="col-span-full sm:col-span-4">
+        <div className="flex-1 flex items-center ">
           <button
             type="submit"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -159,6 +180,7 @@ export default function SCEatsAdmin(props) {
             Save
           </button>
         </div>
+
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
@@ -181,8 +203,8 @@ export default function SCEatsAdmin(props) {
                 food.name
               )}
             </h2>
-            <p className="text-gray-700 mt-1">
-              Price: {editingId === food._id ? (
+            <p className="text-white-700 mt-1">
+              Price: ${editingId === food._id ? (
                 <input
                   type="text"
                   value={editedFood.price}
@@ -194,10 +216,10 @@ export default function SCEatsAdmin(props) {
                   }
                 />
               ) : (
-                `${food.price.toFixed(2)}`
+                food.price.toFixed(2)
               )}
             </p>
-            <p className="text-gray-700 mt-1">
+            <p className="text-white-700 mt-1">
               Quantity: {editingId === food._id ? (
                 <input
                   type="text"
@@ -214,7 +236,22 @@ export default function SCEatsAdmin(props) {
               )}
             </p>
             <div className="flex justify-between items-center">
-              <p className="text-gray-700">Expiration: {food.expiration ? new Date(food.expiration).toLocaleDateString() : 'N/A'}</p>
+              <p className="text-white-700">
+                Expiration: {editingId === food._id ? (
+                  <DatePicker
+                    selected={editedFood.expiration}
+                    onChange={(date) => 
+                      setEditedFood({
+                        ...editedFood,
+                        expiration: date
+                      })
+                    }
+                    dateFormat="MM-dd-y"
+                  />
+                ) : (
+                  food.expiration ? new Date(food.expiration).toLocaleDateString() : 'N/A'
+                )}
+              </p>
               <div className="flex space-x-2">
                 {editingId === food._id ? (
                   <button
@@ -223,7 +260,7 @@ export default function SCEatsAdmin(props) {
                 ) : (
                   <>
                     <button onClick={() => handleEdit(food)} className="bg-blue-500 text-white px-2 py-2 rounded-md">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path stroke-dasharray="20" stroke-dashoffset="20" d="M3 21h18"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.2s" values="20;0" /></path><path stroke-dasharray="48" stroke-dashoffset="48" d="M7 17v-4l10 -10l4 4l-10 10h-4"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.2s" dur="0.6s" values="48;0" /></path><path stroke-dasharray="8" stroke-dashoffset="8" d="M14 6l4 4"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.8s" dur="0.2s" values="8;0" /></path></g></svg>                
+                      <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path stroke-dasharray="20" stroke-dashoffset="20" d="M3 21h18"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.2s" values="20;0" /></path><path stroke-dasharray="48" stroke-dashoffset="48" d="M7 17v-4l10 -10l4 4l-10 10h-4"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.2s" dur="0.6s" values="48;0" /></path><path stroke-dasharray="8" stroke-dashoffset="8" d="M14 6l4 4"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.8s" dur="0.2s" values="8;0" /></path></g></svg>
                     </button>
                     <button onClick={async () => handleDelete(food._id)} className="bg-red-500 text-white px-2 py-2 rounded-md">
                       <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z" /></svg>
