7d7f02000ae5f8513d892c19ba2b499f2b6c21d9
Brendan Ly <brendanly4@gmail.com>
2024-10-16 14:45:54 -0700

    button changes

--- src/Pages/SCEatsAdmin/SCEatsAdmin.js
+++ src/Pages/SCEatsAdmin/SCEatsAdmin.js
@@ -153,14 +153,6 @@ export default function SCEatsAdmin(props) {
             Expiration Date
           </label>
           <div className="mt-2">
-            {/* <input
-              type="text"
-              name="expiration"
-              id="expiration"
-              value={expiration}
-              onChange={e => setExpiration(e.target.value)}
-              className={INPUT_CLASS}
-            /> */}
             <DatePicker
               name="expiration"
               id="expiration"
@@ -171,16 +163,15 @@ export default function SCEatsAdmin(props) {
             />
           </div>
         </div>
-        <div className="flex-1 flex items-center ">
-          <button
-            type="submit"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={() => handleCreate()}
-          >
-            Save
-          </button>
-        </div>
-
+      </div>
+      <div className="flex-1 flex mt-8">
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          onClick={() => handleCreate()}
+        >
+          Save
+        </button>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
@@ -240,7 +231,7 @@ export default function SCEatsAdmin(props) {
                 Expiration: {editingId === food._id ? (
                   <DatePicker
                     selected={editedFood.expiration}
-                    onChange={(date) => 
+                    onChange={(date) =>
                       setEditedFood({
                         ...editedFood,
                         expiration: date
