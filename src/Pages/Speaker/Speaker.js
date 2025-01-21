4e75f326e54a6184602426b3db9574b1570e0c90
Patrick Le <patricktle924@gmail.com>
2025-01-21 01:06:27 -0800

    Fix up Queue Formatting and Page Spacing

--- src/Pages/Speaker/Speaker.js
+++ src/Pages/Speaker/Speaker.js
@@ -154,65 +154,67 @@ function SpeakersPage(props) {
   }, []);
 
   return (
-    <div className="h-3/4 border-black border-solid border-4">
+    <div className="h-3/4 lborder-black lborder-solid lborder-4">
       {/**Main div containing search bar,  play/skip/rewind, next-up, position/name */}
-      <div className='flex justify-center pt-10 h-full border-yellow-500 border-solid border-4'>
-        <div className="w-3/4 flex flex-col justify-between border-green-500 border-solid border-4">
-        {/** Div Containing Input and Play Button */}
-          <div className="flex justify-center items-center gap-2 border-red-500 border-solid border-4">
-            <input placeholder='Enter YouTube Link' onChange=
-              {(e) => setUrl(e.target.value)}
-            className="sign-input indent-2 w-full h-8 inline-block"
-            >
-            </input>
-            {
-              error && <p style={{ color: 'red', paddingTop: '7px' }}>{error}</p>
-            }
-            <button
-              className={`text-white inline-block btn w-1/3 ${playbuttonColor} hover:bg-cyan-600 disabled:bg-cyan-800 disabled:text-gray-400`}
-              onClick={playSong}
-              disabled={!url}
-              // color={playbuttonColor}
-            >
-              {playText}
-            </button>
-            <button onClick={() => setSongIn(false)}>Stop</button>
-          </div>
-          {/*Mini play bar */}
-          <div className="mt-6 mb-4 flex justify-center items-center border-red-500 border-solid border-4">
-            <button 
-            className="p-3 rounded-full focus:outline-none bg-cyan-700 hover:bg-cyan-600  disabled:bg-cyan-800 disabled:text-gray-400" 
-            onClick={handleRewind}
-            disabled={!songIn}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`size-4 fill-current ${songIn ? 'text-white' : 'text-gray-400'}`}>
-                <path d="M9.195 18.44c1.25.714 2.805-.189 2.805-1.629v-2.34l6.945 3.968c1.25.715 2.805-.188 2.805-1.628V8.69c0-1.44-1.555-2.343-2.805-1.628L12 11.029v-2.34c0-1.44-1.555-2.343-2.805-1.628l-7.108 4.061c-1.26.72-1.26 2.536 0 3.256l7.108 4.061Z" />
-              </svg>
-            </button>
-            <button
-              className="p-4 rounded-full focus:outline-none mx-4  bg-cyan-700 hover:bg-cyan-600  disabled:bg-cyan-800 disabled:text-gray-400"
-              onClick={togglePlayback}
-              disabled={!songIn}
-            >
-              {isPlaying ? (
-                // Pause icon
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`w-6 h-6 fill-current ${songIn ? 'text-white' : 'text-gray-400'}`}>
-                  <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z" clipRule="evenodd" />
+      <div className='flex justify-center pt-10 h-full lborder-yellow-500 lborder-solid lborder-4'>
+        <div className="w-3/4 flex flex-col justify-between lborder-green-500 lborder-solid lborder-4">
+          <div className='lborder-pink-500 lborder-solid lborder-4'>
+            {/** Div Containing Input and Play Button */}
+            <div className="flex justify-center items-center gap-2 lborder-red-500 lborder-solid lborder-4">
+              <input placeholder='Enter YouTube Link' onChange=
+                {(e) => setUrl(e.target.value)}
+              className="sign-input indent-2 w-full h-8 inline-block"
+              >
+              </input>
+              {
+                error && <p style={{ color: 'red', paddingTop: '7px' }}>{error}</p>
+              }
+              <button
+                className={`text-white inline-block btn w-1/3 ${playbuttonColor} hover:bg-cyan-600 disabled:bg-cyan-800 disabled:text-gray-400`}
+                onClick={playSong}
+                disabled={!url}
+                // color={playbuttonColor}
+              >
+                {playText}
+              </button>
+              <button onClick={() => setSongIn(false)}>Stop</button>
+            </div>
+            {/*Mini play bar */}
+            <div className="mt-6 mb-4 flex justify-center items-center lborder-red-500 lborder-solid lborder-4">
+              <button 
+              className="p-3 rounded-full focus:outline-none bg-cyan-700 hover:bg-cyan-600  disabled:bg-cyan-800 disabled:text-gray-400" 
+              onClick={handleRewind}
+              disabled={!songIn}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`size-4 fill-current ${songIn ? 'text-white' : 'text-gray-400'}`}>
+                  <path d="M9.195 18.44c1.25.714 2.805-.189 2.805-1.629v-2.34l6.945 3.968c1.25.715 2.805-.188 2.805-1.628V8.69c0-1.44-1.555-2.343-2.805-1.628L12 11.029v-2.34c0-1.44-1.555-2.343-2.805-1.628l-7.108 4.061c-1.26.72-1.26 2.536 0 3.256l7.108 4.061Z" />
                 </svg>
-              ) : (
-                // Play icon
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`w-6 h-6 fill-current ${songIn ? 'text-white' : 'text-gray-400'}`}>
-                  <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
+              </button>
+              <button
+                className="p-4 rounded-full focus:outline-none mx-4  bg-cyan-700 hover:bg-cyan-600  disabled:bg-cyan-800 disabled:text-gray-400"
+                onClick={togglePlayback}
+                disabled={!songIn}
+              >
+                {isPlaying ? (
+                  // Pause icon
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`w-6 h-6 fill-current ${songIn ? 'text-white' : 'text-gray-400'}`}>
+                    <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z" clipRule="evenodd" />
+                  </svg>
+                ) : (
+                  // Play icon
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`w-6 h-6 fill-current ${songIn ? 'text-white' : 'text-gray-400'}`}>
+                    <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
+                  </svg>
+                )}
+              </button>
+              <button className="p-3 rounded-full focus:outline-none bg-cyan-700 hover:bg-cyan-600  disabled:bg-cyan-800 disabled:text-gray-400" onClick={handleForward} disabled={!songIn}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`size-4 fill-current ${songIn ? 'text-white' : 'text-gray-400'}`}>
+                  <path d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z" />
                 </svg>
-              )}
-            </button>
-            <button className="p-3 rounded-full focus:outline-none bg-cyan-700 hover:bg-cyan-600  disabled:bg-cyan-800 disabled:text-gray-400" onClick={handleForward} disabled={!songIn}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`size-4 fill-current ${songIn ? 'text-white' : 'text-gray-400'}`}>
-                <path d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z" />
-              </svg>
-            </button>
+              </button>
+            </div>
           </div>
           {/**Next up Div */}
-          {/**<div className="flex justify-center items-center flex-col border-red-500 border-solid border-4">
+          {/**<div className="flex justify-center items-center flex-col lborder-red-500 lborder-solid lborder-4">
             <button className="p-3 rounded-full focus:outline-none hover:bg-gray-700 transition-colors duration-300" onClick={() => modifySpeakerWrapper(skip)}>
               <svg className="h-8 w-8 text-gray-100" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="5 4 15 12 5 20 5 4"/><line x1="19" y1="5" x2="19" y2="19"/></svg>
             </button>
@@ -230,8 +232,8 @@ function SpeakersPage(props) {
               )}
             </div>
           </div>
-          {<div className='mt-10 mb-20 items-center border-red-500 border-solid border-4'>
-            <table className="table-auto border-collapse w-full border-spacing-x-10 border-spacing-y-5">
+          {<div className='mt-10 mb-20 items-center lborder-red-500 lborder-solid lborder-4'>
+            <table className="table-auto lborder-collapse w-full lborder-spacing-x-10 lborder-spacing-y-5">
               <thead>
                 <th>Position</th>
                 <th className='text-left pl-2'>Name</th>
@@ -252,7 +254,7 @@ function SpeakersPage(props) {
             </table>
           </div>}**/}
           {/**New Queue Bar */}
-          <div className='flex flex-col items-center  border-purple-500 border-solid border-4'>
+          <div className='flex flex-col items-center  lborder-purple-500 lborder-solid lborder-4'>
             {q.map((song, index) => {
               return <SongCard key={index} title={song.title} thumbnailURL={song.thumbnailURL}/>
             })}
@@ -316,7 +318,7 @@ const SongCard = ({ title, thumbnailURL}) => {
   //new component to represent a song 
 
   return (
-  <div className="flex rounded-md items-center justify-center bg-gray-100 px-6 border-blue-500 border-solid border-4">
+  <div className="w-3/4 flex rounded-md items-center justify-between bg-gray-100 px-6 lborder-blue-500 lborder-solid lborder-4">
     <div>{title}</div>
     <img src={thumbnailURL} alt ="video thumbnail"/>
   </div>)
