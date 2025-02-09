eda5ece2dd5852b6a305612e1000f4a3bc52cd5b
Evan Ugarte <evanuxd@gmail.com>
2025-02-08 16:37:52 -0800

    Un-nest user registration logic

--- api/main_endpoints/routes/Auth.js
+++ api/main_endpoints/routes/Auth.js
@@ -33,19 +33,17 @@ const { userWithEmailExists, checkIfPageCountResets, findPasswordReset } = requi
 // Register a member
 router.post('/register', async (req, res) => {
   const registrationStatus = await registerUser(req.body);
-  if (!registrationStatus.userSaved) {
-    if (registrationStatus.status === 'BAD_REQUEST') {
-      return res.status(BAD_REQUEST).send({
-        message: registrationStatus.message
-      });
-    } else {
-      res.status(CONFLICT).send({ message: registrationStatus.message });
-    }
-  } else {
+  if (registrationStatus.userSaved) {
     const name = req.body.firstName + ' ' + req.body.lastName;
     sendVerificationEmail(name, req.body.email);
-    res.sendStatus(OK);
+    return res.sendStatus(OK);
   }
+  if (registrationStatus.status === 'BAD_REQUEST') {
+    return res.status(BAD_REQUEST).send({
+      message: registrationStatus.message
+    });
+  }
+  return res.status(CONFLICT).send({ message: registrationStatus.message });
 });
 
 router.post('/resendVerificationEmail', async (req, res) => {
