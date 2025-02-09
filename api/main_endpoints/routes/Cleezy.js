950cba88fb4f637c28e1d9aae36cee2b2e8491cf
Aidid888 <aiden6688@hotmail.com>
2025-02-09 01:33:27 -0800

    Added expiration date to the website (ignore old branch, added a package on accideent)

--- api/main_endpoints/routes/Cleezy.js
+++ api/main_endpoints/routes/Cleezy.js
@@ -64,8 +64,8 @@ router.post('/createUrl', async (req, res) => {
   } else if (!await decodeToken(req)) {
     return res.sendStatus(UNAUTHORIZED);
   }
-  const { url, alias } = req.body;
-  let jsonbody = { url, alias: alias || null };
+  const { url, alias, expiration_date: expirationDate } = req.body;
+  let jsonbody = { url, alias: alias || null, expirationDate: expirationDate || null };
   try {
     const response = await axios.post(CLEEZY_URL + '/create_url', jsonbody);
     const data = response.data;
