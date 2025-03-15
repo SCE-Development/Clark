0d479cf0720ce0b06f72cbb0c338b427b61c836b
evan <evanuxd@gmail.com>
2025-03-15 09:25:19 -0700

    use req._parsedUrl.pathname in prometheus route label for api requests

--- api/util/SceHttpServer.js
+++ api/util/SceHttpServer.js
@@ -57,11 +57,12 @@ class SceHttpServer {
 
   registerMetricsMiddleware() {
     this.app.use((req, res, next) => {
+      console.log('!!!!!', req._parsedUrl.pathname)
       res.on('finish', () => {
         MetricsHandler.endpointHits.inc({
           method: req.method,
           // for example "/api/Auth/verify"
-          route: req.originalUrl,
+          route: req._parsedUrl.pathname,
           statusCode: res.statusCode,
         });
       });
