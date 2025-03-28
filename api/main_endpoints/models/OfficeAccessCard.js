66a0a3dbd45fe3734f9a47465b2c70d181da4ea7
mceballos123 <martinceballos789@gmail.com>
2025-03-28 14:06:39 -0700

    Fixed lint errors in the office access card file

--- api/main_endpoints/models/OfficeAccessCard.js
+++ api/main_endpoints/models/OfficeAccessCard.js
@@ -12,11 +12,11 @@ const OfficeAccessCardSchema = new Schema(
       type: Date,
       default: Date.now
     },
-    verifiedCount:{ //Checks how many times the card has been verified
+    verifiedCount:{// Checks how many times the card has been verified
       type:Number,
       default:0
     },
-    lastVerified:{ //Checks last time the card was verified
+    lastVerified:{// Checks last time the card was verified
       type:Date,
       default: Date.now
     }
