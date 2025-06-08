c29ff4dffcf5fbe33f8b1fdabad2573f0c238418
Benjamin Nguyen <xccr123@gmail.com>
2025-06-08 01:06:55 -0700

    change image src to compressed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -91,10 +91,9 @@ const Home = () => {
         <div className={`fade-in-img w-full h-full p-12 overflow-hidden xl:w-3/5${showAll ? ' show' : ''}`}>
           <img
             className="w-full mx-auto transform md:w-4/5 rounded-xl"
-            src="https://github.com/SCE-Development/Clark/assets/116464215/1a763961-5f7e-4ddc-9298-139b25c54a08"
+            src="https://shorturl.at/nya9Q"
           />
         </div>
-
       </div>
 
       <Footer />
