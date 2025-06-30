dca52f92f457354ea2e3da05b83c9742b81ccb38
Aidan Spies <spiesaidan@gmail.com>
2025-06-29 18:01:41 -0700

    Moved array of routes and added keyboard shortcut

---
+++ src/Components/Routing/Routes.js
@@ -0,0 +1,179 @@
+import Overview from '../../Pages/Overview/Overview.js';
+import Login from '../../Pages/Login/Login.js';
+import ForgotPassword from '../../Pages/ForgotPassword/ForgotPassword.js';
+import ResetPasswordPage from '../../Pages/ForgotPassword/ResetPassword.js';
+import Profile from '../../Pages/Profile/MemberView/Profile.js';
+import LedSign from '../../Pages/LedSign/LedSign.js';
+import SpeakerPage from '../../Pages/Speaker/Speaker.js';
+import EditUserInfo from '../../Pages/UserManager/EditUserInfo.js';
+
+import Home from '../../Pages/Home/Home.js';
+import MembershipApplication from
+  '../../Pages/MembershipApplication/MembershipApplication.js';
+import VerifyEmailPage from '../../Pages/MembershipApplication/VerifyEmail.js';
+import Printing from '../../Pages/2DPrinting/2DPrinting.js';
+import AdvertisementAdmin from '../../Pages/Advertisement/AdvertisementAdmin.js';
+
+import AboutPage from '../../Pages/About/About.js';
+import ProjectsPage from '../../Pages/Projects/Projects.js';
+import URLShortenerPage from '../../Pages/URLShortener/URLShortener.js';
+
+import EmailPreferencesPage from '../../Pages/EmailPreferences/EmailPreferences.js';
+
+import sendUnsubscribeEmail from '../../Pages/Profile/admin/SendUnsubscribeEmail.js';
+import Messaging from '../../Pages/Messaging/Messaging.js';
+
+import { membershipState } from '../../Enums.js';
+
+export function getSignedInRoutes(authenticated, user) {
+  const userIsAuthenticated = authenticated;
+  const userIsMember =
+    userIsAuthenticated &&
+    user &&
+    user.accessLevel === membershipState.MEMBER;
+  const userIsOfficerOrAdmin =
+    userIsAuthenticated &&
+    user &&
+    user.accessLevel >= membershipState.OFFICER;
+
+  const notAuthenticatedRoutes = [
+    {
+      Component: Login,
+      path: '/login*',
+      allowedIf: !userIsAuthenticated,
+      redirect: '/',
+      queryParams: {
+        redirect: 'redirect',
+      },
+      pageName: "Login",
+    },
+    {
+      Component: ForgotPassword,
+      path: '/forgot',
+      allowedIf: !userIsAuthenticated,
+      redirect: '/',
+      pageName: "Forgot Password",
+    },
+    {
+      Component: MembershipApplication,
+      path: '/register',
+      allowedIf: !userIsAuthenticated,
+      redirect: '/',
+      pageName: "Membership Application",
+    },
+  ]
+  
+  const authenticatedRoutes = [
+    {
+      Component: Profile,
+      path: '/profile',
+      allowedIf: userIsAuthenticated,
+      redirect: '/login',
+      pageName: "Profile",
+    },
+  ]
+
+  const memberRoutes = [
+    {
+      Component: Printing,
+      path: '/2DPrinting',
+      allowedIf: userIsMember || userIsOfficerOrAdmin,
+      redirect: '/login',
+      pageName: "Printing",
+    },
+    {
+      Component: Messaging,
+      path: '/messaging/:id?',
+      allowedIf: userIsMember || userIsOfficerOrAdmin,
+      redirect: '/login',
+      pageName: "Messaging",
+    },
+    ...authenticatedRoutes,
+  ]
+
+  const officerOrAdminRoutes = [
+    // new for Overview
+    {
+      Component: Overview,
+      path: '/user-manager',
+      allowedIf: userIsOfficerOrAdmin,
+      redirect: '/',
+      inAdminNavbar: true,
+      pageName: "User Manager",
+    },
+    //
+    // {
+    //   Component: EmailPage,
+    //   path: '/email-list',
+    //   allowedIf: userIsOfficerOrAdmin,
+    //   redirect: '/',
+    //   inAdminNavbar: true,
+    //   pageName: "Email",
+    // },
+    {
+      Component: LedSign,
+      path: '/led-sign',
+      allowedIf: userIsOfficerOrAdmin,
+      redirect: '/',
+      inAdminNavbar: true,
+      pageName: "Led Sign",
+    },
+    {
+      Component: SpeakerPage,
+      path: '/speakers',
+      allowedIf: userIsOfficerOrAdmin,
+      redirect: '/',
+      inAdminNavbar: true,
+      pageName: "Speakers",
+    },
+    {
+      Component: EditUserInfo,
+      path: '/user/edit/:id',
+      allowedIf: userIsOfficerOrAdmin,
+      redirect: '/',
+      inAdminNavbar: true,
+      pageName: "Edit User Info",
+    },
+    {
+      Component: URLShortenerPage,
+      path: '/short',
+      allowedIf: userIsOfficerOrAdmin,
+      inAdminNavbar: true,
+      redirect: '/',
+      pageName: "URL Shortner",
+    },
+    {
+      Component: sendUnsubscribeEmail,
+      path: '/unsub',
+      allowedIf: userIsOfficerOrAdmin,
+      inAdminNavbar: true,
+      redirect: '/',
+      pageName: "Send Unsubscribe Email",
+    },
+    {
+      Component: AdvertisementAdmin,
+      path: '/advertisement-admin',
+      allowedIf: userIsOfficerOrAdmin,
+      redirect: '/',
+      inAdminNavbar: true,
+      pageName: "Advertisement Admin",
+    },
+    ...memberRoutes,
+  ]
+
+  return {
+    notAuthenticatedRoutes,
+    authenticatedRoutes,
+    memberRoutes,
+    officerOrAdminRoutes
+  }
+}
+
+export const signedOutRoutes = [
+  { Component: Home, path: '/', pageName: "Home" },
+  { Component: VerifyEmailPage, path: '/verify', pageName: "Verify Email" },
+  { Component: ResetPasswordPage, path: '/reset', pageName: "Reset Password" },
+  { Component: AboutPage, path: '/about', pageName: "About" },
+  { Component: ProjectsPage, path: '/projects', pageName: "Projects" },
+  { Component: EmailPreferencesPage, path: '/emailPreferences', pageName: "Email Preferences" },
+];
---
+++ src/Shortcut.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react"
+import { getSignedInRoutes, signedOutRoutes } from "./Components/Routing/Routes";
+import { useHistory } from "react-router-dom";
+
+function Shortcut({ appProps }) {
+  const [text, setText] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
+  const history = useHistory();
+
+  const { notAuthenticatedRoutes, officerOrAdminRoutes } = getSignedInRoutes(appProps.authenticated, appProps.user);
+  const allRoutes = [...notAuthenticatedRoutes, ...officerOrAdminRoutes, ...signedOutRoutes];
+  const routes = allRoutes.filter((route) => route.allowedIf !== false && route.pageName.toLowerCase().includes(text.toLowerCase()));
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.ctrlKey && e.key === 'k') {
+        e.preventDefault();
+        setIsVisible((prev) => !prev);
+      }};
+      window.addEventListener('keydown', handleKeyPress);
+      return () => {
+        window.removeEventListener('keydown', handleKeyPress);
+      };
+    }, []);
+
+    const handleKeyDown = (e) => {
+      if (e.key === 'Enter' && routes.length > 0) {
+        history.push(routes[0].path);
+        setIsVisible(false);
+        setText('');
+        window.location.reload();
+      }
+    };
+
+  return (
+    <>
+    {isVisible &&
+      <div className="fixed top-0 left-0 w-full h-full backdrop-blur-sm bg-slate-900/50">
+        <div className=" p-[15vh] text-[#939AA7] h-full">
+          <div className="max-w-xl mx-auto divide-y divide-[#939AA7] bg-white rounded-md">
+            <div className="relative flex justify-between px-4 py-2 text-sm ">
+              <div className="flex items-center w-full gap-2 text-black">
+                <input
+                  type="text"
+                  className="w-full h-full p-2 bg-transparent focus-within:outline-none"
+                  placeholder="Type a command..."
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+              </div>
+            </div>
+            <div className="p-2 text-black">
+              {routes.map((route) => (
+                <p
+                key={route.path}
+                className="flex items-center w-full gap-2 text-black">
+                  {route.pageName}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    }
+    </>  
+  );
+}
+
+export default Shortcut
