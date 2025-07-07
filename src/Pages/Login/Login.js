6a3b628b732cf40f721fa4a6848ed6227df113b9
Joshua <116464215+joshua-demo@users.noreply.github.com>
2025-07-07 07:32:07 -0700

    initial commit (#1750)

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -46,7 +46,7 @@ export default function Login(props) {
     <div className = 'flex-none md:flex'>
       <div className='rounded-3xl backdrop-blur-sm shadow-2xl md:w-1/3  mt-20 pb-4 mb-auto ml-auto mr-auto px-5 text-center items-center justify-center'>
         <div className='flex justify-center'>
-          <img id='img' alt='sce logo' src='https://sce.sjsu.edu/images/SCE-glow.webp' width='2rem'  className='w-2/3 px-auto'/>
+          <img id='img' alt='sce logo' src={`${window.location.origin}/images/SCE-glow.webp`} width='2rem'  className='w-2/3 px-auto'/>
         </div>
         <form onSubmit={handleSubmit} className=''>
           <div className='flex flex-col items-center'>
