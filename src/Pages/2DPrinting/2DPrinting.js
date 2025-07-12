7867f16520590ec178e22c84f0a5efb8c4e5a67a
Benjamin Nguyen <xccr123@gmail.com>
2025-07-11 17:48:33 -0700

    implement pdf chunking

--- src/Pages/2DPrinting/2DPrinting.js
+++ src/Pages/2DPrinting/2DPrinting.js
@@ -6,15 +6,13 @@ import {
   getPagesPrinted,
 } from '../../APIFunctions/2DPrinting';
 import { editUser } from '../../APIFunctions/User';
-import { useUser } from '../../Components/context/UserContext';
 
 import { PDFDocument } from 'pdf-lib';
 import { healthCheck } from '../../APIFunctions/2DPrinting';
 import ConfirmationModal from
   '../../Components/DecisionModal/ConfirmationModal.js';
 
-export default function Printing() {
-  const { user, setUser } = useUser();
+export default function Printing(props) {
   const [dragActive, setDragActive] = useState(false);
   const [confirmModal, setConfirmModal] = useState(false);
   const [numberOfPagesInPdfPreview, setNumberOfPagesInPdfPreview] = useState(0);
@@ -42,8 +40,8 @@ export default function Printing() {
 
   async function getNumberOfPagesPrintedSoFar() {
     const result = await getPagesPrinted(
-      user.email,
-      user.token,
+      props.user.email,
+      props.user.token,
     );
     setPrinterHealthy(!result.error);
     if (!result.error) {
@@ -52,11 +50,9 @@ export default function Printing() {
   }
 
   useEffect(() => {
-    if (user && user.email && user.token) {
-      checkPrinterHealth();
-      getNumberOfPagesPrintedSoFar();
-    }
-  }, [user]);
+    checkPrinterHealth();
+    getNumberOfPagesPrintedSoFar();
+  }, []);
 
   const INPUT_CLASS_NAME = 'indent-2 block rounded-md border-0 py-1.5   shadow-sm ring-1 ring-inset ring-gray-300 placeholder:  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6';
 
@@ -189,18 +185,27 @@ export default function Printing() {
   }
 
   async function handlePrinting() {
+    if (PdfFile.size > 1024 * 1024 * 150) {
+      setPrintStatus('File exceeds 150 MB size limit');
+      setPrintStatusColor('error');
+      return;
+    }
+
     // send print request with files and configuratiosn in formData
     const data = new FormData();
+
     data.append('file', PdfFile);
     data.append('sides', sides);
     data.append('copies', copies);
-    let status = await printPage(data, user.token);
+    let status = await printPage(data, props.user.token);
 
     if (!status.error) {
+      editUser(
+        { ...props.user, pagesPrinted: pagesPrinted + pagesToBeUsedInPrintRequest },
+        props.user.token,
+      );
       setPrintStatus('Printing succeeded!');
       setPrintStatusColor('success');
-      const updatedUser = { ...user, pagesPrinted: pagesPrinted + pagesToBeUsedInPrintRequest };
-      editUser(updatedUser, user.token);
     } else {
       setPrintStatus('Printing failed. Please try again or reach out to SCE Dev team if the issue persists.');
       setPrintStatusColor('error');
@@ -391,7 +396,7 @@ export default function Printing() {
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
               </svg>
               <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-              <p className="text-xs text-gray-500 dark:text-gray-400">PDF only, max 10MB</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">PDF only, max 150MB</p>
             </div>
           </label>
           <input id="dropzone-file" type="file"
@@ -442,5 +447,4 @@ export default function Printing() {
     </div>
   );
 }
-
 
