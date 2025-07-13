ddc27db9ea1b46e540fb041ec8077b54130ed8f9
adarshm11 <amlearning2023@gmail.com>
2025-07-13 16:52:45 -0700

    added unit tests and refactored backend

--- api/main_endpoints/routes/OfficeAccessCard.js
+++ api/main_endpoints/routes/OfficeAccessCard.js
@@ -20,92 +20,13 @@ const {
   checkIfTokenValid
 } = require('../util/token-functions.js');
 const ROWS_PER_PAGE = 25;
-const { ADJECTIVES, NOUNS } = require('../../util/CardReaderConstants.js');
+const {
+  checkIfCardExists,
+  generateAlias,
+  deleteCard,
+} = require('../util/OfficeAccessCard.js');
 
 router.use(bodyParser.json());
-
-function checkIfCardExists(cardBytes) {
-  return new Promise((resolve) => {
-    try {
-      OfficeAccessCard.findOneAndUpdate(
-        { cardBytes },
-        {
-          $inc: { verifiedCount: 1 },
-          $set: { lastVerified: Date.now() }
-        }, {
-          useFindAndModify: false, new: true, upsert: false
-        }
-        , (error, result) => {
-          if (error) {
-            logger.error('checkIfCardExists got an error querying mongodb: ', error);
-            return resolve(false);
-          }
-          if (!result) {
-            logger.info(`Card:${cardBytes} not found in the database`);
-          }
-          return resolve(result); // return the document
-        });
-    } catch (error) {
-      logger.error('checkIfCardExists caught an error: ', error);
-      return resolve(false);
-    }
-  });
-}
-
-function deleteCard(cardBytes) {
-  return new Promise((resolve) => {
-    try {
-      OfficeAccessCard.findOneAndDelete(
-        { cardBytes }
-        , (error, result) => {
-          if (error) {
-            logger.error('deleteCard got an error querying mongodb: ', error);
-            return resolve(false);
-          }
-          if (!result) {
-            logger.info(`Card ${cardBytes} not found in the database`);
-          }
-          return resolve(!!result);
-        }
-      );
-    } catch (error) {
-      logger.error('deleteCard caught an error: ', error);
-      return resolve(false);
-    }
-  });
-}
-
-function checkIfAliasExists(alias) {
-  return new Promise((resolve) => {
-    try {
-      OfficeAccessCard.findOne(
-        { alias }
-        , (error, result) => {
-          if (error) {
-            logger.error('checkIfAliasExists got an error querying mongodb: ', error);
-            return resolve(false);
-          }
-          return resolve(!!result);
-        }
-      );
-    } catch (error) {
-      logger.error('checkIfAliasExists caught an error: ', error);
-      return resolve(false);
-    }
-  });
-}
-
-async function generateAlias() {
-  for (let i = 0; i < 5; ++i) {
-    const noun = NOUNS[Math.floor(Math.random() * NOUNS.length)];
-    const adjective = ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)];
-    const alias = `${adjective} ${noun}`;
-    const aliasExists = await checkIfAliasExists(alias);
-    if (aliasExists) continue;
-    return alias;
-  }
-  return new Date().toGMTString();
-}
 
 let clients = [];
 
@@ -242,7 +163,7 @@ router.post('/delete', async (req, res) => {
     return res.sendStatus(OK);
   }
 
-  writeLogToClient({
+  writeLogToClient(req.method, {
     alias: cardExists.alias,
     statusCode: SERVER_ERROR,
     message: 'Error deleting card',
