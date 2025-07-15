8bb25fe86056eedd8ba61425adcb683753c29538
kingsand <clinvil2@gmail.com>
2025-07-15 11:47:53 -0700

    Made trashcan white in dark mode, kept color for light mode

--- src/Pages/Overview/SVG.js
+++ src/Pages/Overview/SVG.js
@@ -45,6 +45,7 @@ export function trashcanSymbol(color = 'black') {
       <path
         d='M3 6l3 18h12l3-18h-18zm19-4v2h-20v-2h5.711c.9 0
       1.631-1.099 1.631-2h5.316c0 .901.73 2 1.631 2h5.711z'
+        className='dark:fill-white black'
       />
     </svg>
   );
