e860bd0c0da019d6274353ee700a3d49f1e430ae
ariansbahram <ariansbahram@gmail.com>
2025-07-24 01:23:19 -0700

    Fix URL error handling for Cleezy page

--- api/main_endpoints/routes/Cleezy.js
+++ api/main_endpoints/routes/Cleezy.js
@@ -73,7 +73,7 @@ router.post('/createUrl', async (req, res) => {
     res.json({ ...data, link: u });
   } catch (err) {
     logger.error('/createUrl had an error', err);
-    res.status(err.response.status).json({ error: err.response.status });
+    res.status(err.response.status).json({ error: err.response.data?.detail || err.response.data || 'Unknown error from Cleezy' });
   }
 });
 
--- src/Pages/URLShortener/URLShortener.js
+++ src/Pages/URLShortener/URLShortener.js
@@ -77,7 +77,7 @@ export default function URLShortenerPage(props) {
       return true;
     } else {
       setAliasTaken(true);
-      setErrorAlertMessage('That alias is taken!');
+      setErrorAlertMessage(response.responseData || 'Unknown error during URL creation!');
       return false;
     }
   }
