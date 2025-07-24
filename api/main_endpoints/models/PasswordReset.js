1429002a6751926ac410663b2bb29ce40cb986f1
alvintran <tralvin3@gmail.com>
2025-07-24 12:45:45 -0700

    update schema

--- api/main_endpoints/models/PasswordReset.js
+++ api/main_endpoints/models/PasswordReset.js
@@ -1,21 +1,24 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
-const PasswordResetSchema = new Schema({
-  resetToken: {
-    type: String,
-    required: true,
-    index: true,
+const PasswordResetSchema = new Schema(
+  {
+    resetToken: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    expireAt: {
+      type: Date,
+      default: () => new Date(Date.now() + 86400000), // 24 hours
+      index: { expireAfterSeconds: 0 },
+    }
   },
-  userId: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: '24h',
-  }
-});
+  { collection: 'PasswordResets' }
+);
 
 module.exports = mongoose.model('PasswordReset', PasswordResetSchema);
