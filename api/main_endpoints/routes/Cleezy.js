d15d557bb696a7f4b143be6bc433dec641d8d014
Pikalot <pikalot@hotmail.com>
2025-07-29 08:08:48 -0700

    Moved CleezyFunction.js to api/util and renamed it cleezyHelpers.js.

--- api/main_endpoints/routes/Cleezy.js
+++ api/main_endpoints/routes/Cleezy.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const {
   decodeToken,
   checkIfTokenSent,
-  checkIfTokenValid,
 } = require('../util/token-functions.js');
 const {
   OK,
@@ -14,7 +13,6 @@ const {
 } = require('../../util/constants').STATUS_CODES;
 const logger = require('../../util/logger');
 const { Cleezy } = require('../../config/config.json');
-const membershipState = require('../../util/constants').MEMBERSHIP_STATE;
 const { ENABLED } = Cleezy;
 
 let CLEEZY_URL = process.env.CLEEZY_URL
@@ -99,36 +97,4 @@ router.post('/deleteUrl', async (req, res) => {
     });
 });
 
-const searchCleezyUrls = async (req) => {
-  if(!ENABLED || !req.body.query) {
-    return { status: OK, data: [] };
-  }
-
-  if (!checkIfTokenSent(req)) {
-    return { status: FORBIDDEN, data: [] };
-  } else if (!checkIfTokenValid(req, membershipState.OFFICER)) {
-    return { status: UNAUTHORIZED, data: [] };
-  }
-
-  try {
-    const cleezyQuery = req.body.query.replace(/[^a-zA-Z0-9]/g, '');
-    const cleezyRes = await axios.get(CLEEZY_URL + '/list', {
-      params: {
-        search: cleezyQuery
-      }
-    });
-    const cleezyData = cleezyRes.data?.data
-      .slice(0, 5)
-      .map(e => {
-        const u = new URL(e.alias, URL_SHORTENER_BASE_URL);
-        return { ...e, link: u.href };
-      });
-
-    return { status: OK, data: cleezyData };
-  } catch (err) {
-    logger.error('cleezy search urls had an error', err);
-    return { status: SERVER_ERROR, data: [] };
-  }
-};
-
-module.exports = {router, searchCleezyUrls};
+module.exports = router;
