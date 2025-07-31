55d19280e2cdbe28bf662b777979aaaccbd15acb
luSteven01 <146397397+luSteven01@users.noreply.github.com>
2025-07-30 21:40:37 -0700

    added support for button on mobile devices

--- src/Components/Navbar/UserNavbar.js
+++ src/Components/Navbar/UserNavbar.js
@@ -86,17 +86,17 @@ export default function UserNavbar(props) {
       <div className="hidden navbar-center sm:flex">
         <ul className="menu menu-horizontal items-center">
           {getRoutesForNavbar()}
-            <button
-              onClick={() => setModalOpen(true)}
-              className="btn btn-ghost"
-              aria-label="Open search"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-            </button>
         </ul>
       </div>
 
       <div className="navbar-end">
+        <button
+          onClick={() => setModalOpen(true)}
+          className="btn btn-ghost btn-circle"
+          aria-label="Open search"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </button>
         {authenticated && user ? (
           <>
             <div className="dropdown dropdown-end sm:hidden">
