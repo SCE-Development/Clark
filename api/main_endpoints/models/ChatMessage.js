b8142fcf61a9dbf4c4a450345cec4b44b460d2a7
Embotic-Wayne <ngowayne12@gmail.com>
2025-08-03 17:48:52 -0700

    Add ChatMessage model and update /send and /getLatestMessage

---
+++ api/main_endpoints/models/ChatMessage.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const ChatMessageSchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    index: true,
+  },
+  expiresAt: {
+    type: Date,
+    default: () => new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours from now
+  },
+  chatroomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    index: true,
+    ref: 'Chatroom',
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  text: {
+    type: String,
+    required: true,
+  }
+});
+
+// Compound index
+ChatMessageSchema.index({ chatroomId: 1, createdAt: -1 });
+
+module.exports = mongoose.model('ChatMessage', ChatMessageSchema);
