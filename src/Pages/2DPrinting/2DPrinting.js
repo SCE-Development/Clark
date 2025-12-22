f74fe1ddd0cd020b3c81ccb0c32eb9bc80e8021f
evan <evanuxd@gmail.com>
2025-12-22 14:12:43 -0800

    debug zone

--- src/Pages/2DPrinting/2DPrinting.js
+++ src/Pages/2DPrinting/2DPrinting.js
@@ -76,6 +76,7 @@ export default function Printing() {
   useEffect(() => {
     if (printJobs === null || Object.keys(printJobs).length === 0) return;
     const ids = Object.keys(printJobs);
+    console.log('useEffect', {ids, printJobs})
 
     const interval = setInterval(async () => {
       if (ids.length === 0) {
@@ -86,7 +87,7 @@ export default function Printing() {
       ids.map(async (id) => {
         const completedOrFailed = ['completed', 'failed'].includes(printJobs[id].status);
         if (completedOrFailed) return;
-
+        console.log('ids.map', {id, 'printJobs[id]': printJobs[id]})
         const status = await getPrintStatus(id, printJobs[id].pages, user.token);
         const newPrintJobs = {...printJobs};
         newPrintJobs[id].status = status;
