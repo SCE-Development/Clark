a924543ff5f2e1933c470b2878e8472cf4ee18a1
Benjamin Nguyen <89621837+thebeninator@users.noreply.github.com>
2025-12-26 14:57:50 -0800

    print job status polling (#1894)
    * save
    
    * poll job status on print page; local storage first pass
    
    * delete printId from local storage on complete
    
    * resume polling if printId still exists
    
    * add undefined check for localStorage in useEffect
    
    * save
    
    * save
    
    * use promise for tryResolvePrint
    
    * convert /status to GET, require token
    
    * multiple print job tracking first pass
    
    * fix lint
    
    * fix interval using stale jobs state
    
    * implement doodoo ui for active prints
    
    * store job in local immediately; remove guard clause for 'PRINTED' state
    
    * some clean up
    
    * FIX LINT
    
    * check if localstorage printJobs exists first before setting state
    
    * undoodoo the doodoo ui first pass
    
    * implement page escrow
    
    * some cleanup; actually save escrow modify on print fail
    
    * failed/completed jobs notifs now persist for 5 secs
    
    * fix fail/complete notif not being destroyed after page refresh
    
    * rename function
    
    * checkiftokensent
    
    * const user = await User.findById(decoded._id);
    
    * get ready for some noise
    
    * const user = await User.findById(decoded.token._id);
    
    * debug zone
    
    * logger.error('/status had an error: ', err);
    
    * const response = await axios.get(url);
    
    * const response = await axios.get(, {
    
    * const user = await User.findById(decodedToken.token._id);
    
    * one thing
    
    * one
    
    ---------
    
    Co-authored-by: evan <evanuxd@gmail.com>

--- src/APIFunctions/2DPrinting.js
+++ src/APIFunctions/2DPrinting.js
@@ -60,6 +60,24 @@ export function parseRange(pages, maxPages) {
   return result;
 }
 
+export async function getPrintStatus(printId, totalPages, token) {
+  const url = new URL('/api/Printer/status', BASE_API_URL);
+  url.searchParams.append('id', printId);
+  url.searchParams.append('pages', totalPages);
+
+  const response = await fetch(url, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    method: 'GET',
+  });
+
+  const json = await response.json();
+  const status = json.status;
+
+  return status;
+}
+
 /**
  * Print the page
  * @param {Object} data - PDF File and its configurations
@@ -80,6 +98,7 @@ export async function printPage(data, token) {
   const pdf = data.get('file');
   const sides = data.get('sides');
   const copies = data.get('copies');
+  const totalPages = data.get('totalPages');
   const id = crypto.randomUUID();
   const CHUNK_SIZE = 1024 * 1024 * 0.5; // 0.5 MB ------- SENT DATA **CANNOT** EXCEED 1 MB
   const totalChunks = Math.ceil(pdf.size / CHUNK_SIZE);
@@ -98,6 +117,7 @@ export async function printPage(data, token) {
       chunkData.append('id', id);
       chunkData.append('sides', sides);
       chunkData.append('copies', copies);
+      chunkData.append('totalPages', totalPages);
     }
 
     try {
