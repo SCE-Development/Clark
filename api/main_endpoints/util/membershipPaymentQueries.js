6e3675fdf33e543f1e8b47c80fd4df23cddd4148
Emily <emilythach005@gmail.com>
2026-01-04 22:25:20 -0800

    fixed spacing 2

--- api/main_endpoints/util/membershipPaymentQueries.js
+++ api/main_endpoints/util/membershipPaymentQueries.js
@@ -6,26 +6,26 @@ const status = {
 };
 
 export async function findPayment(confirmationCode) {
-    return MembershipPayment.findOne({
-        confirmationCode,
-        status: status.PENDING,
-    });
+  return MembershipPayment.findOne({
+    confirmationCode,
+    status: status.PENDING,
+  });
 }
 
 export async function verifyPayment(paymentId, userId) {
-    await MembershipPayment.updateOne(
-        { _id: paymentId }, 
-        { $set: { 
-            userId,
-            status: status.COMPLETED
-        }}
-    )
+  await MembershipPayment.updateOne(
+    { _id: paymentId },
+    { $set: {
+      userId,
+      status: status.COMPLETED
+    }}
+  );
 }
 
 export async function rejectPayment(paymentId) {
-    await MembershipPayment.updateOne(
-        { _id: paymentId },
-        { $set: { status: status.REJECTED } }
-    );
+  await MembershipPayment.updateOne(
+    { _id: paymentId },
+    { $set: { status: status.REJECTED } }
+  );
 }
 
