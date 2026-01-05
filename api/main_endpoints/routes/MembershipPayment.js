026fe55d816d1620b2adc199548a8f368805d33d
Emily <emilythach005@gmail.com>
2026-01-04 22:24:47 -0800

    fixed spacing 2

--- api/main_endpoints/routes/MembershipPayment.js
+++ api/main_endpoints/routes/MembershipPayment.js
@@ -15,74 +15,74 @@ const { decodeToken } = require('../../util/auth');
 const { writeLogToClient } = require('../../util/logging');
 
 router.post('/verifyMembership', async (req, res) => {
-    const decoded = await decodeToken(req, membershipState.PENDING);
-    if (decoded.status !== OK) {
-        return res.sendStatus(decoded.status);
+  const decoded = await decodeToken(req, membershipState.PENDING);
+  if (decoded.status !== OK) {
+    return res.sendStatus(decoded.status);
+  }
+
+  const { confirmationCode } = req.body;
+  const userId = decoded.token._id;
+
+  if (!confirmationCode) {
+    writeLogToClient(req.method, {
+      statusCode: BAD_REQUEST,
+      message: 'please type in your confirmation code',
+    });
+    return res.sendStatus(BAD_REQUEST);
+  }
+
+  try {
+    const paymentDocument = await findPayment(confirmationCode);
+
+    if (!paymentDocument) {
+      writeLogToClient(req.method, {
+        statusCode: NOT_FOUND,
+        message: 'payment not found',
+      });
+      return res.sendStatus(NOT_FOUND);
+    }
+    const paymentId = paymentDocument._id;
+    const amount = paymentDocument.amount;
+    let accessLevel;
+    let membershipValidUntil;
+
+    if (amount >= 30) {
+      accessLevel = membershipState.MEMBER;
+      membershipValidUntil = getMemberExpirationDate(2);
+    } else if (amount >= 20) {
+      accessLevel = membershipState.MEMBER;
+      membershipValidUntil = getMemberExpirationDate(1);
+    } else {
+      await rejectPayment(paymentId);
+      writeLogToClient(req.method, {
+        statusCode: BAD_REQUEST,
+        message: 'not enough money sent to become a member',
+      });
+      return res.sendStatus(BAD_REQUEST);
     }
 
-    const { confirmationCode } = req.body 
-    const userId = decoded.token._id
-
-    if (!confirmationCode) {
-        writeLogToClient(req.method, {
-            statusCode: BAD_REQUEST,
-            message: 'please type in your confirmation code',
-        });
-        return res.sendStatus(BAD_REQUEST);
-    }
-
-    try {
-        const paymentDocument = await findPayment(confirmationCode)
-
-        if (!paymentDocument) {
-            writeLogToClient(req.method, {
-                statusCode: NOT_FOUND,
-                message: 'payment not found',
-            });
-            return res.sendStatus(NOT_FOUND);
+    await verifyPayment(paymentId, userId);
+    await User.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          accessLevel,
+          membershipValidUntil
         }
-        const paymentId = paymentDocument._id
-        const amount = paymentDocument.amount
-        let accessLevel
-        let membershipValidUntil
-        
-        if (amount >= 30) {
-            accessLevel = membershipState.MEMBER
-            membershipValidUntil = getMemberExpirationDate(2)
-        } else if (amount >= 20) {
-            accessLevel = membershipState.MEMBER
-            membershipValidUntil = getMemberExpirationDate(1)
-        } else {
-            await rejectPayment(paymentId)
-            writeLogToClient(req.method, {
-                statusCode: BAD_REQUEST,
-                message: 'not enough money sent to become a member',
-            });
-            return res.sendStatus(BAD_REQUEST);
-        }
+      }
+    );
 
-        await verifyPayment(paymentId, userId)
-        await User.updateOne(
-            { _id: userId },
-            {
-                $set: {
-                    accessLevel,
-                    membershipValidUntil
-                }
-            }
-        )
+    writeLogToClient(req.method, {
+      statusCode: OK,
+      message: 'successfully verified user',
+    });
+    return res.sendStatus(OK);
 
-        writeLogToClient(req.method, {
-            statusCode: OK,
-            message: 'successfully verified user',
-        });
-        return res.sendStatus(OK);
-
-    } catch (error){
-        writeLogToClient(req.method, {
-            statusCode: SERVER_ERROR,
-            message: 'internal server error',
-        });
-        return res.sendStatus(SERVER_ERROR);
-    }
-})
+  } catch (error){
+    writeLogToClient(req.method, {
+      statusCode: SERVER_ERROR,
+      message: 'internal server error',
+    });
+    return res.sendStatus(SERVER_ERROR);
+  }
+});
