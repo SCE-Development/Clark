5d7079e9fdc9541f2f1cbf3deeafb51471a6733a
Emily <emilythach005@gmail.com>
2026-01-05 18:37:57 -0800

    final fixes

--- api/main_endpoints/routes/MembershipPayment.js
+++ api/main_endpoints/routes/MembershipPayment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 const {
