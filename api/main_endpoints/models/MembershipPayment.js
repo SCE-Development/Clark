fadaefafde71ebb015e184557b4d23811ff1467e
Tyffoni <tyffoni@gmail.com>
2026-01-05 22:03:35 -0800

    added membership payment schema

---
+++ api/main_endpoints/models/MembershipPayment.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const MembershipPaymentSchema = new Schema(
+  {
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      default: null,
+    },
+    status: {
+      type: String,
+      enum: Object.keys({PENDING: 'PENDING', COMPLETED: 'COMPLETED', FAILED: 'FAILED'  }),
+      default: 'PENDING',
+      required: true,
+    },
+    confirmationCode: {
+      type: String,
+      required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    venmoDetails:{
+        transactionId: { type: String, default: '' },
+        payerName: { type: String, default: '' },
+        note: { type: String, default: '' },
+    }
+  },
+  { collection: 'MembershipPayments' }
+);
+
+
+module.exports = mongoose.model('MembershipPayment.js', MembershipPaymentSchema);
