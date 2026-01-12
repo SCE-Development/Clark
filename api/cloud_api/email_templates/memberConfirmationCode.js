5ca42f711401030d3b7b09ccebf2014a7341fb69
Charlynn Nguyen <charred@Charlynns-MacBook-Pro.local>
2026-01-11 21:29:59 -0800

    i pray i fixed my origin

---
+++ api/cloud_api/email_templates/memberConfirmationCode.js
@@ -0,0 +1,20 @@
+function membershipConfirmationCode(user, recipient, confirmCode) {
+  if (!confirmCode) {
+    throw new Error('Confirmation code is required');
+  }
+
+  return {
+    from: user,
+    to: recipient,
+    subject: 'SCE Membership Confirmation',
+    generateTextFromHTML: true,
+    html: `
+            <p>>Hi,</p><br/>
+            <p>
+              Thank you for signing up for membership! <br />
+              Here is your confirmation code:</p>
+             <b>${confirmCode}</b>
+      `
+  };
+}
+module.exports = { membershipConfirmationCode };
