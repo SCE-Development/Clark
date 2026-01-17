839bb6e97bd1fdaa665cc8c98931fbb9cf20404b
evan <evanuxd@gmail.com>
2026-01-17 09:30:00 -0800

    reduce diff size

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState} from 'react';
 import Footer from '../../Components/Footer/Footer.js';
 import './Home.css';
 
