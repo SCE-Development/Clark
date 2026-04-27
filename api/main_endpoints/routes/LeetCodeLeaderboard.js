f75eeb2d9f6de506591b6e820c435d038edcd549
adarshm11 <amlearning2023@gmail.com>
2026-04-26 20:24:04 -0700

    simplify api

--- api/main_endpoints/routes/LeetCodeLeaderboard.js
+++ api/main_endpoints/routes/LeetCodeLeaderboard.js
@@ -7,15 +7,12 @@ const {
 } = require('../../util/constants.js').STATUS_CODES;
 const { decodeToken } = require('../util/token-functions.js');
 const logger = require('../../util/logger.js');
-const {
-  getAllUsers,
-  addUserToLeaderboard,
-  deleteUserFromLeaderboard,
-} = require('../util/LeetCodeLeaderboard.js');
 const AuditLogActions = require('../util/auditLogActions.js');
 const AuditLog = require('../models/AuditLog.js');
 const membershipState = require('../../util/constants').MEMBERSHIP_STATE;
-const { SIGN2 = {} } = require('../../config/config.json');
+const { LEETCODE_LED_SIGN = {} } = require('../../config/config.json');
+const axios = require('axios');
+const LEETCODE_LED_SIGN_URL = process.env.LEETCODE_LED_SIGN_URL || 'http://localhost:12121';
 
 router.get('/', async (req, res) => {
   const decoded = await decodeToken(req, membershipState.OFFICER);
@@ -23,16 +20,23 @@ router.get('/', async (req, res) => {
     return res.sendStatus(decoded.status);
   }
 
-  if (!SIGN2.ENABLED) {
+  if (!LEETCODE_LED_SIGN.ENABLED) {
     logger.warn('LeetCode Leaderboard is disabled, returning 200 to mock the service');
     return res.sendStatus(OK);
   }
 
-  const users = await getAllUsers();
-  if (!users) {
-    return res.sendStatus(SERVER_ERROR);
+  try {
+    const url = new URL('/getAllUsers', LEETCODE_LED_SIGN_URL);
+    const { data } = await axios.get(url.href);
+    if ('error' in data) {
+      logger.error('Error from LeetCode Leaderboard: ', data.error);
+      return res.status(SERVER_ERROR).send('Error fetching users from LeetCode Leaderboard');
+    }
+    return res.status(OK).json({ users: data.users });
+  } catch (err) {
+    logger.error('Error fetching users from LeetCode Leaderboard: ', err);
+    return res.status(SERVER_ERROR).send('Error fetching users from LeetCode Leaderboard');
   }
-  return res.status(OK).send({ users });
 });
 
 router.post('/addUser', async (req, res) => {
@@ -41,32 +45,31 @@ router.post('/addUser', async (req, res) => {
     return res.sendStatus(decoded.status);
   }
 
-  if (!SIGN2.ENABLED) {
+  if (!LEETCODE_LED_SIGN.ENABLED) {
     logger.warn('LeetCode Leaderboard is disabled, returning 200 to mock the service');
     return res.sendStatus(OK);
   }
 
   const { username, firstName, lastName } = req.body;
-  const required = [
-    { value: username, title: 'LeetCode username', },
-    { value: firstName, title: 'User\'s first name', },
-    { value: lastName, title: 'User\'s last name', }
-  ];
 
-  const missingValue = required.find(({ value }) => !value);
-
-  if (missingValue) {
-    return res.status(BAD_REQUEST).send(`${missingValue.title} missing from request`);
-  }
-
-  const tryAddUser = await addUserToLeaderboard({
-    username,
-    firstName,
-    lastName
-  });
-
-  if (tryAddUser !== OK) {
-    return res.status(tryAddUser).send('Error adding user to LeetCode Leaderboard');
+  try {
+    const url = new URL('/user/add', LEETCODE_LED_SIGN_URL);
+    const { data } = await axios.post(url.href, {
+      username,
+      firstName,
+      lastName
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if ('error' in data) {
+      logger.error('Error from LeetCode Leaderboard: ', data.error);
+      return res.status(SERVER_ERROR).send('Error adding user to LeetCode Leaderboard');
+    }
+  } catch (err) {
+    logger.error('Error adding user to LeetCode Leaderboard: ', err);
+    return res.status(SERVER_ERROR).send('Error adding user to LeetCode Leaderboard');
   }
 
   AuditLog.create({
@@ -83,18 +86,27 @@ router.post('/deleteUser', async (req, res) => {
     return res.sendStatus(decoded.status);
   }
 
-  if (!SIGN2.ENABLED) {
+  if (!LEETCODE_LED_SIGN.ENABLED) {
     logger.warn('LeetCode Leaderboard is disabled, returning 200 to mock the service');
     return res.sendStatus(OK);
   }
 
   const { username } = req.body;
-  if (!username) {
-    return res.status(BAD_REQUEST).send('Username field missing');
-  }
 
-  if (!await deleteUserFromLeaderboard(username)) {
-    return res.sendStatus(SERVER_ERROR);
+  try {
+    const url = new URL('/user/remove', LEETCODE_LED_SIGN_URL);
+    const { data } = await axios.post(url.href, { username }, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if ('error' in data) {
+      logger.error('Error from LeetCode Leaderboard: ', data.error);
+      return res.status(SERVER_ERROR).send('Error deleting user from LeetCode Leaderboard');
+    }
+  } catch (err) {
+    logger.error('Error deleting user from LeetCode Leaderboard: ', err);
+    return res.status(SERVER_ERROR).send('Error deleting user from LeetCode Leaderboard');
   }
 
   AuditLog.create({
