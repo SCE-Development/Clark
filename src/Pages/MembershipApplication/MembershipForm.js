d9507d494147ef73d1dae0dfe0a6e4c8e9d3de24
adarsh <110150037+adarshm11@users.noreply.github.com>
2026-08-20 17:08:23 -0700

    update venmo link to be cleezy venmo shortcut

--- src/Pages/MembershipApplication/MembershipForm.js
+++ src/Pages/MembershipApplication/MembershipForm.js
@@ -287,7 +287,7 @@ export default function MembershipForm(props) {
           </div>
           <div className='venmo-link text-2xl  md:text-3xl  py-3'>
             <a
-              href='https://venmo.com/u/jonathan-nguyen04'
+              href='https://sce.sjsu.edu/s/venmo'
               style={{ color: 'white' }}
               className='opacity-50 hover:opacity-100 font-bold underline duration-300'
             >
